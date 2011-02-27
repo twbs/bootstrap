10199da5717d6078b5120ef33e84063c385e49ff
Samus_ <mail2samus@gmail.com>
2011-02-27 06:55:40 -0200

    fix nonsensical copy process

--- createproject.sh
+++ createproject.sh
@@ -12,66 +12,31 @@
 # $ cd  html5-boilerplate/build
 # $ ./createproject.sh
 
-hash cpio >/dev/null 2>&1 || { echo "cpio not found, aborting." >&2; exit 1; }
+# find project root (also ensure script is ran from within repo)
+src=$(git rev-parse --show-toplevel) || exit 1
 
+# get a name for new project
 while [[ -z $name ]]
 do
     echo "To create a new html5-boilerplate project, enter a new directory name:"
     read name || exit
 done
-
-cd ..
-
-webroot=$PWD
-
-src=$webroot/html5-boilerplate
-dst=$webroot/../$name
+dst=$src/../$name
 
 if [[ -d $dst ]]
 then
     echo "$dst exists"
 else
     #create new project
-    mkdir "$name"
+    mkdir "$dst" || exit 1
 
     #sucess message
     echo "Created Directory: $dst"
     
     cd "$src"
-    
-    #copy to new project directory
-    #http://en.wikipedia.org/wiki/Cpio#Copy
-    #http://cybertiggyr.com/cpio-howto/
-    #http://www.cyberciti.biz/faq/how-do-i-use-cpio-command-under-linux/
-    find . -depth -print0 | cpio -0pdmv "$dst"
-    
+    cp -vr css/ js/ images/ *.html *.xml *.txt *.png *.ico .htaccess "$dst"
 
     #sucess message
     echo "Created Project: $dst"
-    
-    # delete that temporary folder
-    rm -r "$name"
-    
-    #move into new project
-    cd "$dst"
-    
-    #in Bourne Again Shell, the cpio was copying 
-    #the whole dir into the new project, along with the contents
-    if [[ -d $dst/html5-boilerplate ]]
-    then
-        rm -r html5-boilerplate
-    fi        
-    
-    if [[ -e $dst/createproject.sh ]]
-    then
-        rm -r createproject.sh
-    fi  
-    
-    if [[ -e $dst/.git ]]
-    then
-        rm -rf .git
-    fi
-
-
 fi
 
