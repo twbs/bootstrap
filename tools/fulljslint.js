6ce870bf180b5bc6c2cbf2d638eee4fe6aab3ce1
Joe Morgan <josepht.morgan@gmail.com>
2011-04-04 21:50:14 -0400

    added config options to jslint

--- tools/fulljslint.js
+++ tools/fulljslint.js
@@ -535,6 +535,6 @@ o.push(m+'<br>*/</pre>');}
 o.push('</div>');}}
 return o.join('');};itself.jslint=itself;itself.edition='2010-09-13';return itself;}());(function(a){var e,i,input;if(!a[0]){print("Usage: jslint.js file.js");quit(1);}
 input=readFile(a[0]);if(!input){print("jslint: Couldn't open file '"+a[0]+"'.");quit(1);}
-if(!JSLINT(input,{bitwise:true,eqeqeq:true,immed:true,newcap:true,nomen:true,onevar:true,plusplus:true,regexp:true,rhino:true,undef:true,white:false})){for(i=0;i<JSLINT.errors.length;i+=1){e=JSLINT.errors[i];if(e){print('Lint at line '+e.line+' character '+
+if(!JSLINT(input,{predef:["$","jQuery"],maxerr:100,evil:true,browser:true,eqeqeq:true,immed:true,newcap:true,nomen:true,onevar:true,es5:true,rhino:true,undef:true,white:false,devel:true})){for(i=0;i<JSLINT.errors.length;i+=1){e=JSLINT.errors[i];if(e){print('Lint at line '+e.line+' character '+
 e.character+': '+e.reason);print((e.evidence||'').replace(/^\s*(\S*(\s+\S+)*)\s*$/,"$1"));print('');}}
 quit(2);}else{print("jslint: No problems found in "+a[0]);quit();}}(arguments));
