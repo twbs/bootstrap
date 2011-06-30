cde63b98bc97595e47a15b3178097f89edb7c6c7
Mark Otto <markdotto@gmail.com>
2011-06-30 13:36:47 -0700

    remove unnecessary js lang files from prettify, update styles for prettify

--- docs/assets/js/google-code-prettify/lang-tex.js
+++
@@ -1,1 +0,0 @@
-PR.registerLangHandler(PR.createSimpleLexer([["pln",/^[\t\n\r \xa0]+/,null,"\t\n\r Â\xa0"],["com",/^%[^\n\r]*/,null,"%"]],[["kwd",/^\\[@-Za-z]+/],["kwd",/^\\./],["typ",/^[$&]/],["lit",/[+-]?(?:\.\d+|\d+(?:\.\d*)?)(cm|em|ex|in|pc|pt|bp|mm)/i],["pun",/^[()=[\]{}]+/]]),["latex","tex"]);
