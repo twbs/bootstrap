52c624079e685a5bcdc13af25b86ea08ca34d6d1
Jacob Thornton <jacobthornton@gmail.com>
2011-08-27 00:07:42 -0700

    sometimes i get emotional about semicolons

--- examples/assets/js/bootstrap-modals.js
+++ examples/assets/js/bootstrap-modals.js
@@ -6,9 +6,9 @@
   $.support.transition = (function(){
     var thisBody = document.body || document.documentElement
       , thisStyle = thisBody.style
-      , support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined;
-    return support;
-  })();
+      , support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined
+    return support
+  })()
 
 
  /* SHARED VARS
