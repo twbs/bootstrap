ed96f181c89a4bb152d58b5c9753ee5aedbd8c4b
Jacob Thornton <jacobthornton@gmail.com>
2011-08-27 21:46:50 -0700

    add topbar dropdown js

---
+++ examples/assets/js/bootstrap-dropdown.js
@@ -0,0 +1,22 @@
+(function( $ ){
+
+ /* DROPDOWN PLUGIN DEFINITION
+  * ========================== */
+
+  function clearMenus() {
+    $('a.menu').parent('li').removeClass('open')
+  }
+
+  $(window).bind("click", clearMenus)
+
+  $.fn.dropdown = function ( options ) {
+    return this.each(function () {
+      $(this).delegate('a.menu', 'click', function (e) {
+        clearMenus()
+        $(this).parent('li').toggleClass('open')
+        return false
+      })
+    })
+  }
+
+})( jQuery || ender )
