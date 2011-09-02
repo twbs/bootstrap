fa94b264fa9b552427a349235e176543af4be6d1
Mark Otto <mark.otto@twitter.com>
2011-09-02 00:50:05 -0700

    add temp second topbar to nav section, fix it's compatibility issues

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -48,10 +48,9 @@ $(document).ready(function(){
   // ===============================
 
   $("body").bind("click", function (e) {
-    $('.dropdown-toggle').parent("li").removeClass("open");
+    $('.dropdown-toggle, .menu').parent("li").removeClass("open");
   });
-
-  $(".dropdown-toggle").click(function (e) {
+  $(".dropdown-toggle, .menu").click(function (e) {
     var $li = $(this).parent("li").toggleClass('open');
     return false;
   });
