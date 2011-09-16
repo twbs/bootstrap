13068f933d85c1a33d9c32d8345723d678164b9f
Jacob Thornton <jacobthornton@gmail.com>
2011-09-16 11:46:13 -0700

    update for 1.3

---
+++ 1.3.0/bootstrap-modal.js
@@ -0,0 +1,227 @@
+/* =========================================================
+ * bootstrap-modal.js v1.3.0
+ * http://twitter.github.com/bootstrap/javascript.html#modal
+ * =========================================================
+ * Copyright 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================= */
+
+
+(function( $ ){
+
+ /* CSS TRANSITION SUPPORT (https://gist.github.com/373874)
+  * ======================================================= */
+
+  var transitionEnd
+
+  $(document).ready(function () {
+
+    $.support.transition = (function () {
+      var thisBody = document.body || document.documentElement
+        , thisStyle = thisBody.style
+        , support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined
+      return support
+    })()
+
+    // set CSS transition event type
+    if ( $.support.transition ) {
+      transitionEnd = "TransitionEnd"
+      if ( $.browser.webkit ) {
+      	transitionEnd = "webkitTransitionEnd"
+      } else if ( $.browser.mozilla ) {
+      	transitionEnd = "transitionend"
+      } else if ( $.browser.opera ) {
+      	transitionEnd = "oTransitionEnd"
+      }
+    }
+
+  })
+
+
+ /* MODAL PUBLIC CLASS DEFINITION
+  * ============================= */
+
+  var Modal = function ( content, options ) {
+    this.settings = $.extend({}, $.fn.modal.defaults)
+    this.$element = $(content)
+      .delegate('.close', 'click.modal', $.proxy(this.hide, this))
+
+    if ( options ) {
+      $.extend( this.settings, options )
+
+      if ( options.show ) {
+        this.show()
+      }
+    }
+
+    return this
+  }
+
+  Modal.prototype = {
+
+      toggle: function () {
+        return this[!this.isShown ? 'show' : 'hide']()
+      }
+
+    , show: function () {
+        var that = this
+        this.isShown = true
+        this.$element.trigger('show')
+
+        escape.call(this)
+        backdrop.call(this, function () {
+          that.$element
+            .appendTo(document.body)
+            .show()
+
+          setTimeout(function () {
+            that.$element
+              .addClass('in')
+              .trigger('shown')
+          }, 1)
+        })
+
+        return this
+      }
+
+    , hide: function (e) {
+        e && e.preventDefault()
+
+        var that = this
+        this.isShown = false
+
+        escape.call(this)
+
+        this.$element
+          .trigger('hide')
+          .removeClass('in')
+
+        function removeElement () {
+          that.$element
+            .hide()
+            .trigger('hidden')
+
+          backdrop.call(that)
+        }
+
+        $.support.transition && this.$element.hasClass('fade') ?
+          this.$element.one(transitionEnd, removeElement) :
+          removeElement()
+
+        return this
+      }
+
+  }
+
+
+ /* MODAL PRIVATE METHODS
+  * ===================== */
+
+  function backdrop ( callback ) {
+    var that = this
+      , animate = this.$element.hasClass('fade') ? 'fade' : ''
+    if ( this.isShown && this.settings.backdrop ) {
+      this.$backdrop = $('<div class="modal-backdrop ' + animate + '" />')
+        .click($.proxy(this.hide, this))
+        .appendTo(document.body)
+
+      setTimeout(function () {
+        that.$backdrop && that.$backdrop.addClass('in')
+        $.support.transition && that.$backdrop.hasClass('fade') ?
+          that.$backdrop.one(transitionEnd, callback) :
+          callback()
+      })
+    } else if ( !this.isShown && this.$backdrop ) {
+      this.$backdrop.removeClass('in')
+
+      function removeElement() {
+        that.$backdrop.remove()
+        that.$backdrop = null
+      }
+
+      $.support.transition && this.$element.hasClass('fade')?
+        this.$backdrop.one(transitionEnd, removeElement) :
+        removeElement()
+    } else if ( callback ) {
+       callback()
+    }
+  }
+
+  function escape() {
+    var that = this
+    if ( this.isShown && this.settings.keyboard ) {
+      $('body').bind('keyup.modal', function ( e ) {
+        if ( e.which == 27 ) {
+          that.hide()
+        }
+      })
+    } else if ( !this.isShown ) {
+      $('body').unbind('keyup.modal')
+    }
+  }
+
+
+ /* MODAL PLUGIN DEFINITION
+  * ======================= */
+
+  $.fn.modal = function ( options ) {
+    var modal = this.data('modal')
+
+    if (!modal) {
+
+      if (typeof options == 'string') {
+        options = {
+          show: /show|toggle/.test(options)
+        }
+      }
+
+      return this.each(function () {
+        $(this).data('modal', new Modal(this, options))
+      })
+    }
+
+    if ( options === true ) {
+      return modal
+    }
+
+    if ( typeof options == 'string' ) {
+      modal[options]()
+    } else if ( modal ) {
+      modal.toggle()
+    }
+
+    return this
+  }
+
+  $.fn.modal.Modal = Modal
+
+  $.fn.modal.defaults = {
+    backdrop: false
+  , keyboard: false
+  , show: true
+  }
+
+
+ /* MODAL DATA- IMPLEMENTATION
+  * ========================== */
+
+  $(document).ready(function () {
+    $('body').delegate('[data-controls-modal]', 'click', function (e) {
+      e.preventDefault()
+      var $this = $(this).data('show', true)
+      $('#' + $this.attr('data-controls-modal')).modal( $this.data() )
+    })
+  })
+
+})( window.jQuery || window.ender )
--- assets/js/application.js
+++ assets/js/application.js
@@ -1,134 +1,52 @@
 $(document).ready(function(){
 
-  // Google code prettify
-  // ====================
-
-  prettyPrint();
-
-
-  // scroll spy logic
-  // ================
-
-  var activeTarget,
-      position = {},
-      $window = $(window),
-      nav = $('body > .topbar li a'),
-      targets = nav.map(function () {
-        return $(this).attr('href');
-      }),
-      offsets = $.map(targets, function (id) {
-        return $(id).offset().top;
-      });
-
-  function setButton(id) {
-    nav.parent("li").removeClass('active');
-    $(nav[$.inArray(id, targets)]).parent("li").addClass('active');
-  }
-
-  function processScroll(e) {
-    var scrollTop = $window.scrollTop() + 10, i;
-    for (i = offsets.length; i--;) {
-      if (activeTarget != targets[i] && scrollTop >= offsets[i] && (!offsets[i + 1] || scrollTop <= offsets[i + 1])) {
-        activeTarget = targets[i];
-        setButton(activeTarget);
-      }
-    }
-  }
-
-  nav.click(function () {
-    processScroll();
-  });
-
-  processScroll();
-
-  $window.scroll(processScroll);
-
-
-  // Dropdown example for topbar nav
-  // ===============================
-
-  $("body").bind("click", function (e) {
-    $('.dropdown-toggle, .menu').parent("li").removeClass("open");
-  });
-  $(".dropdown-toggle, .menu").click(function (e) {
-    var $li = $(this).parent("li").toggleClass('open');
-    return false;
-  });
-
-
   // table sort example
   // ==================
 
-  $("#sortTableExample").tablesorter( {sortList: [[1,0]]} );
+  $("#sortTableExample").tablesorter( { sortList: [[ 1, 0 ]] } )
 
 
   // add on logic
   // ============
 
-  $('.add-on :checkbox').click(function() {
+  $('.add-on :checkbox').click(function () {
     if ($(this).attr('checked')) {
-      $(this).parents('.add-on').addClass('active');
+      $(this).parents('.add-on').addClass('active')
     } else {
-      $(this).parents('.add-on').removeClass('active');
+      $(this).parents('.add-on').removeClass('active')
     }
-  });
+  })
 
 
   // Disable certain links in docs
   // =============================
+  // Please do not carry these styles over to your projects, it's merely here to prevent button clicks form taking you away from your spot on page
 
-  $('ul.tabs a, ul.pills a, .pagination a, .well .btn, .actions .btn, .alert-message .btn, a.close').click(function(e) {
-    e.preventDefault();
-  });
+  $('ul.tabs a, ul.pills a, .pagination a, .well .btn, .actions .btn, .alert-message .btn, a.close').click(function (e) {
+    e.preventDefault()
+  })
 
   // Copy code blocks in docs
-  $(".copy-code").focus(function() {
+  $(".copy-code").focus(function () {
     var el = this;
     // push select to event loop for chrome :{o
     setTimeout(function () { $(el).select(); }, 1);
   });
 
 
-  // POSITION TWIPSIES
-  // =================
+  // POSITION STATIC TWIPSIES
+  // ========================
 
-  $('.twipsies.well a').each(function () {
-    var type = this.title
-      , $anchor = $(this)
-      , $twipsy = $('.twipsy.' + type)
-
-      , twipsy = {
-          width: $twipsy.width() + 10
-        , height: $twipsy.height() + 10
-        }
-
-      , anchor = {
-          position: $anchor.position()
-        , width: $anchor.width()
-        , height: $anchor.height()
-        }
-
-      , offset = {
-          above: {
-            top: anchor.position.top - twipsy.height
-          , left: anchor.position.left + (anchor.width/2) - (twipsy.width/2)
-          }
-        , below: {
-            top: anchor.position.top + anchor.height
-          , left: anchor.position.left + (anchor.width/2) - (twipsy.width/2)
-          }
-        , left: {
-            top: anchor.position.top + (anchor.height/2) - (twipsy.height/2)
-          , left: anchor.position.left - twipsy.width - 5
-          }
-        , right: {
-            top: anchor.position.top + (anchor.height/2) - (twipsy.height/2)
-          , left: anchor.position.left + anchor.width + 5
-          }
-      }
-
-    $twipsy.css(offset[type])
-
-  });
-
+  $(window).bind( 'load resize', function () {
+    $(".twipsies a").each(function () {
+       $(this)
+        .twipsy({
+          live: false
+        , placement: $(this).attr('title')
+        , trigger: 'manual'
+        , offset: 2
+        })
+        .twipsy('show')
+      })
+  })
 });
