92616e0187e83782ab9f529795e0d6eb7fe0358c
Jacob Thornton <jacobthornton@gmail.com>
2011-10-04 21:27:47 -0700

    update js

--- 1.3.0/bootstrap-dropdown.js
+++ 1.3.0/bootstrap-dropdown.js
@@ -20,17 +20,6 @@
 
 !function( $ ){
 
-  var d = 'a.menu, .dropdown-toggle'
-
-  function clearMenus() {
-    $(d).parent('li').removeClass('open')
-  }
-
-  $(function () {
-    $('html').bind("click", clearMenus)
-    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' )
-  })
-
   /* DROPDOWN PLUGIN DEFINITION
    * ========================== */
 
@@ -46,5 +35,19 @@
       })
     })
   }
+
+  /* APPLY TO STANDARD DROPDOWN ELEMENTS
+   * =================================== */
+
+  var d = 'a.menu, .dropdown-toggle'
+
+  function clearMenus() {
+    $(d).parent('li').removeClass('open')
+  }
+
+  $(function () {
+    $('html').bind("click", clearMenus)
+    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' )
+  })
 
 }( window.jQuery || window.ender );
