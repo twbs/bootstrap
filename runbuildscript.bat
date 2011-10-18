e8ae79e53d0845d1b8c33b7ef51c53351df24ee6
localpcguy <mike@local-pc-guy.com>
2011-10-18 19:37:30 -0300

    Use REM for comments to avoid error messages on the DOS prompt for Windows users

--- runbuildscript.bat
+++ runbuildscript.bat
@@ -1,5 +1,5 @@
-# This is for windows users only.
-# If you're on a mac or linux, just run `ant build` from this folder in Terminal
+REM # This is for windows users only.
+REM # If you're on a mac or linux, just run `ant build` from this folder in Terminal
 
 set MYDIR=%~dp0
 set ANT_OPTS=-D"file.encoding=UTF-8"
