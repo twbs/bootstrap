bd8745a98b93fb55e4c1c08a34e97bd30a16798c
Jacob Thornton <jacobthornton@gmail.com>
2011-11-25 17:30:52 -0800

    allow scrollspy to target a specific nav

--- js/bootstrap-scrollspy.js
+++ js/bootstrap-scrollspy.js
@@ -26,10 +26,10 @@
 
   function ScrollSpy( element ) {
     var process = $.proxy(this.process, this)
-    this.selector = '.nav li > a'
 
-    this.$body = $('body').delegate(this.selector, 'click.scroll.data-api', process)
     this.$scrollElement = $(element).bind('scroll.scroll.data-api', process)
+    this.selector = (this.$scrollElement.attr('data-target') || '') + ' .nav li > a'
+    this.$body = $('body').delegate(this.selector, 'click.scroll.data-api', process)
 
     this.refresh()
     this.process()
