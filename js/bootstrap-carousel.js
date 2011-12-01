dc628c9115b74e49940c0cc200a8914232c9d2a2
Jacob Thornton <jacobthornton@gmail.com>
2011-11-30 22:05:09 -0800

    start of carousel

---
+++ js/bootstrap-carousel.js
@@ -0,0 +1,59 @@
+/* ==========================================================
+ * bootstrap-carousel.js v2.0.0
+ * http://twitter.github.com/bootstrap/javascript.html#alerts
+ * ==========================================================
+ * Copyright 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+
+
+!function( $ ){
+
+  "use strict"
+
+ /* CAROUSEL CLASS DEFINITION
+  * ========================= */
+
+  var Carousel = function ( el ) {
+    $(el).delegate(dismiss, 'click', this.close)
+  }
+
+  Carousel.prototype = {
+
+  }
+
+
+ /* CAROUSEL PLUGIN DEFINITION
+  * ========================== */
+
+  $.fn.carousel = function ( option ) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('alert')
+      if (!data) $this.data('alert', (data = new Alert(this)))
+      if (typeof option == 'string') data[option].call($this)
+    })
+  }
+
+  $.fn.carousel.Carousel = Carousel
+
+
+ /* CAROUSEL DATA-API
+  * ================= */
+
+  // $(function () {
+  //   $('body').delegate(dismiss, 'click.alert.data-api', Alert.prototype.close)
+  // })
+
+}( window.jQuery || window.ender )
