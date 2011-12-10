a3a2d0c7dd62be8dbc09069843c6cf9d33f552cc
elexx <fal3xx@gmail.com>
2011-12-10 13:13:39 +0100

    Removed test from copy, because the directory has been removed long ago (0a80ebafd0).

--- createproject.sh
+++ createproject.sh
@@ -68,7 +68,7 @@ else
     echo "Created Directory: $dst"
 
     cd -- "$src"
-    cp -vr -- css js img build test *.html *.xml *.txt *.png *.ico .htaccess "$dst"
+    cp -vr -- css js img build *.html *.xml *.txt *.png *.ico .htaccess "$dst"
 
     #success message
     echo "Created Project: $dst"
