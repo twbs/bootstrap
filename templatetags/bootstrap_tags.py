ea7ab4d0b540ac3be00f7fdc65061ca8fc658244
Pete Gadomski <pete.gadomski@gmail.com>
2011-12-20 21:58:39 -0800

    Allow complete hiding of hidden fields

--- templatetags/bootstrap_tags.py
+++ templatetags/bootstrap_tags.py
@@ -4,14 +4,15 @@ register = template.Library()
 
 
 @register.inclusion_tag("bootstrap_field.html")
-def bootstrap_field(field, class_=None):
+def bootstrap_field(field, class_=None, label=True):
     input_ = field.as_widget(attrs={'class': class_})
-    label = field.label_tag() if not field.is_hidden else ''
+    label = field.label_tag() if not field.is_hidden and label else ''
     help_text = field.help_text
     wrapper_class = 'error' if field.errors else ''
     errors = ' '.join(field.errors)
     return {'label': label, 'input': input_, 'help_text': help_text,
-            'wrapper_class': wrapper_class, 'errors': errors}
+            'wrapper_class': wrapper_class, 'errors': errors,
+            'hidden': field.is_hidden}
 
 
 @register.inclusion_tag("bootstrap_form.html")
