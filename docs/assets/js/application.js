09df780b7512b1e501aeed3ec3463470e33952aa
Jacob Thornton <jacobthornton@gmail.com>
2012-01-28 00:46:04 -0800

    clean up application js a bit

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -6,7 +6,6 @@
 
   $(function(){
 
-
     // Disable certain links in docs
     $('section [href^=#]').click(function (e) {
       e.preventDefault()
@@ -28,34 +27,29 @@
       $(this).parents('.add-on')[method]('active')
     })
 
-    if ($.fn.tooltip) {
-
-      // position static twipsies for components page
-      if ($(".twipsies a").length) {
-        $(window).on('load resize', function () {
-          $(".twipsies a").each(function () {
-            $(this)
-              .tooltip({
-                placement: $(this).attr('title')
-              , trigger: 'manual'
-              })
-              .tooltip('show')
+    // position static twipsies for components page
+    if ($(".twipsies a").length) {
+      $(window).on('load resize', function () {
+        $(".twipsies a").each(function () {
+          $(this)
+            .tooltip({
+              placement: $(this).attr('title')
+            , trigger: 'manual'
             })
-        })
-      }
-
-      // add tipsies to grid for scaffolding
-      if ($('#grid-system').length) {
-
-        $('#grid-system').tooltip({
-            selector: '.show-grid > div'
-          , title: function () { return $(this).width() + 'px' }
-        })
-
-      }
+            .tooltip('show')
+          })
+      })
     }
 
-    // fix sub nav playa
+    // add tipsies to grid for scaffolding
+    if ($('#grid-system').length) {
+      $('#grid-system').tooltip({
+          selector: '.show-grid > div'
+        , title: function () { return $(this).width() + 'px' }
+      })
+    }
+
+    // fix sub nav on scroll
     var $win = $(window)
       , $nav = $('.subnav')
       , navTop = $('.subnav').length && $('.subnav').offset().top - 40
@@ -76,136 +70,123 @@
       }
     }
 
+    // tooltip demo
+    $('.tooltip-demo.well').tooltip({
+      selector: "a[rel=tooltip]"
+    })
+
+    $('.tooltip-test').tooltip({
+      'z-index': 3000
+    })
+
+    $('.popover-test').popover({
+      'z-index': 3000
+    })
+
+    // popover demo
+    $("a[rel=popover]")
+      .popover()
+      .click(function(e) {
+        e.preventDefault()
+      })
+
+    // button state demo
+    $('#fat-btn')
+      .click(function () {
+        var btn = $(this)
+        btn.button('loading')
+        setTimeout(function () {
+          btn.button('reset')
+        }, 3000)
+      })
+
+    // carousel demo
+    $('#myCarousel').carousel()
+
+    // javascript build logic
+    var inputsComponent = $("#components.download input")
+      , inputsPlugin = $("#plugins.download input")
+      , inputsVariables = $("#variables.download input")
+
+    // toggle all plugin checkboxes
+    $('#components.download .toggle-all').on('click', function (e) {
+      e.preventDefault()
+      inputsComponent.attr('checked', !inputsComponent.is(':checked'))
+    })
+
+    $('#plugins.download .toggle-all').on('click', function (e) {
+      e.preventDefault()
+      inputsPlugin.attr('checked', !inputsPlugin.is(':checked'))
+    })
+
+    $('#variables.download .toggle-all').on('click', function (e) {
+      e.preventDefault()
+      inputsVariables.val('')
+    })
+
+    // request built javascript
+    $('.download-btn').on('click', function () {
+
+      var css = $("#components.download input:checked")
+            .map(function () { return this.value })
+            .toArray()
+        , js = $("#plugins.download input:checked")
+            .map(function () { return this.value })
+            .toArray()
+        , vars = {}
+        , img = ['glyphicons-halflings-sprite.png', 'glyphicons-halflings-sprite-white.png']
+
+    $("#variables.download input")
+      .each(function () {
+        $(this).val() && (vars[ $(this).prev().text() ] = $(this).val())
+      })
+
+      $.ajax({
+        type: 'POST'
+      , url: 'http://bootstrap.herokuapp.com'
+      , dataType: 'jsonpi'
+      , params: {
+          branch: '2.0-wip'
+        , js: js
+        , css: css
+        , vars: vars
+        , img: img
+      }
+      })
+    })
+
+  })
+
+// Modified from the original jsonpi https://github.com/benvinegar/jquery-jsonpi
+$.ajaxTransport('jsonpi', function(opts, originalOptions, jqXHR) {
+  var url = opts.url;
+
+  return {
+    send: function(_, completeCallback) {
+      var name = 'jQuery_iframe_' + jQuery.now()
+        , iframe, form
+
+      iframe = $('<iframe>')
+        .attr('name', name)
+        .appendTo('head')
+
+      form = $('<form>')
+        .attr('method', opts.type) // GET or POST
+        .attr('action', url)
+        .attr('target', name)
+
+      $.each(opts.params, function(k, v) {
+
+        $('<input>')
+          .attr('type', 'hidden')
+          .attr('name', k)
+          .attr('value', typeof v == 'string' ? v : JSON.stringify(v))
+          .appendTo(form)
+      })
+
+      form.appendTo('body').submit()
+    }
+  }
 })
-
-// JS for javascript demos
-// $(function () {
-//   // tooltip demo
-//   $('.tooltip-demo.well').tooltip({
-//     selector: "a[rel=tooltip]"
-//   })
-
-//   $('.tooltip-test').tooltip({
-//     'z-index': 3000
-//   })
-
-//   $('.popover-test').popover({
-//     'z-index': 3000
-//   })
-
-//   // popover demo
-//   $("a[rel=popover]")
-//     .popover()
-//     .click(function(e) {
-//       e.preventDefault()
-//     })
-
-//   // button state demo
-//   $('#fat-btn')
-//     .click(function () {
-//       var btn = $(this)
-//       btn.button('loading')
-//       setTimeout(function () {
-//         btn.button('reset')
-//       }, 3000)
-//     })
-
-//   // carousel demo
-//   $('#myCarousel').carousel()
-
-// })
-
-
-// // Modified from the original jsonpi https://github.com/benvinegar/jquery-jsonpi
-// // by the talented Ben Vinegar
-// !function($) {
-//   $.ajaxTransport('jsonpi', function(opts, originalOptions, jqXHR) {
-//     var url = opts.url;
-
-//     return {
-//       send: function(_, completeCallback) {
-//         var name = 'jQuery_iframe_' + jQuery.now()
-//           , iframe, form
-
-//         iframe = $('<iframe>')
-//           .attr('name', name)
-//           .appendTo('head')
-
-//         form = $('<form>')
-//           .attr('method', opts.type) // GET or POST
-//           .attr('action', url)
-//           .attr('target', name)
-
-//         $.each(opts.params, function(k, v) {
-
-//           $('<input>')
-//             .attr('type', 'hidden')
-//             .attr('name', k)
-//             .attr('value', typeof v == 'string' ? v : JSON.stringify(v))
-//             .appendTo(form)
-//         })
-
-//         form.appendTo('body').submit()
-//       }
-//     }
-//   })
-// }(jQuery);
-
-//  // javascript build logic
-
-// $(function () {
-
-//   var inputsComponent = $("#components.download input")
-//     , inputsPlugin = $("#plugins.download input")
-//     , inputsVariables = $("#variables.download input")
-
-//   // toggle all plugin checkboxes
-//   $('#components.download .toggle-all').on('click', function (e) {
-//     e.preventDefault()
-//     inputsComponent.attr('checked', !inputsComponent.is(':checked'))
-//   })
-
-//   $('#plugins.download .toggle-all').on('click', function (e) {
-//     e.preventDefault()
-//     inputsPlugin.attr('checked', !inputsPlugin.is(':checked'))
-//   })
-
-//   $('#variables.download .toggle-all').on('click', function (e) {
-//     e.preventDefault()
-//     inputsVariables.val('')
-//   })
-
-//   // request built javascript
-//   $('.download-btn').on('click', function () {
-
-//     var css = $("#components.download input:checked")
-//           .map(function () { return this.value })
-//           .toArray()
-//       , js = $("#plugins.download input:checked")
-//           .map(function () { return this.value })
-//           .toArray()
-//       , vars = {}
-//       , img = ['glyphicons-halflings-sprite.png', 'glyphicons-halflings-sprite-white.png']
-
-//   $("#variables.download input")
-//     .each(function () {
-//       $(this).val() && (vars[ $(this).prev().text() ] = $(this).val())
-//     })
-
-//     $.ajax({
-//       type: 'POST'
-//     , url: 'http://bootstrap.herokuapp.com'
-//     , dataType: 'jsonpi'
-//     , params: {
-//         branch: '2.0-wip'
-//       , js: js
-//       , css: css
-//       , vars: vars
-//       , img: img
-//     }
-//     })
-//   })
-
-// }) 
 
 }(window.jQuery)
