0cec075f64ab1b6a44b178e68f03258daebca650
Jorge del Casar <jorge.casar@gmail.com>
2012-02-07 17:56:50 +0100

    Carousel paginator
    Display carousel pages in the top right corner

--- js/bootstrap-carousel.js
+++ js/bootstrap-carousel.js
@@ -127,7 +127,9 @@
         , options = typeof option == 'object' && option
       if (!data) $this.data('carousel', (data = new Carousel(this, options)))
       if (typeof option == 'number') data.to(option)
-      else if (typeof option == 'string' || (option = options.slide)) data[option]()
+      else if (typeof option == 'string' || (option = options.slide))
+        if (typeof option == 'number') data.to(option)
+        else data[option]()
       else data.cycle()
     })
   }
