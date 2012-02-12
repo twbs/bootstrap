5b4a29d84e6290a93db98da34b89776a9740e455
Jacob Thornton <jacobthornton@gmail.com>
2012-02-12 11:15:12 -0800

    honor data attributes set on modal as well

--- docs/assets/js/bootstrap-modal.js
+++ docs/assets/js/bootstrap-modal.js
@@ -177,7 +177,7 @@
     return this.each(function () {
       var $this = $(this)
         , data = $this.data('modal')
-        , options = $.extend({}, $.fn.modal.defaults, typeof option == 'object' && option)
+        , options = $.extend({}, $.fn.modal.defaults, $this.data(), typeof option == 'object' && option)
       if (!data) $this.data('modal', (data = new Modal(this, options)))
       if (typeof option == 'string') data[option]()
       else if (options.show) data.show()
