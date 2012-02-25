43760eb2029c73b6be6b0dbeb753be1e247697d9
Jacob Thornton <jacobthornton@gmail.com>
2012-02-24 22:17:36 -0800

    fix for @pamelafox and #1861

--- js/bootstrap-tooltip.js
+++ js/bootstrap-tooltip.js
@@ -206,7 +206,7 @@
       title = $e.attr('data-original-title')
         || (typeof o.title == 'function' ? o.title.call($e[0]) :  o.title)
 
-      title = title.toString().replace(/(^\s*|\s*$)/, "")
+      title = (title || '').toString().replace(/(^\s*|\s*$)/, "")
 
       return title
     }
