a64bdd2555f0f6035f3f03643f7e0e77f46a60d5
Matthew Urtnowski <Murtnowski@gmail.com>
2012-03-08 15:20:39 -0800

    Resolved Issue #2043
    Added variable focusshow which defaults to false
    
    When focusshow is true, it makes it so when you gain focus on typeahead it causes the menu to show up.
    
    I added a focus listener to catch when the typeahead gains focus
    I added a method called showmenu when will show then menu when typeahead gains focus

--- js/bootstrap-typeahead.js
+++ js/bootstrap-typeahead.js
@@ -4,7 +4,7 @@
  * =============================================================
  * Copyright 2012 Twitter, Inc.
  *
- * Licensed under the Apaache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
