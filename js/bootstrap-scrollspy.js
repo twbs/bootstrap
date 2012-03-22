706ee46b73b58059230b24589160da784778aad2
Jacob Thornton <jacobthornton@gmail.com>
2012-03-21 23:45:58 -0700

    sort scrollspy to prevent flicker for unordered nav

--- js/bootstrap-scrollspy.js
+++ js/bootstrap-scrollspy.js
@@ -43,16 +43,25 @@
       constructor: ScrollSpy
 
     , refresh: function () {
-        this.targets = this.$body
+        var self = this
+          , $targets
+
+        this.offsets = []
+        this.targets = []
+
+        $targets = this.$body
           .find(this.selector)
           .map(function () {
             var href = $(this).attr('href')
-            return /^#\w/.test(href) && $(href).length ? href : null
+            return /^#\w/.test(href)
+              && $(href).length
+              && [[ $(href).position().top, href ]]
           })
-
-        this.offsets = $.map(this.targets, function (id) {
-          return $(id).position().top
-        })
+          .sort(function (a, b) { return a[0] - b[0] })
+          .each(function () {
+            self.offsets.push(this[0])
+            self.targets.push(this[1])
+          })
       }
 
     , process: function () {
