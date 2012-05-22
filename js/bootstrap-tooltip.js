a2c513b07cf32cf7664b2fb9a40caae3bfffbb19
Andrew Hubbs <andrew.hubbs@rally.org>
2012-05-22 13:57:23 -0700

    Fix error where IE 9 will return null for offsetHeight/offsetWidth when the element is an SVG.

--- js/bootstrap-tooltip.js
+++ js/bootstrap-tooltip.js
@@ -200,8 +200,8 @@
 
   , getPosition: function (inside) {
       return $.extend({}, (inside ? {top: 0, left: 0} : this.$element.offset()), {
-        width: this.$element[0].offsetWidth
-      , height: this.$element[0].offsetHeight
+        width: (this.$element[0].offsetWidth || 0)
+      , height: (this.$element[0].offsetHeight || 0)
       })
     }
 
