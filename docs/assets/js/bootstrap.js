5b401a8c16bf54aad4df38790d20f05bfea03822
Jacob Thornton <jacobthornton@gmail.com>
2012-06-02 17:28:38 -0700

    add collapsed class to collapse invoker when it's target is collapsed #3525

--- docs/assets/js/bootstrap.js
+++ docs/assets/js/bootstrap.js
@@ -562,6 +562,7 @@
           || e.preventDefault()
           || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
         , option = $(target).data('collapse') ? 'toggle' : $this.data()
+      $this[$(target).hasClass('in') ? 'removeClass' : 'addClass']('show')
       $(target).collapse(option)
     })
   })
--- js/bootstrap-collapse.js
+++ js/bootstrap-collapse.js
@@ -144,12 +144,13 @@
   * ==================== */
 
   $(function () {
-    $('body').on('click.collapse.data-api', '[data-toggle=collapse]', function ( e ) {
+    $('body').on('click.collapse.data-api', '[data-toggle=collapse]', function (e) {
       var $this = $(this), href
         , target = $this.attr('data-target')
           || e.preventDefault()
           || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
         , option = $(target).data('collapse') ? 'toggle' : $this.data()
+      $this[$(target).hasClass('in') ? 'addClass' : 'removeClass']('collapsed')
       $(target).collapse(option)
     })
   })
