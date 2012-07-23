c8015f24b366924a534fde22c4058972bf6ea068
Vizeroth <mwhitney@mail.sdsu.edu>
2012-07-22 19:31:18 -0700

    Add unit test for numeric data-slide value

--- js/tests/unit/bootstrap-carousel.js
+++ js/tests/unit/bootstrap-carousel.js
@@ -39,4 +39,18 @@ $(function () {
           .carousel('next')
       })
 
+
+      test("should set active on slide with given numeric index", function () {
+        var carouselHtml = '<div id="myCarousel" class="carousel">'
+          + '<div class="carousel-inner">'
+          + '<div class="active item"/>'
+          + '<div class="item"/>'
+          + '</div>'
+          + '<a class="carousel-control" href="#myCarousel" data-slide="1">1</a>'
+          + '</div>';
+    	$('#qunit-fixture').append(carouselHtml);
+        $('#myCarousel').carousel();
+        $('#myCarousel').find('.carousel-control').click();
+        ok($('#myCarousel').find('.item').eq($('#myCarousel').find('.carousel-control').data('slide')).hasClass('active'));
+      })
 })
