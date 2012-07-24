c10a357bb3aa75a0e1252f3bd17aa8a0476400a3
Jacob Thornton <jacobthornton@gmail.com>
2012-07-24 12:57:00 -0700

    don't bite so hard

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -6,17 +6,6 @@
 
   $(function(){
 
-    // fancy fade jumbotron
-
-    var $jumbotron  = $('.jumbotron')
-      , $jcontainer = $('.jumbotron .container')
-      , opacRatio   = ($jumbotron.height() / 200 ) * 2
-
-    $(window).on('scroll', function () {
-      var diff = 100 - ($(window).scrollTop() / opacRatio)
-      $jcontainer.css({ opacity: (diff > 0 ? Math.min(diff, 100) : 0) / 100 })
-    })
-
     // Disable certain links in docs
     $('section [href^=#]').click(function (e) {
       e.preventDefault()
