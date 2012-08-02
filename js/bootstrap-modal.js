7c6392329b0494fb105259b3ef0a18bc8e68e3f1
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-08-03 00:34:12 +0100

    Allow passing data arguments to modal (fixes #531)
    Implements @bronson's suggestion, all credit to him.

--- js/bootstrap-modal.js
+++ js/bootstrap-modal.js
@@ -201,6 +201,7 @@
         , data = $this.data('modal')
         , options = $.extend({}, $.fn.modal.defaults, $this.data(), typeof option == 'object' && option)
       if (!data) $this.data('modal', (data = new Modal(this, options)))
+      else $.extend($this.data('modal').options, option)
       if (typeof option == 'string') data[option]()
       else if (options.show) data.show()
     })
@@ -232,6 +233,9 @@
       , href = $this.attr('href')
       , $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))) //strip for ie7
       , option = $target.data('modal') ? 'toggle' : $.extend({ remote:!/#/.test(href) && href }, $target.data(), $this.data())
+    if(option == 'toggle') {
+      $.extend($target.data('modal').options, $this.data())
+    }
 
     e.preventDefault()
 
