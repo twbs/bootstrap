7fd72958ac39810aacb4a93a1c37b4a490f4c491
Sean Stephenson <seanmichaelstephenson@gmail.com>
2012-08-02 21:58:17 -0700

    remove role assignment from tab

--- js/bootstrap-tab.js
+++ js/bootstrap-tab.js
@@ -129,13 +129,7 @@
     $('body').on('click.tab.data-api', '[data-toggle="tab"], [data-toggle="pill"]', function (e) {
       e.preventDefault()
       $(this).tab('show')
-      e.target.focus();
     })
-
-    $('body').on('focusin.[data-toggle="tab"]', function (e) {
-      $(e.target).attr('role', 'tab');
-    })
-
   })
 
 }(window.jQuery);
