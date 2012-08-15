dee57462e2805421a0fe0d786229446bbaef677b
Jacob Thornton <jacobthornton@gmail.com>
2012-08-14 21:06:08 -0700

    get affix actually working and update docs

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -6,9 +6,19 @@
 
   $(function(){
 
+    var $window = $(window)
+
     // Disable certain links in docs
     $('section [href^=#]').click(function (e) {
       e.preventDefault()
+    })
+
+    // side bar
+    $('.bs-docs-sidenav').affix({
+      offset: {
+        top: function () { return $window.width() <= 980 ? 290 : 210 }
+      , bottom: 270
+      }
     })
 
     // make code pretty
