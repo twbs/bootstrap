73abfd5db86cc0d4f5d510edb90d2854141aaf6a
teo <teo.sibileau@gmail.com>
2012-08-29 16:48:28 -0300

    cambiando ssh por https

--- create_mirror_clone.sh
+++ create_mirror_clone.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-test -d ../bootstrap-gh-pages || (cd .. && git clone git@github.com:gcba/BAstrap.git bootstrap-gh-pages && cd bootstrap-gh-pages && git checkout gh-pages && cd ..)
+test -d ../bootstrap-gh-pages || (cd .. && git clone https://github.com/gcba/BAstrap.git bootstrap-gh-pages && cd bootstrap-gh-pages && git checkout gh-pages && cd ..)
