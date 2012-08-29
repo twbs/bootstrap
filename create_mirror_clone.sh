e99575ffbb460888ca08df27c79a5754d89e112d
teo <teo.sibileau@gmail.com>
2012-08-29 17:37:18 -0300

    deleting ugly ash scripts

--- create_mirror_clone.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-test -d ../bootstrap-gh-pages || (cd .. && git clone https://github.com/gcba/BAstrap.git bootstrap-gh-pages && cd bootstrap-gh-pages && git checkout gh-pages && cd ..)
--- push_to_mirror_clone.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-export ts=`date +%s` && cd ../bootstrap-gh-pages && git add . -A && git commit -m "gh-pages $ts" && git push origin gh-pages
