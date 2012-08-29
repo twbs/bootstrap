5146e96173dd8b4bf18e3ed69058ebfed6c3cad5
Prathan Thananart <semicomma@gmail.com>
2012-08-29 10:27:23 +0700

    Allow ScrollSpy to match non-latin anchors
    Currently it's using `\w` to test link validity. ScrollSpy shouldn't
    be in the business of specifying which anchors are valid or not. In
    this case anchors like `#^_^` or `#香港` will fail even though browser
    recognizes them just fine.

--- js/bootstrap-scrollspy.js
+++ js/bootstrap-scrollspy.js
@@ -56,7 +56,7 @@
           .map(function () {
             var $el = $(this)
               , href = $el.data('target') || $el.attr('href')
-              , $href = /^#\w/.test(href) && $(href)
+              , $href = /^#.+/.test(href) && $(href)
             return ( $href
               && $href.length
               && [[ $href.position().top, href ]] ) || null
