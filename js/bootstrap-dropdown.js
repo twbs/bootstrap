f1a1ec7c15af5ad082109939348c299318580100
Aleksandar Totic <a@totic.org>
2012-09-20 15:54:42 -0700

    Update js/bootstrap-dropdown.js
    Problem:
    Firefox & jquery 1.8.1 will issue a warning on every click event:
    Warning: Empty string passed to getElementById().
    I try to run without warnings, so this bugs me.
    
    Fix:
    do not query with '#' selector.

--- js/bootstrap-dropdown.js
+++ js/bootstrap-dropdown.js
@@ -113,7 +113,7 @@
       selector = selector && /#/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
     }
 
-    $parent = $(selector)
+    $parent = selector == '#' ? $() : $(selector)
     $parent.length || ($parent = $this.parent())
 
     return $parent
