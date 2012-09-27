c1186ca1c0aa462f3c514c41ff354a7105350d17
jschr <jordan.schroter@gmail.com>
2012-09-27 11:11:23 -0300

    Update js/bootstrap-alert.js
    Use triggerHandler instead of trigger for custom events

--- js/bootstrap-alert.js
+++ js/bootstrap-alert.js
@@ -47,7 +47,7 @@
 
     $parent.length || ($parent = $this.hasClass('alert') ? $this : $this.parent())
 
-    $parent.trigger(e = $.Event('close'))
+    $parent.triggerHandler(e = $.Event('close'))
 
     if (e.isDefaultPrevented()) return
 
@@ -55,7 +55,7 @@
 
     function removeElement() {
       $parent
-        .trigger('closed')
+        .triggerHandler('closed')
         .remove()
     }
 
