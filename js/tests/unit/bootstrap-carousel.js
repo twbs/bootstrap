5855e383cfd3382f7de963c3233394da6588a9ec
M. Auayan <sxtxixtxcxh@gmail.com>
2012-10-01 12:44:37 -0700

    fix empty relatedTarget in 'slide' event on carousel

--- js/tests/unit/bootstrap-carousel.js
+++ js/tests/unit/bootstrap-carousel.js
@@ -2,6 +2,8 @@ $(function () {
 
     module("bootstrap-carousel")
 
+      var template = '<div id="myCarousel" class="carousel slide"><div class="carousel-inner"><div class="item active"><img alt=""><div class="carousel-caption"><h4>{{_i}}First Thumbnail label{{/i}}</h4><p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p></div></div><div class="item"><img alt=""><div class="carousel-caption"><h4>{{_i}}Second Thumbnail label{{/i}}</h4><p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p></div></div><div class="item"><img alt=""><div class="carousel-caption"><h4>{{_i}}Third Thumbnail label{{/i}}</h4><p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p></div></div></div><a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a><a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a></div>'
+
       test("should be defined on jquery object", function () {
         ok($(document.body).carousel, 'carousel method is defined')
       })
@@ -26,7 +28,6 @@ $(function () {
       })
 
       test("should fire slide event with relatedTarget", function () {
-        var template = '<div id="myCarousel" class="carousel slide"><div class="carousel-inner"><div class="item active"><img alt=""><div class="carousel-caption"><h4>{{_i}}First Thumbnail label{{/i}}</h4><p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p></div></div><div class="item"><img alt=""><div class="carousel-caption"><h4>{{_i}}Second Thumbnail label{{/i}}</h4><p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p></div></div><div class="item"><img alt=""><div class="carousel-caption"><h4>{{_i}}Third Thumbnail label{{/i}}</h4><p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p></div></div></div><a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a><a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a></div>'
         $.support.transition = false
         stop()
         $(template)
@@ -39,4 +40,16 @@ $(function () {
           .carousel('next')
       })
 
+      test("should fire slide event with relatedTarget when hitting bounds", function () {
+        $.support.transition = false
+        stop()
+        $(template)
+          .on('slide', function (e) {
+            e.preventDefault();
+            ok(e.relatedTarget);
+            ok($(e.relatedTarget).hasClass('item'));
+            start();
+          })
+          .carousel('prev')
+      })
 })
