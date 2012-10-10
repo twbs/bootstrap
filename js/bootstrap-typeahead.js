c324c670864b882b994d2ee1028c29bbc96cee91
andresmoschini <andresmoschini@gmail.com>
2012-10-10 16:46:00 -0300

    Allow to not autoselect typeahead suggestions

--- js/bootstrap-typeahead.js
+++ js/bootstrap-typeahead.js
@@ -45,9 +45,11 @@
 
   , select: function () {
       var val = this.$menu.find('.active').attr('data-value')
-      this.$element
-        .val(this.updater(val))
-        .change()
+      if (val) {
+        this.$element
+          .val(this.updater(val))
+          .change()
+      }
       return this.hide()
     }
 
@@ -141,7 +143,10 @@
         return i[0]
       })
 
-      items.first().addClass('active')
+      if (this.options.autoselect) {
+        items.first().addClass('active')
+      }
+
       this.$menu.html(items)
       return this
     }
@@ -280,6 +285,7 @@
   , menu: '<ul class="typeahead dropdown-menu"></ul>'
   , item: '<li><a href="#"></a></li>'
   , minLength: 1
+  , autoselect: true
   }
 
   $.fn.typeahead.Constructor = Typeahead
