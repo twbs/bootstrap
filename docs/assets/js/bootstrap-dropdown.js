3b3dd3ac3c7b69d02406ede69bffcc4ee8a1ed6b
fat <jacobthornton@gmail.com>
2012-10-30 21:34:39 -0700

    2.2.1 - hotfix for carousel

--- docs/assets/js/bootstrap-dropdown.js
+++ docs/assets/js/bootstrap-dropdown.js
@@ -1,5 +1,5 @@
 /* ============================================================
- * bootstrap-dropdown.js v2.2.0
+ * bootstrap-dropdown.js v2.2.1
  * http://twitter.github.com/bootstrap/javascript.html#dropdowns
  * ============================================================
  * Copyright 2012 Twitter, Inc.
