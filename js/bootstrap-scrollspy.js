778ecbbfddda4cefd69857a30503cc5f751bff69
fat <jacobthornton@gmail.com>
2012-10-30 21:34:39 -0700

    2.1.1 - hotfix for carousel

--- js/bootstrap-scrollspy.js
+++ js/bootstrap-scrollspy.js
@@ -1,5 +1,5 @@
 /* =============================================================
- * bootstrap-scrollspy.js v2.2.0
+ * bootstrap-scrollspy.js v2.2.1
  * http://twitter.github.com/bootstrap/javascript.html#scrollspy
  * =============================================================
  * Copyright 2012 Twitter, Inc.
