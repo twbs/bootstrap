fad3e2ccaad642452a6876fc75fc88bc06f95d7d
William Stam <awstam@gmail.com>
2012-11-07 15:09:57 +0200

    hopefully this will fix #5687
    added a container option so either use the html data api
    
    ```
    data-container="parent"
    data-container="body"
    data-container="#some_element"
    ```
    
    or the javascript
    
    ```
    {
    container: "body"
    }
    ```
    added 3 tests to the tool tip aswell to test if the tooltip is being added to the right places

--- js/bootstrap-tooltip.js
+++ js/bootstrap-tooltip.js
@@ -68,7 +68,7 @@
         }
       }
 
-      return options
+		return options
     }
 
   , enter: function (e) {
@@ -121,7 +121,26 @@
         $tip
           .detach()
           .css({ top: 0, left: 0, display: 'block' })
-          .insertAfter(this.$element)
+
+
+	      switch (this.options.container||'parent') {
+		      case "parent":
+			      $tip.insertAfter(this.$element);
+			      break;
+		      case "body":
+			      console.log("body")
+			      $tip.appendTo(document.body);
+			      break;
+		      default :
+			      var container = $(this.options.container);
+			      if (container.length){
+				      console.log("container: "+ this.options.container)
+				      $tip.appendTo(container);
+			      } else {
+				      $tip.insertAfter(this.$element);
+			      }
+	      }
+
 
         pos = this.getPosition(inside)
 
