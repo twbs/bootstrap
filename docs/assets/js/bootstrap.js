624fda5e30edc094128504ae385d455df8972bfa
Blake Embrey <hello@blakeembrey.com>
2012-11-29 06:56:01 +1000

    Fix typeahead click accessibility.

--- docs/assets/js/bootstrap.js
+++ docs/assets/js/bootstrap.js
@@ -1778,6 +1778,8 @@
     }
 
   , listen: function () {
+      var that = this
+
       this.$element
         .on('blur',     $.proxy(this.blur, this))
         .on('keypress', $.proxy(this.keypress, this))
@@ -1788,7 +1790,10 @@
       }
 
       this.$menu
-        .on('click', $.proxy(this.click, this))
+        // Target click and mouseup which matches default browser behavior
+        .on('click mouseup', $.proxy(this.click, this))
+        // Prevent mousedown from firing the blur event on the input
+        .on('mousedown', function () { that.cancelBlur = true })
         .on('mouseenter', 'li', $.proxy(this.mouseenter, this))
     }
 
@@ -1864,8 +1869,13 @@
   }
 
   , blur: function (e) {
-      var that = this
-      setTimeout(function () { that.hide() }, 150)
+      if (this.cancelBlur) {
+        this.$element.focus()
+        this.cancelBlur = false
+      } else {
+        var that = this
+        setTimeout(function () { that.hide() }, 150)
+      }
     }
 
   , click: function (e) {
