91fbadad5b058f160eb3b5eb8e88432e0088efee
fat <jacobthornton@gmail.com>
2012-12-07 17:49:05 -0500

    remove slide from carousel constructor #5579

--- docs/assets/js/bootstrap-carousel.js
+++ docs/assets/js/bootstrap-carousel.js
@@ -29,7 +29,6 @@
   var Carousel = function (element, options) {
     this.$element = $(element)
     this.options = options
-    this.options.slide && this.slide(this.options.slide)
     this.options.pause == 'hover' && this.$element
       .on('mouseenter', $.proxy(this.pause, this))
       .on('mouseleave', $.proxy(this.cycle, this))
