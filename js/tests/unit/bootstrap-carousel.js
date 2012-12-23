44c6f6eac49d41cb3afe80861e46b96dbbdc9d01
Yohn <john.skem9@gmail.com>
2012-12-23 10:20:47 -0500

    Update js/tests/unit/bootstrap-carousel.js

--- js/tests/unit/bootstrap-carousel.js
+++ js/tests/unit/bootstrap-carousel.js
@@ -78,4 +78,13 @@ $(function () {
         ok($('#myCarousel').data('carousel').options.interval == 1814, "attributes should be read only on intitialization");
         $('#myCarousel').remove();
       })
+
+      test("should listen to interval from data attribute", function () {
+        var template = $('<div id="myCarousel" class="carousel slide" data-interval="2000"> <div class="carousel-inner"> <div class="item active"> <img alt=""> <div class="carousel-caption"> <h4>{{_i}}First Thumbnail label{{/i}}</h4> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> </div> </div> <div class="item"> <img alt=""> <div class="carousel-caption"> <h4>{{_i}}Second Thumbnail label{{/i}}</h4> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> </div> </div> <div class="item"> <img alt=""> <div class="carousel-caption"> <h4>{{_i}}Third Thumbnail label{{/i}}</h4> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> </div> </div> </div> <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a> <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a> </div>');
+
+        template.appendTo("body");
+        $('[data-slide]').first().click();
+        ok($('#myCarousel').data('carousel').options.interval == 2000);
+        $('#myCarousel').remove();
+      })
 })
