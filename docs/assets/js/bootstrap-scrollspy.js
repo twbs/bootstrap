2f42851405a829bf081482916b4a11779fbd6e9e
Cloudyan <txw1219@163.com>
2012-12-25 23:43:47 +0800

    Update docs/assets/js/bootstrap-scrollspy.js
    Refresh again appeared anchor identification error
    
    https://github.com/twitter/bootstrap/issues/6217

--- docs/assets/js/bootstrap-scrollspy.js
+++ docs/assets/js/bootstrap-scrollspy.js
@@ -59,7 +59,7 @@
               , $href = /^#\w/.test(href) && $(href)
             return ( $href
               && $href.length
-              && [[ $href.position().top + self.$scrollElement.scrollTop(), href ]] ) || null
+              && [[ $href.position().top , href ]] ) || null
           })
           .sort(function (a, b) { return a[0] - b[0] })
           .each(function () {
