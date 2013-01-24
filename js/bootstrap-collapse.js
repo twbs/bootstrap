0ad855c1509c7c8ab3e6441a562dc9f48fbb9e2a
Freek Wielstra <fwielstra@xebia.com>
2013-01-24 14:10:10 +0100

    Do not toggle 'collapsed' state of clicked collapse element when target is still animating
    Possible fix for issue #6691

--- js/bootstrap-collapse.js
+++ js/bootstrap-collapse.js
@@ -160,7 +160,7 @@
         || e.preventDefault()
         || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
       , option = $(target).data('collapse') ? 'toggle' : $this.data()
-    $this[$(target).hasClass('in') ? 'addClass' : 'removeClass']('collapsed')
+    if ($(target).css("height") !== "auto") $this[$(target).hasClass('in') ? 'addClass' : 'removeClass']('collapsed')
     $(target).collapse(option)
   })
 
