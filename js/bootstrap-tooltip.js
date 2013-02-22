3cc425aa15e021a6d5d97599313ae93eb47f7fe8
Vilius Paulauskas <viliusp@gmail.com>
2013-02-22 13:05:56 +0000

    Clear timeout on every mouseenter event to avoid delayed leave() hiding the tooltip

--- js/bootstrap-tooltip.js
+++ js/bootstrap-tooltip.js
@@ -82,9 +82,9 @@
   , enter: function (e) {
       var self = $(e.currentTarget)[this.type](this._options).data(this.type)
 
+      clearTimeout(this.timeout)
       if (!self.options.delay || !self.options.delay.show) return self.show()
 
-      clearTimeout(this.timeout)
       self.hoverState = 'in'
       this.timeout = setTimeout(function() {
         if (self.hoverState == 'in') self.show()
