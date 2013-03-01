01e0f8c653db442326ee1559eac4691af246d49e
fat <jacobthornton@gmail.com>
2013-02-28 20:39:07 -0800

    only pass unique options through to child nodes in tooltips

--- docs/assets/js/bootstrap.js
+++ docs/assets/js/bootstrap.js
@@ -1125,7 +1125,16 @@
     }
 
   , enter: function (e) {
-      var self = $(e.currentTarget)[this.type](this._options).data(this.type)
+      var defaults = $.fn[this.type].defaults
+        , options = {}
+        , self
+
+
+      this._options && $.each(this._options, function (key, value) {
+        if (defaults[key] != value) options[key] = value
+      }, this)
+
+      self = $(e.currentTarget)[this.type](options).data(this.type)
 
       if (!self.options.delay || !self.options.delay.show) return self.show()
 
