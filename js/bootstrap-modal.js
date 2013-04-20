twbs/bootstrap

commit 366e1e0a6d1442b8cf8546a27bac44a841b892a2

only fire hidden once backdrop has been removed

js/bootstrap-modal.js
@@ -138,12 +138,13 @@
         })
       }
 
-    , hideModal: function (that) {
-        this.$element
-          .hide()
-          .trigger('hidden')
-
-        this.backdrop()
+    , hideModal: function () {
+        var that = this
+        this.$element.hide()
+        this.backdrop(function () {
+          that.removeBackdrop()
+          that.$element.trigger('hidden')
+        })
       }
 
     , removeBackdrop: function () {
@@ -181,8 +182,8 @@
           this.$backdrop.removeClass('in')
 
           $.support.transition && this.$element.hasClass('fade')?
-            this.$backdrop.one($.support.transition.end, $.proxy(this.removeBackdrop, this)) :
-            this.removeBackdrop()
+            this.$backdrop.one($.support.transition.end, callback) :
+            callback()
 
         } else if (callback) {
           callback()

docs/assets/js/bootstrap-modal.js
@@ -138,12 +138,13 @@
         })
       }
 
-    , hideModal: function (that) {
-        this.$element
-          .hide()
-          .trigger('hidden')
-
-        this.backdrop()
+    , hideModal: function () {
+        var that = this
+        this.$element.hide()
+        this.backdrop(function () {
+          that.removeBackdrop()
+          that.$element.trigger('hidden')
+        })
       }
 
     , removeBackdrop: function () {
@@ -181,8 +182,8 @@
           this.$backdrop.removeClass('in')
 
           $.support.transition && this.$element.hasClass('fade')?
-            this.$backdrop.one($.support.transition.end, $.proxy(this.removeBackdrop, this)) :
-            this.removeBackdrop()
+            this.$backdrop.one($.support.transition.end, callback) :
+            callback()
 
         } else if (callback) {
           callback()

docs/assets/js/bootstrap.js
@@ -936,12 +936,13 @@
         })
       }
 
-    , hideModal: function (that) {
-        this.$element
-          .hide()
-          .trigger('hidden')
-
-        this.backdrop()
+    , hideModal: function () {
+        var that = this
+        this.$element.hide()
+        this.backdrop(function () {
+          that.removeBackdrop()
+          that.$element.trigger('hidden')
+        })
       }
 
     , removeBackdrop: function () {
@@ -979,8 +980,8 @@
           this.$backdrop.removeClass('in')
 
           $.support.transition && this.$element.hasClass('fade')?
-            this.$backdrop.one($.support.transition.end, $.proxy(this.removeBackdrop, this)) :
-            this.removeBackdrop()
+            this.$backdrop.one($.support.transition.end, callback) :
+            callback()
 
         } else if (callback) {
           callback()

commit 642ecc15b87a320671eee587443660f99f922f9c

Fixed modal backdrop bug that was introduced with
https://github.com/twitter/bootstrap/commit/366e1e0a6d1442b8cf8546a27bac44a841b892a2

Check to make sure backdrop is used before removing it.

js/bootstrap-modal.js
@@ -142,13 +142,15 @@
         var that = this
         this.$element.hide()
         this.backdrop(function () {
-          that.removeBackdrop()
+          if (that.options.backdrop) {
+            that.removeBackdrop()
+          }
           that.$element.trigger('hidden')
         })
       }
 
     , removeBackdrop: function () {
-        this.$backdrop && this.$backdrop.remove()
+         this.$backdrop && this.$backdrop.remove()
         this.$backdrop = null
       }
 