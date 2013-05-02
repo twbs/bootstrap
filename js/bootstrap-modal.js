c40bbebd0eb23dec8076cf08348a06f4300e8c63
Cody Lundquist <cody.lundquist@gmail.com>
2013-05-02 13:28:06 +1000

    Changing out jQuery.delegate() for jQuery.on()

--- js/bootstrap-modal.js
+++ js/bootstrap-modal.js
@@ -29,7 +29,7 @@
   var Modal = function (element, options) {
     this.options = options
     this.$element = $(element)
-      .delegate('[data-dismiss="modal"]', 'click.dismiss.modal', $.proxy(this.hide, this))
+      .on('click.dismiss.modal', '[data-dismiss="modal"]', $.proxy(this.hide, this))
     this.options.remote && this.$element.find('.modal-body').load(this.options.remote)
   }
 
