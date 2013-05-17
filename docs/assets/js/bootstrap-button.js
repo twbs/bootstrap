c9789f75153539a0ca0a2f31234716f527024333
fat <jacobthornton@gmail.com>
2013-05-17 10:20:13 -0700

    update versioning to 2.3.2

--- docs/assets/js/bootstrap-button.js
+++ docs/assets/js/bootstrap-button.js
@@ -1,5 +1,5 @@
 /* ============================================================
- * bootstrap-button.js v2.3.1
+ * bootstrap-button.js v2.3.2
  * http://twitter.github.com/bootstrap/javascript.html#buttons
  * ============================================================
  * Copyright 2012 Twitter, Inc.
--- docs/assets/js/bootstrap-carousel.js
+++ docs/assets/js/bootstrap-carousel.js
@@ -1,5 +1,5 @@
 /* ==========================================================
- * bootstrap-carousel.js v2.3.1
+ * bootstrap-carousel.js v2.3.2
  * http://twitter.github.com/bootstrap/javascript.html#carousel
  * ==========================================================
  * Copyright 2012 Twitter, Inc.
