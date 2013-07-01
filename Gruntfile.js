df9250d36c88c3804112c5019a2b8bb443ec30c8
John-Philip Johansson <seriema@gmail.com>
2013-07-01 20:38:04 +0530

    Small prettyfication of Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,14 @@
 module.exports = function(grunt) {
-    var jsFiles = [
-        'js/transition.js',
-        'js/alert.js',
-        'js/button.js',
-        'js/carousel.js',
-        'js/collapse.js',
-        'js/dropdown.js',
-        'js/modal.js',
-        'js/tooltip.js',
-        'js/popover.js',
-        'js/scrollspy.js',
-        'js/tab.js',
-        'js/affix.js'
-    ];
 
     // Project configuration.
     grunt.initConfig({
         // Metadata.
         pkg: grunt.file.readJSON('package.json'),
-        banner: '/**\n' + '* <%= pkg.name %>.js v<%= pkg.version %> by @fat and @mdo\n' + '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' + '* <%= _.pluck(pkg.licenses, "url").join(", ") %>\n' + '*/\n',
+        banner: '/**\n' +
+                '* <%= pkg.name %>.js v<%= pkg.version %> by @fat and @mdo\n' +
+                '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                '* <%= _.pluck(pkg.licenses, "url").join(", ") %>\n' +
+                '*/\n',
         // Task configuration.
         clean: {
             dist: ['bootstrap']
@@ -29,7 +19,7 @@ module.exports = function(grunt) {
                 stripBanners: false
             },
             bootstrap: {
-                src: jsFiles,
+                src: ['js/*.js'],
                 dest: 'bootstrap/js/<%= pkg.name %>.js'
             }
         },
