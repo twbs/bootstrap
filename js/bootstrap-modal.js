4311ff86c8148c0a4a87f87e34e198a27533bfdf
Roman Kinyakin <1@grep.su>
2013-07-12 11:52:15 +0600

    Modal "loaded" event
    The event is triggered after ajax load complete.
    Is very useful when need to change modal buttons actions according to loaded content.

--- js/bootstrap-modal.js
+++ js/bootstrap-modal.js
@@ -30,7 +30,10 @@
     this.options = options
     this.$element = $(element)
       .delegate('[data-dismiss="modal"]', 'click.dismiss.modal', $.proxy(this.hide, this))
-    this.options.remote && this.$element.find('.modal-body').load(this.options.remote)
+    this.options.remote && this.$element.find('.modal-body').load(this.options.remote, function () {
+      var e = $.Event('loaded')
+      $(element).trigger(e)
+    } )
   }
 
   Modal.prototype = {
