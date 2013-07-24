a346ea2a1a595fec447e3a86b2e14a504bd57456
Mark Otto <otto@github.com>
2013-07-23 21:23:13 -0700

    make on prev commit

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -683,7 +683,7 @@
     if (!isActive) {
       if ('ontouchstart' in document.documentElement) {
         // if mobile we we use a backdrop because click events don't delegate
-        $('<div class="dropdown-backdrop"/>').insertBefore($(this)).on('click', clearMenus)
+        $('<div class="dropdown-backdrop"/>').insertAfter($(this)).on('click', clearMenus)
       }
 
       $parent.trigger(e = $.Event('show.bs.dropdown'))
