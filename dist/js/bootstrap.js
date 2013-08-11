068cda01483ae1b0d117afb4b34ba0e264c3f6e7
Chris Seaton <chris@chrisseaton.com>
2013-08-11 13:26:15 +0100

    Broken merge.

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -903,7 +903,34 @@ if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
             that.$element.focus().trigger(e)
           })
           .emulateTransitionEnd(300) :
-        that.$element.focus().trigger(e)
+        that.$element.focus().trigger('shown.bs.modal')
+
+      // iOS specific fix - can't be feature detected.
+
+      if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g)) {
+        /*
+         * iOS will not move the fixed backdrop and modal container when the
+         * keyboard is shown, so switch them to absolute and manually make them
+         * fill the page as it is shaped now. This will break if the page
+         * changes shape. We also need to add padding to the modal container so
+         * the contents appears where we are looking, otherwise it would be at
+         * the top of the page. We do the modal container first as it may
+         * change the shape of the page.
+         */
+
+        $(".modal").css("padding-top", window.pageYOffset);
+        $(".modal").addClass("modal-ios");
+
+        $(".modal").height(Math.max(
+          $(document).height(),
+          $(window).height()));
+
+        $(".modal-backdrop").addClass("modal-backdrop-ios");
+
+        $(".modal-backdrop").height(Math.max(
+          $(document).height(),
+          $(window).height()));
+      }
     })
   }
 
