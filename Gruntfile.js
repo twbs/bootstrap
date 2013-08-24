48d929d69edf06fd0b02e05522e0c8efe6b5b77f
Mark Otto <otto@github.com>
2013-08-23 22:38:51 -0700

    Use jekyll var for current version in docs; update banner in gruntfile to match .less banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,9 +9,11 @@ module.exports = function(grunt) {
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
     banner: '/**\n' +
-              '* <%= pkg.name %>.js v<%= pkg.version %> by @fat and @mdo\n' +
+              '* Bootstrap v<%= pkg.version %> by @fat and @mdo\n' +
               '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-              '* <%= _.pluck(pkg.licenses, "url").join(", ") %>\n' +
+              '* Licensed under <%= _.pluck(pkg.licenses, "url").join(", ") %>.\n' +
+              '*\n' +
+              '* Designed and built with all the love in the world by @mdo and @fat.\n' +
               '*/\n',
     jqueryCheck: 'if (!jQuery) { throw new Error(\"Bootstrap requires jQuery\") }\n\n',
 
