ed1bd2b150dd645c11f92126a12183985ddbbc15
Roland Warmerdam <rowno@webspirited.com>
2013-08-31 18:03:09 +1200

    carousel: correctly reset when the slide event is prevented

--- js/tests/unit/carousel.js
+++ js/tests/unit/carousel.js
@@ -16,7 +16,7 @@ $(function () {
         ok($(document.body).carousel()[0] == document.body, 'document.body returned')
       })
 
-      test("should not fire sliden when slide is prevented", function () {
+      test("should not fire slid when slide is prevented", function () {
         $.support.transition = false
         stop()
         $('<div class="carousel"/>')
@@ -29,6 +29,29 @@ $(function () {
             ok(false);
           })
           .carousel('next')
+      })
+
+      test("should reset when slide is prevented", function () {
+        var template = '<div id="carousel-example-generic" class="carousel slide"><ol class="carousel-indicators"><li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li><li data-target="#carousel-example-generic" data-slide-to="1"></li><li data-target="#carousel-example-generic" data-slide-to="2"></li></ol><div class="carousel-inner"><div class="item active"><div class="carousel-caption"></div></div><div class="item"><div class="carousel-caption"></div></div><div class="item"><div class="carousel-caption"></div></div></div><a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"></a><a class="right carousel-control" href="#carousel-example-generic" data-slide="next"></a></div>'
+        var $carousel = $(template)
+        $.support.transition = false
+        stop()
+        $carousel.one('slide.bs.carousel', function (e) {
+          e.preventDefault()
+          setTimeout(function () {
+            ok($carousel.find('.item:eq(0)').is('.active'))
+            ok($carousel.find('.carousel-indicators li:eq(0)').is('.active'))
+            $carousel.carousel('next')
+          }, 1);
+        })
+        $carousel.one('slid.bs.carousel', function () {
+          setTimeout(function () {
+            ok($carousel.find('.item:eq(1)').is('.active'))
+            ok($carousel.find('.carousel-indicators li:eq(1)').is('.active'))
+            start()
+          }, 1);
+        })
+        $carousel.carousel('next')
       })
 
       test("should fire slide event with direction", function () {
