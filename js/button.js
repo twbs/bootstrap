fb20a4678f0deeb3dd38af5442f042ce1d746996
alexbea <beaudoin.alexis@gmail.com>
2013-09-29 13:30:25 -0400

    As @clement911 said, it might be a good idea to raise click too. Now it has the same behaviour as an input type radio

--- js/button.js
+++ js/button.js
@@ -58,6 +58,8 @@
     if ($parent.length) {
       var $input = this.$element.find('input')
         .prop('checked', !this.$element.hasClass('active'))
+
+      $input.triggerHandler("click")
       if (!this.$element.hasClass('active')) $input.trigger('change')
       if ($input.prop('type') === 'radio') $parent.find('.active').removeClass('active')
     }
