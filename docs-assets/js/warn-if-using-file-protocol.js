9e6d179deb5b5e790f42a1cff3cdc62198a252bc
Chris Rebert <code@rebertia.com>
2013-10-09 16:12:57 -0700

    use JS to warn people in the examples themselves that opening the HTML files directly doesn't work

---
+++ docs-assets/js/warn-if-using-file-protocol.js
@@ -0,0 +1,7 @@
+if (window.location.protocol === 'file:') {
+  window.alert("ERROR: This Bootstrap example won't work correctly\n" +
+    "because you've opened the HTML file directly\n" +
+    "instead of serving it via HTTP.\n" +
+    "Web browser security rules will prevent\n" +
+    "some of our resources from loading.  :-(")
+}
