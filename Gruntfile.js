0d122fc3230663cadfef7474bbf54aa297334c72
Chris Rebert <code@rebertia.com>
2013-10-23 23:25:17 -0700

    silence validator regarding http-equiv="X-UA-Compatible"

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,7 +132,10 @@ module.exports = function(grunt) {
 
     validation: {
       options: {
-        reset: true
+        reset: true,
+        relaxerror: [
+            "Bad value X-UA-Compatible for attribute http-equiv on element meta."
+        ]
       },
       files: {
         src: ["_gh_pages/**/*.html"]
