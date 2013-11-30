a7a9dcaeb2231b2580cf79fcee7d777b98ac24d8
Mark Otto <otto@github.com>
2013-11-30 00:42:36 -0800

    Alternate fix for #11303: add link to site in banner and remove @mdo and @fat usernames from it

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1,9 +1,7 @@
 /*!
- * Bootstrap v3.0.2 by @fat and @mdo
+ * Bootstrap v3.0.2 (http://getbootstrap.com)
  * Copyright 2013 Twitter, Inc.
  * Licensed under http://www.apache.org/licenses/LICENSE-2.0
- *
- * Designed and built with all the love in the world by @mdo and @fat.
  */
 
 if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery") }
