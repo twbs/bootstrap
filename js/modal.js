31d131883e64b4d139c24dcf7bd8c26e13036791
Dana Shalev <dana.shalev@hp.com>
2013-12-03 12:43:01 +0200

    Enable specifying elements which can be focused
    In a modal dialog enable specifying element which can receive focus even
    though they are not descendants of the modal dialog. This is a very
    common use case with drop down components.

--- js/modal.js
+++ js/modal.js
@@ -113,12 +113,16 @@
         .emulateTransitionEnd(300) :
       this.hideModal()
   }
+  
+  Modal.prototype.allowFocus: function() {
+    return false
+  }
 
   Modal.prototype.enforceFocus = function () {
     $(document)
       .off('focusin.bs.modal') // guard against infinite focus loop
       .on('focusin.bs.modal', $.proxy(function (e) {
-        if (this.$element[0] !== e.target && !this.$element.has(e.target).length) {
+        if (this.$element[0] !== e.target && !this.$element.has(e.target).length  && !that.allowFocus(e)) {
           this.$element.focus()
         }
       }, this))
