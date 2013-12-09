e1247e3edc35ff8419af3ab984bda69529aa4da1
Mark Otto <otto@github.com>
2013-12-08 17:17:51 -0800

    Match customizer banner to Gruntfile banner

--- docs-assets/js/customizer.js
+++ docs-assets/js/customizer.js
@@ -8,7 +8,7 @@
 /* jshint multistr:true */
 
 window.onload = function () { // wait for load in a dumb way because B-0
-  var cw = '/*!\n * Bootstrap v3.0.3\n *\n * Copyright 2013 Twitter, Inc\n * Licensed under the Apache License v2.0\n * http://www.apache.org/licenses/LICENSE-2.0\n *\n * Designed and built with all the love in the world @twitter by @mdo and @fat.\n */\n\n'
+  var cw = '/*!\n * Bootstrap v3.0.3 (http://getbootstrap.com)\n\n *\n * Copyright 2013 Twitter, Inc\n * Licensed under the Apache License v2.0\n * http://www.apache.org/licenses/LICENSE-2.0\n */\n\n'
 
   function showError(msg, err) {
     $('<div id="bsCustomizerAlert" class="bs-customizer-alert">\
