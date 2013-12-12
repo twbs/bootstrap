df0a24966353884192887cbef88b8b672f4171a6
Jim Le <jim@heightdigital.co.uk>
2013-12-12 16:44:30 +0000

    added bootstrap-datepicker to compile js task commented out js files not needed

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,15 +75,16 @@ module.exports = function (grunt) {
           'js/transition.js',
           'js/alert.js',
           'js/button.js',
-          'js/carousel.js',
+          //'js/carousel.js',
           'js/collapse.js',
           'js/dropdown.js',
           'js/modal.js',
           'js/tooltip.js',
           'js/popover.js',
-          'js/scrollspy.js',
-          'js/tab.js',
-          'js/affix.js'
+          //'js/scrollspy.js',
+          //'js/tab.js',
+          //'js/affix.js',
+          'js/bootstrap-datepicker.js'
         ],
         dest: 'dist/js/<%= pkg.name %>.js'
       }
