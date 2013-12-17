dcfe710fa4813da95dcfecc50a0581427d4cb491
Chris Rebert <github@rebertia.com>
2013-12-17 01:35:16 -0800

    enable failHard grunt-html-validation option; fixes #11876

--- Gruntfile.js
+++ Gruntfile.js
@@ -195,6 +195,7 @@ module.exports = function (grunt) {
       options: {
         charset: 'utf-8',
         doctype: 'HTML5',
+        failHard: true,
         reset: true,
         relaxerror: [
           'Bad value X-UA-Compatible for attribute http-equiv on element meta.',
