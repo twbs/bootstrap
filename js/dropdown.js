22f9767714a9f80d5d12479725e8875bdd83f341
fat <jacobthornton@gmail.com>
2013-12-18 21:14:01 -0800

    check that href id's are followed by valid char – fixes #10044

--- js/dropdown.js
+++ js/dropdown.js
@@ -95,7 +95,7 @@
 
     if (!selector) {
       selector = $this.attr('href')
-      selector = selector && /#/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
+      selector = selector && /#[A-Za-z]/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
     }
 
     var $parent = selector && $(selector)
