4e48f21bf40a7181c3139bac76b4f9c5858cfddc
Fabien <fabien.creative@gmail.com>
2013-12-19 15:26:43 +0100

    New example view switch
    Add a grid/view list switcher example

---
+++ examples/grid-list-switcher/grid-list-switcher.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#list').click(function(){
+    $('#grid-list-switcher .item').addClass('list-view-item');
+  });
+  $('#grid').click(function(){
+    $('#grid-list-switcher .item').removeClass('list-view-item');
+  });
+});
