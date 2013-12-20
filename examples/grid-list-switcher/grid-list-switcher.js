8b352ee1660fbd0e4288cdb1aa77a41fee549fad
Fabien <fabien.creative@gmail.com>
2013-12-20 22:20:45 +0100

    remove semicolon

--- examples/grid-list-switcher/grid-list-switcher.js
+++ examples/grid-list-switcher/grid-list-switcher.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('#list').click(function(){
-    $('#grid-list-switcher .item').addClass('list-view-item');
-  });
+    $('#grid-list-switcher .item').addClass('list-view-item')
+  })
   $('#grid').click(function(){
-    $('#grid-list-switcher .item').removeClass('list-view-item');
-  });
-});
+    $('#grid-list-switcher .item').removeClass('list-view-item')
+  })
+})
