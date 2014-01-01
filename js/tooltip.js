1c83d68ca45adce77d8eca9bb5643db7b57b9ef7
Zlatan Vasović <legospace9876@gmail.com>
2014-01-01 15:37:08 +0100

    New Year

--- js/tooltip.js
+++ js/tooltip.js
@@ -3,7 +3,7 @@
  * http://getbootstrap.com/javascript/#tooltip
  * Inspired by the original jQuery.tipsy by Jason Frame
  * ========================================================================
- * Copyright 2013 Twitter, Inc.
+ * Copyright 2014 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
