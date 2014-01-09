a89a073e32fbb43386775c4e818beb21fc1f6e8d
David Kitchen <david@buro9.com>
2014-01-09 15:19:32 +0000

    Removed glyphicons from the build

--- docs-assets/js/customize.js
+++ docs-assets/js/customize.js
@@ -1,6 +1,6 @@
 /*!
  * Bootstrap v3.0.3 (http://getbootstrap.com)
- * Copyright 2013 Twitter, Inc.
+ * Copyright 2014 Twitter, Inc.
  * Licensed under http://www.apache.org/licenses/LICENSE-2.0
  */
 
