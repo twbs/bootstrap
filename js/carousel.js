1409cde7e800ca83fd761f87e5ad8f0d259e38d1
Mark Otto <otto@github.com>
2014-01-30 08:30:37 -0800

    Bump versions, run grunt

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: carousel.js v3.0.3
+ * Bootstrap: carousel.js v3.1.0
  * http://getbootstrap.com/javascript/#carousel
  * ========================================================================
  * Copyright 2011-2014 Twitter, Inc.
