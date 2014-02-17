a06e15dd368ea2b24b911c7f77892a2b19a58f98
Zlatan Vasović <legospace9876@gmail.com>
2014-02-17 20:56:46 +0100

    Remove semicolons from JS tests

--- js/tests/unit/carousel.js
+++ js/tests/unit/carousel.js
@@ -21,12 +21,12 @@ $(function () {
     stop()
     $('<div class="carousel"/>')
       .on('slide.bs.carousel', function (e) {
-        e.preventDefault();
-        ok(true);
-        start();
+        e.preventDefault()
+        ok(true)
+        start()
       })
       .on('slid.bs.carousel', function () {
-        ok(false);
+        ok(false)
       })
       .carousel('next')
   })
@@ -42,14 +42,14 @@ $(function () {
         ok($carousel.find('.item:eq(0)').is('.active'))
         ok($carousel.find('.carousel-indicators li:eq(0)').is('.active'))
         $carousel.carousel('next')
-      }, 1);
+      }, 1)
     })
     $carousel.one('slid.bs.carousel', function () {
       setTimeout(function () {
         ok($carousel.find('.item:eq(1)').is('.active'))
         ok($carousel.find('.carousel-indicators li:eq(1)').is('.active'))
         start()
-      }, 1);
+      }, 1)
     })
     $carousel.carousel('next')
   })
@@ -72,39 +72,39 @@ $(function () {
     stop()
     $(template)
       .on('slide.bs.carousel', function (e) {
-        e.preventDefault();
-        ok(e.relatedTarget);
-        ok($(e.relatedTarget).hasClass('item'));
-        start();
+        e.preventDefault()
+        ok(e.relatedTarget)
+        ok($(e.relatedTarget).hasClass('item'))
+        start()
       })
       .carousel('next')
   })
 
   test('should set interval from data attribute', 4, function () {
-    var template = $('<div id="myCarousel" class="carousel slide"> <div class="carousel-inner"> <div class="item active"> <img alt=""> <div class="carousel-caption"> <h4>{{_i}}First Thumbnail label{{/i}}</h4> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> </div> </div> <div class="item"> <img alt=""> <div class="carousel-caption"> <h4>{{_i}}Second Thumbnail label{{/i}}</h4> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> </div> </div> <div class="item"> <img alt=""> <div class="carousel-caption"> <h4>{{_i}}Third Thumbnail label{{/i}}</h4> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> </div> </div> </div> <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a> <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a> </div>');
-    template.attr('data-interval', 1814);
+    var template = $('<div id="myCarousel" class="carousel slide"> <div class="carousel-inner"> <div class="item active"> <img alt=""> <div class="carousel-caption"> <h4>{{_i}}First Thumbnail label{{/i}}</h4> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> </div> </div> <div class="item"> <img alt=""> <div class="carousel-caption"> <h4>{{_i}}Second Thumbnail label{{/i}}</h4> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> </div> </div> <div class="item"> <img alt=""> <div class="carousel-caption"> <h4>{{_i}}Third Thumbnail label{{/i}}</h4> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> </div> </div> </div> <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a> <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a> </div>')
+    template.attr('data-interval', 1814)
 
-    template.appendTo('body');
-    $('[data-slide]').first().click();
-    ok($('#myCarousel').data('bs.carousel').options.interval == 1814);
-    $('#myCarousel').remove();
+    template.appendTo('body')
+    $('[data-slide]').first().click()
+    ok($('#myCarousel').data('bs.carousel').options.interval == 1814)
+    $('#myCarousel').remove()
 
-    template.appendTo('body').attr('data-modal', 'foobar');
-    $('[data-slide]').first().click();
-    ok($('#myCarousel').data('bs.carousel').options.interval == 1814, 'even if there is an data-modal attribute set');
-    $('#myCarousel').remove();
+    template.appendTo('body').attr('data-modal', 'foobar')
+    $('[data-slide]').first().click()
+    ok($('#myCarousel').data('bs.carousel').options.interval == 1814, 'even if there is an data-modal attribute set')
+    $('#myCarousel').remove()
 
-    template.appendTo('body');
-    $('[data-slide]').first().click();
-    $('#myCarousel').attr('data-interval', 1860);
-    $('[data-slide]').first().click();
-    ok($('#myCarousel').data('bs.carousel').options.interval == 1814, 'attributes should be read only on intitialization');
-    $('#myCarousel').remove();
+    template.appendTo('body')
+    $('[data-slide]').first().click()
+    $('#myCarousel').attr('data-interval', 1860)
+    $('[data-slide]').first().click()
+    ok($('#myCarousel').data('bs.carousel').options.interval == 1814, 'attributes should be read only on intitialization')
+    $('#myCarousel').remove()
 
-    template.attr('data-interval', false);
-    template.appendTo('body');
-    $('#myCarousel').carousel(1);
-    ok($('#myCarousel').data('bs.carousel').options.interval === false, 'data attribute has higher priority than default options');
-    $('#myCarousel').remove();
+    template.attr('data-interval', false)
+    template.appendTo('body')
+    $('#myCarousel').carousel(1)
+    ok($('#myCarousel').data('bs.carousel').options.interval === false, 'data attribute has higher priority than default options')
+    $('#myCarousel').remove()
   })
 })
