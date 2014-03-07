6f9e3f0d23071dad82f0acf942b01d2d79455a6d
XhmikosR <xhmikosr@users.sourceforge.net>
2014-03-07 13:25:59 +0200

    Fix typo.

--- grunt/bs-glyphicons-data-generator.js
+++ grunt/bs-glyphicons-data-generator.js
@@ -11,14 +11,14 @@ module.exports = function generateGlyphiconsData() {
   // Pass encoding, utf8, so `readFileSync` will return a string instead of a
   // buffer
   var glyphiconsFile = fs.readFileSync('less/glyphicons.less', 'utf8');
-  var glpyhiconsLines = glyphiconsFile.split('\n');
+  var glyphiconsLines = glyphiconsFile.split('\n');
 
   // Use any line that starts with ".glyphicon-" and capture the class name
   var iconClassName = /^\.(glyphicon-[^\s]+)/;
   var glyphiconsData = '# This file is generated via Grunt task. **Do not edit directly.**\n' +
                        '# See the \'build-glyphicons-data\' task in Gruntfile.js.\n\n';
-  for (var i = 0, len = glpyhiconsLines.length; i < len; i++) {
-    var match = glpyhiconsLines[i].match(iconClassName);
+  for (var i = 0, len = glyphiconsLines.length; i < len; i++) {
+    var match = glyphiconsLines[i].match(iconClassName);
 
     if (match !== null) {
       glyphiconsData += '- ' + match[1] + '\n';
