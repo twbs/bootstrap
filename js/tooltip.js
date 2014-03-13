8a6cdff825a114ec43532701e29087ed8cdedd50
Chris Rebert <code@rebertia.com>
2014-03-13 15:05:13 -0700

    add role="tooltip" to tooltips by default to improve accessibility
    See https://github.com/paypal/bootstrap-accessibility-plugin/blob/master/README.md#tooltip

--- js/tooltip.js
+++ js/tooltip.js
@@ -29,7 +29,7 @@
     animation: true,
     placement: 'top',
     selector: false,
-    template: '<div class="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>',
+    template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>',
     trigger: 'hover focus',
     title: '',
     delay: 0,
