35ddc8427ed664241cf5a4b87cfd2d7d6ce70697
Mark Otto <otto@github.com>
2014-03-17 02:03:16 -0700

    revamp all the things

--- docs/assets/js/src/application.js
+++ docs/assets/js/src/application.js
@@ -31,44 +31,43 @@
     var $window = $(window)
     var $body   = $(document.body)
 
-    var navHeight = $('.navbar').outerHeight(true) + 10
+    // var navHeight = $('.navbar').outerHeight(true) + 10
 
     $body.scrollspy({
-      target: '.bs-docs-sidebar',
-      // offset: navHeight
+      target: '.bs-docs-toc-item',
     })
 
     $window.on('load', function () {
       $body.scrollspy('refresh')
     })
 
-    $('.bs-docs-container [href=#]').click(function (e) {
-      e.preventDefault()
-    })
+    // $('.bs-docs-container [href=#]').click(function (e) {
+    //   e.preventDefault()
+    // })
 
     // back to top
-    setTimeout(function () {
-      var $sideBar = $('.bs-docs-sidebar')
+    // setTimeout(function () {
+    //   var $sideBar = $('.bs-docs-sidebar')
 
-      $sideBar.affix({
-        offset: {
-          top: function () {
-            var offsetTop      = $sideBar.offset().top
-            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
-            var navOuterHeight = $('.bs-docs-nav').height()
+    //   $sideBar.affix({
+    //     offset: {
+    //       top: function () {
+    //         var offsetTop      = $sideBar.offset().top
+    //         var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+    //         var navOuterHeight = $('.bs-docs-nav').height()
 
-            return (this.top = offsetTop - navOuterHeight - sideBarMargin)
-          },
-          bottom: function () {
-            return (this.bottom = $('.bs-docs-footer').outerHeight(true))
-          }
-        }
-      })
-    }, 100)
+    //         return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+    //       },
+    //       bottom: function () {
+    //         return (this.bottom = $('.bs-docs-footer').outerHeight(true))
+    //       }
+    //     }
+    //   })
+    // }, 100)
 
-    setTimeout(function () {
-      $('.bs-top').affix()
-    }, 100)
+    // setTimeout(function () {
+    //   $('.bs-top').affix()
+    // }, 100)
 
     // tooltip demo
     $('.tooltip-demo').tooltip({
