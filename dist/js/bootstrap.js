adaeaf47119f03b9b6229974c2302e3b36cacf71
Tyler Cipriani <tyler@tylercipriani.com>
2014-03-17 13:04:58 -0600

    Modify scrollbar check, stop static nav shift

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -795,11 +795,13 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   // ======================
 
   var Modal = function (element, options) {
-    this.options   = options
-    this.$body     = $(document.body)
-    this.$element  = $(element)
-    this.$backdrop =
-    this.isShown   = null
+    this.options        = options
+    this.$body          = $(document.body)
+    this.$fixedNavs    = this.$body.children('.navbar-fixed-top, .navbar-fixed-bottom')
+    this.$element       = $(element)
+    this.$backdrop      =
+    this.isShown        = null
+    this.scrollbarWidth = 0
 
     if (this.options.remote) {
       this.$element
@@ -830,9 +832,11 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
 
     this.isShown = true
 
+    this.checkScrollbar()
     this.$body.addClass('modal-open')
 
     this.setScrollbar()
+
     this.escape()
 
     this.$element.on('click.dismiss.bs.modal', '[data-dismiss="modal"]', $.proxy(this.hide, this))
@@ -976,15 +980,22 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
     }
   }
 
+  Modal.prototype.checkScrollbar = function () {
+    if (document.body.clientWidth >= window.innerWidth) return
+    this.scrollbarWidth = this.scrollbarWidth || this.measureScrollbar()
+  }
+
   Modal.prototype.setScrollbar =  function () {
-    if (document.body.clientHeight <= window.innerHeight) return
-    var scrollbarWidth = this.measureScrollbar()
-    var bodyPad        = parseInt(this.$body.css('padding-right') || 0)
-    if (scrollbarWidth) this.$body.css('padding-right', bodyPad + scrollbarWidth)
+    var bodyPad = parseInt(this.$body.css('padding-right') || 0)
+    if (this.scrollbarWidth) {
+      this.$body.css('padding-right', bodyPad + this.scrollbarWidth)
+      this.$fixedNavs.css('right', '+=' + this.scrollbarWidth)
+    }
   }
 
   Modal.prototype.resetScrollbar = function () {
     this.$body.css('padding-right', '')
+    this.$fixedNavs.css('right', '')
   }
 
   Modal.prototype.measureScrollbar = function () { // thx walsh
