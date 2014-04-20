448086f95a9114fb9ad96b76c5fc7b12a6fc51fd
Chris Rebert <code@rebertia.com>
2014-04-20 16:09:47 -0700

    add IE10 viewport bug workaround to examples
    Closes #13379.

--- docs/assets/js/_src/application.js
+++ docs/assets/js/_src/application.js
@@ -14,20 +14,6 @@
 
   $(function () {
 
-    // IE10 viewport hack for Surface/desktop Windows 8 bug
-    //
-    // See Getting Started docs for more information
-    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-      var msViewportStyle = document.createElement('style')
-      msViewportStyle.appendChild(
-        document.createTextNode(
-          '@-ms-viewport{width:auto!important}'
-        )
-      )
-      document.querySelector('head').appendChild(msViewportStyle)
-    }
-
-
     var $window = $(window)
     var $body   = $(document.body)
 
