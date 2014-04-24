937ef4824e544343252e00a5aa5462e60e5ddd19
Chris Rebert <code@rebertia.com>
2014-04-24 16:00:13 -0700

    remove semicolons :'-(

--- docs/assets/js/ie-emulation-modes-warning.js
+++ docs/assets/js/ie-emulation-modes-warning.js
@@ -12,40 +12,40 @@
   'use strict';
 
   function emulatedIEMajorVersion() {
-    var groups = /MSIE ([0-9.]+)/.exec(window.navigator.userAgent);
+    var groups = /MSIE ([0-9.]+)/.exec(window.navigator.userAgent)
     if (groups === null) {
-      return null;
+      return null
     }
-    var ieVersionNum = parseInt(groups[1], 10);
-    var ieMajorVersion = Math.floor(ieVersionNum);
-    return ieMajorVersion;
+    var ieVersionNum = parseInt(groups[1], 10)
+    var ieMajorVersion = Math.floor(ieVersionNum)
+    return ieMajorVersion
   }
 
   function actualNonEmulatedIEMajorVersion() {
     // Detects the actual version of IE in use, even if it's in an older-IE emulation mode.
     // IE JavaScript conditional compilation docs: http://msdn.microsoft.com/en-us/library/ie/121hztk3(v=vs.94).aspx
     // @cc_on docs: http://msdn.microsoft.com/en-us/library/ie/8ka90k2e(v=vs.94).aspx
-    var jscriptVersion = new Function('/*@cc_on return @_jscript_version; @*/')(); // jshint ignore:line
+    var jscriptVersion = new Function('/*@cc_on return @_jscript_version; @*/')() // jshint ignore:line
     if (jscriptVersion === undefined) {
-      return 11; // IE11+ not in emulation mode
+      return 11 // IE11+ not in emulation mode
     }
     if (jscriptVersion < 9) {
-      return 8; // IE8 (or lower; haven't tested on IE<8)
+      return 8 // IE8 (or lower; haven't tested on IE<8)
     }
-    return jscriptVersion; // IE9 or IE10 in any mode, or IE11 in non-IE11 mode
+    return jscriptVersion // IE9 or IE10 in any mode, or IE11 in non-IE11 mode
   }
 
-  var ua = window.navigator.userAgent;
+  var ua = window.navigator.userAgent
   if (ua.indexOf('Opera') > -1 || ua.indexOf('Presto') > -1) {
-    return; // Opera, which might pretend to be IE
+    return // Opera, which might pretend to be IE
   }
-  var emulated = emulatedIEMajorVersion();
+  var emulated = emulatedIEMajorVersion()
   if (emulated === null) {
-    return; // Not IE
+    return // Not IE
   }
-  var nonEmulated = actualNonEmulatedIEMajorVersion();
+  var nonEmulated = actualNonEmulatedIEMajorVersion()
 
   if (emulated !== nonEmulated) {
-    window.alert('WARNING: You appear to be using IE' + nonEmulated + ' in IE' + emulated + ' emulation mode.\nIE emulation modes can behave significantly differently from ACTUAL older versions of IE.\nPLEASE DON\'T FILE BOOTSTRAP BUGS based on testing in IE emulation modes!');
+    window.alert('WARNING: You appear to be using IE' + nonEmulated + ' in IE' + emulated + ' emulation mode.\nIE emulation modes can behave significantly differently from ACTUAL older versions of IE.\nPLEASE DON\'T FILE BOOTSTRAP BUGS based on testing in IE emulation modes!')
   }
 })();
