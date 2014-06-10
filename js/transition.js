1c6fa9010daf0d0c21de9e20fe6ac4dba1788d90
Katie Zhu <to.kzhu@gmail.com>
2014-06-09 22:18:05 -0700

    MD/CommonJS/Globals #12909

--- js/transition.js
+++ js/transition.js
@@ -7,7 +7,11 @@
  * ======================================================================== */
 
 
-+function ($) {
+(function (o_o) {
+  typeof define  === 'function' && define.amd ? define(['jquery'], o_o) :
+  typeof exports === 'object' ? o_o(require('jquery')) : o_o(this.jQuery)
+})(function ($) {
+
   'use strict';
 
   // CSS TRANSITION SUPPORT (Shoutout: http://www.modernizr.com/)
@@ -46,4 +50,4 @@
     $.support.transition = transitionEnd()
   })
 
-}(jQuery);
+});
