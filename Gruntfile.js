85c15eec0fc585b512cb323caf0044ae03aa2afe
Chris Rebert <code@rebertia.com>
2014-06-17 18:48:15 -0700

    Gruntfile: add note about duplicate copy of jqueryCheck code
    Refs #13842

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,7 @@ module.exports = function (grunt) {
             ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
             ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
             ' */\n',
+    // NOTE: This jqueryCheck code is duplicated in customizer.js; if making changes here, be sure to update the other copy too.
     jqueryCheck: 'if (typeof jQuery === \'undefined\') { throw new Error(\'Bootstrap\\\'s JavaScript requires jQuery\') }\n\n',
 
     // Task configuration.
