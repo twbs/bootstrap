6ec5b8cea8c7a787db3ae1393f258f2803c4da23
Chris Rebert <code@rebertia.com>
2014-06-24 12:12:21 -0700

    retry each test up to 3 times in the event of timeout
    Big thanks to @gvas for https://github.com/axemclion/grunt-saucelabs/pull/136 ! <3

--- Gruntfile.js
+++ Gruntfile.js
@@ -379,6 +379,7 @@ module.exports = function (grunt) {
         options: {
           build: process.env.TRAVIS_JOB_ID,
           concurrency: 10,
+          maxRetries: 3,
           urls: ['http://127.0.0.1:3000/js/tests/index.html'],
           browsers: grunt.file.readYAML('grunt/sauce_browsers.yml')
         }
