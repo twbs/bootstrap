4b0883230866b018f2cbfdc90071ee405b1f3faf
Wing <SteelyWing@users.noreply.github.com>
2014-07-01 10:22:18 +0800

    no need to create $.Event
    `trigger()` can handle string argument

--- js/affix.js
+++ js/affix.js
@@ -83,7 +83,7 @@
     this.$element
       .removeClass(Affix.RESET)
       .addClass(affixType)
-      .trigger($.Event(affixType.replace('affix', 'affixed') + '.bs.affix'))
+      .trigger(affixType.replace('affix', 'affixed') + '.bs.affix')
 
     if (affix == 'bottom') {
       this.$element.offset({
