d738c8729b578e06027677fb45d173de501f5fd3
Mark Otto <otto@github.com>
2014-07-09 02:44:11 -0700

    remove commented out docs js

--- docs/assets/js/src/application.js
+++ docs/assets/js/src/application.js
@@ -32,30 +32,6 @@
       e.preventDefault()
     })
 
-    // Sidenav affixing
-    // setTimeout(function () {
-    //   var $sideBar = $('.bs-docs-sidebar')
-
-    //   $sideBar.affix({
-    //     offset: {
-    //       top: function () {
-    //         var offsetTop      = $sideBar.offset().top
-    //         var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
-    //         var navOuterHeight = $('.bs-docs-nav').height()
-
-    //         return (this.top = offsetTop - navOuterHeight - sideBarMargin)
-    //       },
-    //       bottom: function () {
-    //         return (this.bottom = $('.bs-docs-footer').outerHeight(true))
-    //       }
-    //     }
-    //   })
-    // }, 100)
-
-    // setTimeout(function () {
-    //   $('.bs-top').affix()
-    // }, 100)
-
     // theme toggler
     (function () {
       var stylesheetLink = $('#bs-theme-stylesheet')
