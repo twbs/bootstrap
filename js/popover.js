519a087dbed55e3e350c5a7251ae3d76d195e47d
Chris Hynes <chris@programcsharp.com>
2014-07-25 10:16:44 -0700

    Remove popover content with .children().detach() instead of .empty() so it can be reused

--- js/popover.js
+++ js/popover.js
@@ -46,7 +46,8 @@
     var content = this.getContent()
 
     $tip.find('.popover-title')[this.options.html ? 'html' : 'text'](title)
-    $tip.find('.popover-content').empty()[ // we use append for html objects to maintain js events
+    $tip.find('.popover-content').children().detach()
+    $tip.find('.popover-content')[ // we use append for html objects to maintain js events
       this.options.html ? (typeof content == 'string' ? 'html' : 'append') : 'text'
     ](content)
 
