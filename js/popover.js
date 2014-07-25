531fc80885bd7ed9ead85017fb2c258a3069d7f7
Chris Hynes <chris@programcsharp.com>
2014-07-25 11:10:46 -0700

    Use .end() to remove extra line

--- js/popover.js
+++ js/popover.js
@@ -46,8 +46,7 @@
     var content = this.getContent()
 
     $tip.find('.popover-title')[this.options.html ? 'html' : 'text'](title)
-    $tip.find('.popover-content').children().detach()
-    $tip.find('.popover-content')[ // we use append for html objects to maintain js events
+    $tip.find('.popover-content').children().detach().end()[ // we use append for html objects to maintain js events
       this.options.html ? (typeof content == 'string' ? 'html' : 'append') : 'text'
     ](content)
 
