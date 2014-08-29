52be129689d4f87403c4512fa6dab0f04f513797
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2014-08-29 09:54:31 -0300

    Now ignoring clicks on divider too

--- js/dropdown.js
+++ js/dropdown.js
@@ -152,7 +152,7 @@
 
   $(document)
     .on('click.bs.dropdown.data-api', clearMenus)
-    .on('click.bs.dropdown.data-api', '.dropdown form, .dropdown .dropdown-header', function (e) { e.stopPropagation() })
+    .on('click.bs.dropdown.data-api', '.dropdown form, .dropdown .dropdown-header, .dropdown .divider', function (e) { e.stopPropagation() })
     .on('click.bs.dropdown.data-api', toggle, Dropdown.prototype.toggle)
     .on('keydown.bs.dropdown.data-api', toggle + ', [role="menu"], [role="listbox"]', Dropdown.prototype.keydown)
 
