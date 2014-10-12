4cae79115c26013f35f7894a343813a95085a040
Jon Anders Skorpen <jaskorpe@skorpa.net>
2014-10-12 20:41:44 +0200

    Allow special meaning characters in panel-collapse id
    Issue #13832 suggests escaping the selector, but this issue is fixed
    much more cleanly with an attribute selector. Because bootstrap
    documentation uses href's with hash links, this patch allows links
    both with and without leading hash

--- js/collapse.js
+++ js/collapse.js
@@ -155,8 +155,8 @@
     var $this   = $(this)
     var target  = $this.attr('data-target')
         || e.preventDefault()
-        || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') // strip for ie7
-    var $target = $(target)
+        || (href = $this.attr('href')) && href.replace(/^#{1}/, '') && href.replace(/.*(?=#[^\s]+$)/, '') // strip for ie7
+    var $target = $('[id="' + target + '"]')
     var data    = $target.data('bs.collapse')
     var option  = data ? 'toggle' : $this.data()
     var parent  = $this.attr('data-parent')
