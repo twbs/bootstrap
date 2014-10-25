c1f669e83b9f867d38ba251bb1f944cec874fa4b
Heinrich Fenkart <hnrch02@gmail.com>
2014-10-26 01:50:38 +0200

    Revert "Scrollspy: ignore invisible list items"

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -65,7 +65,6 @@
         return ($href
           && $href.length
           && $href.is(':visible')
-          && $el.is(':visible')
           && [[$href[offsetMethod]().top + offsetBase, href]]) || null
       })
       .sort(function (a, b) { return a[0] - b[0] })
