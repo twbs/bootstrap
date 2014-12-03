116822f4aa61385bf05d3a16be282ab5fc72fae2
DiegoLopesLima <contact.diego.lima@gmail.com>
2014-12-03 21:52:52 -0200

    Keep the `Click + Ctrl` functionality clicking in a modal anchor.

--- js/modal.js
+++ js/modal.js
@@ -310,7 +310,11 @@
     var $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))) // strip for ie7
     var option  = $target.data('bs.modal') ? 'toggle' : $.extend({ remote: !/#/.test(href) && href }, $target.data(), $this.data())
 
-    if ($this.is('a')) e.preventDefault()
+    if ($this.is('a')) {
+      if (e.ctrlKey) return
+
+      e.preventDefault()
+    }
 
     $target.one('show.bs.modal', function (showEvent) {
       if (showEvent.isDefaultPrevented()) return // only register focus restorer if modal will actually get shown
