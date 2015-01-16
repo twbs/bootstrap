abc628bcb826b10250e6c76e3a4eaf9003b7d6dc
benkennedysnc <ben.kennedy.snc@gmail.com>
2015-01-16 15:17:35 -0800

    Horizontal Scrolling Tooltip Misalignment
    Proposal for resolving an issue with horizontal scrolling affecting the placement of tooltips.

--- js/tooltip.js
+++ js/tooltip.js
@@ -340,7 +340,7 @@
     }
     var elOffset  = isBody ? { top: 0, left: 0 } : $element.offset()
     var scroll    = { scroll: isBody ? document.documentElement.scrollTop || document.body.scrollTop : $element.scrollTop() }
-    var outerDims = isBody ? { width: $(window).width(), height: $(window).height() } : null
+    var outerDims = isBody ? { width: document.documentElement.scrollWidth, height: $(window).height() } : null
 
     return $.extend({}, elRect, scroll, outerDims, elOffset)
   }
