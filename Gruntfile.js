f41acae9fca55702a464bf126b7c1ab75703ae29
Chris Rebert <github@rebertia.com>
2015-03-08 21:31:58 -0700

    add TODO item regarding clean-css v3.2 & zeroUnits
    Refs https://github.com/twbs/bootstrap/issues/14837#issuecomment-77441999

--- Gruntfile.js
+++ Gruntfile.js
@@ -223,6 +223,8 @@ module.exports = function (grunt) {
 
     cssmin: {
       options: {
+        // TODO: disable `zeroUnits` optimization once clean-css 3.2 is released
+        //    and then simplify the fix for https://github.com/twbs/bootstrap/issues/14837 accordingly
         compatibility: 'ie8',
         keepSpecialComments: '*',
         advanced: false
