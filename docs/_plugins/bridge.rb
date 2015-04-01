7722de170cec4a5b5f8e52810294434a8e28ee69
mrmrs <hi@mrmrs.cc>
2015-04-01 11:28:48 -0700

    First pass at refactoring.

--- docs/_plugins/bridge.rb
+++
@@ -1,9 +0,0 @@
-require 'yaml'
-
-module Bridge
-  class Generator < Jekyll::Generator
-    def generate(site)
-      site.data["configBridge"] = YAML.load_file("./grunt/configBridge.json")
-    end
-  end
-end
