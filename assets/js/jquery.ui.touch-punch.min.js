ddf00862c21cfdfba535b10d8cec5e30c3699a00
Oliver <linchong223@gmail.com>
2015-04-17 16:34:17 +0800

    Daily build

---
+++ assets/js/jquery.ui.touch-punch.min.js
@@ -0,0 +1,11 @@
+/*!
+ * jQuery UI Touch Punch 0.2.3
+ *
+ * Copyright 2011–2014, Dave Furfero
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Depends:
+ *  jquery.ui.widget.js
+ *  jquery.ui.mouse.js
+ */
+!function(a){function f(a,b){if(!(a.originalEvent.touches.length>1)){a.preventDefault();var c=a.originalEvent.changedTouches[0],d=document.createEvent("MouseEvents");d.initMouseEvent(b,!0,!0,window,1,c.screenX,c.screenY,c.clientX,c.clientY,!1,!1,!1,!1,0,null),a.target.dispatchEvent(d)}}if(a.support.touch="ontouchend"in document,a.support.touch){var e,b=a.ui.mouse.prototype,c=b._mouseInit,d=b._mouseDestroy;b._touchStart=function(a){var b=this;!e&&b._mouseCapture(a.originalEvent.changedTouches[0])&&(e=!0,b._touchMoved=!1,f(a,"mouseover"),f(a,"mousemove"),f(a,"mousedown"))},b._touchMove=function(a){e&&(this._touchMoved=!0,f(a,"mousemove"))},b._touchEnd=function(a){e&&(f(a,"mouseup"),f(a,"mouseout"),this._touchMoved||f(a,"click"),e=!1)},b._mouseInit=function(){var b=this;b.element.bind({touchstart:a.proxy(b,"_touchStart"),touchmove:a.proxy(b,"_touchMove"),touchend:a.proxy(b,"_touchEnd")}),c.call(b)},b._mouseDestroy=function(){var b=this;b.element.unbind({touchstart:a.proxy(b,"_touchStart"),touchmove:a.proxy(b,"_touchMove"),touchend:a.proxy(b,"_touchEnd")}),d.call(b)}}}(jQuery);
--- assets/js/ui-components.js
+++ assets/js/ui-components.js
@@ -73,17 +73,25 @@
 
   $('.section-select').find('select.select2-fusion').select2({
     containerCssClass: 'select2-fusion',
-    dropdownCssClass: 'select2-fusion'
+    dropdownCssClass: 'select2-fusion',
+    minimumResultsForSearch: -1
   });
 
   $('.section-select').find('select.select2-dropdown').select2({
     containerCssClass: 'select2-dropdown',
-    dropdownCssClass: 'select2-dropdown'
+    dropdownCssClass: 'select2-dropdown',
+    minimumResultsForSearch: -1
+  });
+
+  $('.section-select').find('select.select2-simple').select2({
+    containerCssClass: 'select2-simple',
+    dropdownCssClass: 'select2-simple',
+    minimumResultsForSearch: -1
   });
 
   $('.section-lightbox').on('click', '.btn-lightbox', function(e) {
     return $('.section-lightbox').find('.carousel-generic').toggleClass('carousel-active', true);
-  }).on('click', '.carousel-generic', function(e) {
+  }).on('click touchstart', '.carousel-generic', function(e) {
     var $el;
     $el = $(e.target);
     if (!$el.parents('.carousel-generic').length) {
