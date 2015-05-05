5f6355a3e0284abf7d74380b446aacd65ae35aa5
XhmikosR <xhmikosr@gmail.com>
2015-05-05 07:54:57 +0300

    Use bundler to handle Ruby gems.

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+gem 'jekyll', '~> 2.5.3'
+gem 'rouge', '~> 1.8.0'
--- Gruntfile.js
+++ Gruntfile.js
@@ -309,6 +309,7 @@ module.exports = function (grunt) {
 
     jekyll: {
       options: {
+        bundleExec: true,
         config: '_config.yml'
       },
       docs: {},
