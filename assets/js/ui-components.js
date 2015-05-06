14e809cdcad9b767005fe65a2c13408f3aa7f063
Oliver <linchong223@gmail.com>
2015-05-06 11:46:43 +0800

    Update gh pages

--- assets/js/ui-components.js
+++ assets/js/ui-components.js
@@ -1,17 +1,9 @@
 (function() {
-  var ResponsiveLayout;
-
-  if ($('#datatable').length) {
-    $('#datatable').DataTable({
-      paging: false,
-      searching: false,
-      info: false
-    });
-  }
+  var ResponsiveLayout, SelectSwitcher;
 
   ResponsiveLayout = (function() {
-    function ResponsiveLayout(_at_$dom) {
-      this.$dom = _at_$dom;
+    function ResponsiveLayout($dom1) {
+      this.$dom = $dom1;
       this.eventBinding();
     }
 
@@ -36,7 +28,7 @@
         })(this));
       }
       if (this.$dom.not('.res-single-column').length) {
-        return $(document).on('click touchend', ".res-layout[data-active='navigation'] *:not(.res-navigation)", (function(_this) {
+        $(document).on('click touchend', ".res-layout[data-active='navigation'] *:not(.res-navigation)", (function(_this) {
           return function(e) {
             var $el, in_navigation;
             e.stopPropagation();
@@ -50,6 +42,16 @@
           };
         })(this));
       }
+      if (this.$dom.is('#settings-layout')) {
+        return this.$dom.on('click touchend', '.header-menu', function(e) {
+          var $el, $target, is_open;
+          $el = $(e.currentTarget);
+          $el.toggleClass('open');
+          $target = $($el.data('target'));
+          is_open = $el.hasClass('open');
+          return $target.toggle(is_open);
+        });
+      }
     };
 
     return ResponsiveLayout;
@@ -60,6 +62,14 @@
     new ResponsiveLayout($('.res-layout'));
   }
 
+  if ($('#datatable').length) {
+    $('#datatable').DataTable({
+      paging: false,
+      searching: false,
+      info: false
+    });
+  }
+
   $('.section-datepicker').find('#datepicker').datepicker({
     orientation: "auto"
   });
@@ -68,6 +78,7 @@
 
   $('.select2-navigation').select2({
     containerCssClass: 'select2-navigation',
+    dropdownCssClass: 'select2-navigation',
     minimumResultsForSearch: -1
   });
 
@@ -98,5 +109,77 @@
       return $('.section-lightbox').find('.carousel-generic').toggleClass('carousel-active', false);
     }
   });
+
+  SelectSwitcher = (function() {
+    function SelectSwitcher($dom) {
+      this.$dom = $dom;
+      this.$select = this.$dom.find('select');
+      this.$prev = this.$dom.find('.prev');
+      this.$next = this.$dom.find('.next');
+      this.eventBinding();
+      this.refreshState();
+    }
+
+    SelectSwitcher.prototype.refreshState = function() {
+      var current_index, options_length;
+      current_index = this.getCurrentIndex();
+      options_length = this.getAllValues().length;
+      this.$prev.removeClass('disabled');
+      this.$next.removeClass('disabled');
+      if (current_index === 0) {
+        return this.$prev.addClass('disabled');
+      } else if ((current_index + 1) === options_length) {
+        return this.$next.addClass('disabled');
+      }
+    };
+
+    SelectSwitcher.prototype.getAllValues = function() {
+      return this.$select.find('option').map(function() {
+        var $option;
+        $option = $(this);
+        return $option.val();
+      });
+    };
+
+    SelectSwitcher.prototype.getCurrentIndex = function() {
+      var allValues, current_value;
+      allValues = this.getAllValues();
+      current_value = this.$select.val();
+      return $.inArray(current_value, allValues);
+    };
+
+    SelectSwitcher.prototype.eventBinding = function() {
+      this.$select.on('change', (function(_this) {
+        return function() {
+          return _this.refreshState();
+        };
+      })(this));
+      return this.$dom.on('click', '[data-switch]:not(".disabled")', (function(_this) {
+        return function(e) {
+          var $el, allValues, current_index, direction, new_index;
+          $el = $(e.currentTarget);
+          direction = $el.data('switch');
+          new_index = 0;
+          allValues = _this.getAllValues();
+          current_index = _this.getCurrentIndex();
+          if (direction === 'prev') {
+            new_index = current_index - 1;
+          } else if (direction === 'next') {
+            new_index = current_index + 1;
+          }
+          if (new_index === -1) {
+            new_index = 0;
+          }
+          _this.$select.val(allValues[new_index]).trigger('change');
+          return _this.refreshState();
+        };
+      })(this));
+    };
+
+    return SelectSwitcher;
+
+  })();
+
+  new SelectSwitcher($('.section-select').find('.select-switcher'));
 
 }).call(this);
