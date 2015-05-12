3efc53661b82e000f9a2ab9c86917b6b9cfa282d
Christian <christianoliff@yahoo.com>
2015-05-12 18:20:36 +0900

    Update anchor.js to v0.4.0
    Update anchor.js to latest version
    https://raw.githubusercontent.com/bryanbraun/anchorjs/master/anchor.js

--- docs/assets/js/vendor/anchor.js
+++ docs/assets/js/vendor/anchor.js
@@ -1,7 +1,7 @@
 /*!
- * AnchorJS - v0.1.0 - 2014-08-17
+ * AnchorJS - v0.4.0 - 2015-04-20
  * https://github.com/bryanbraun/anchorjs
- * Copyright (c) 2014 Bryan Braun; Licensed MIT
+ * Copyright (c) 2015 Bryan Braun; Licensed MIT
  */
 
 function addAnchors(selector) {
@@ -16,6 +16,16 @@ function addAnchors(selector) {
 
   // Select any elements that match the provided selector.
   var elements = document.querySelectorAll(selector);
+  if (elements.length === 0) {
+    // Selector was valid but no elements were found.
+    return false;
+  }
+
+  // Produce a list of existing IDs so we don't generate a duplicate.
+  var elsWithIds = document.querySelectorAll('[id]');
+  var idList = [].map.call(elsWithIds, function assign(el) {
+    return el.id;
+  });
 
   // Loop through the selected elements.
   for (var i = 0; i < elements.length; i++) {
@@ -24,24 +34,53 @@ function addAnchors(selector) {
     if (elements[i].hasAttribute('id')) {
       elementID = elements[i].getAttribute('id');
     } else {
-      // We need to create an ID on our element. First, we find which text selection method is available to the browser.
+      // We need to create an ID on our element. First, we find which text
+      // selection method is available to the browser.
       var textMethod = document.body.textContent ? 'textContent' : 'innerText';
 
       // Get the text inside our element
       var roughText = elements[i][textMethod];
 
-      // Refine it so it makes a good ID. Makes all lowercase and hyphen separated.
-      // Ex. Hello World > hello-world
-      var tidyText = roughText.replace(/\s+/g, '-').toLowerCase();
+      // Refine it so it makes a good ID. Strip out non-safe characters, replace
+      // spaces with hyphens, truncate to 32 characters, and make toLowerCase.
+      //
+      // Example string:                                 // "⚡⚡⚡ Unicode icons are cool--but don't belong in a URL."
+      var tidyText = roughText.replace(/[^\w\s-]/gi, '') // " Unicode icons are cool--but dont belong in a URL"
+                              .replace(/\s+/g, '-')      // "-Unicode-icons-are-cool--but-dont-belong-in-a-URL"
+                              .replace(/-{2,}/g, '-')    // "-Unicode-icons-are-cool-but-dont-belong-in-a-URL"
+                              .substring(0, 32)          // "-Unicode-icons-are-cool-but-dont"
+                              .replace(/^-+|-+$/gm, '')  // "Unicode-icons-are-cool-but-dont"
+                              .toLowerCase();            // "unicode-icons-are-cool-but-dont"
+
+      // Compare our generated ID to existing IDs (and increment it if needed)
+      // before we add it to the page.
+      var index,
+          count = 0,
+          newTidyText = tidyText;
+      do {
+        if (index !== undefined) {
+          newTidyText = tidyText + '-' + count;
+        }
+        // .indexOf is supported in IE9+.
+        index = idList.indexOf(newTidyText);
+        count += 1;
+      } while (index !== -1);
+      index = undefined;
+      idList.push(newTidyText);
 
       // Assign it to our element.
       // Currently the setAttribute element is only supported in IE9 and above.
-      elements[i].setAttribute('id', tidyText);
+      elements[i].setAttribute('id', newTidyText);
 
       // Grab it for use in our anchor.
-      elementID = tidyText;
+      elementID = newTidyText;
     }
-    var anchor = '<a class="anchorjs-link" href="#' + elementID + '"><span class="anchorjs-icon"></span></a>';
+
+    var readableID = elementID.replace(/-/g, ' ');
+    var anchor = '<a class="anchorjs-link" href="#' + elementID + '">' +
+                    '<span class="anchorjs-description">Anchor link for: ' + readableID + '</span>' +
+                    '<span class="anchorjs-icon" aria-hidden="true"></span>' +
+                 '</a>';
 
     elements[i].innerHTML += anchor;
   }
