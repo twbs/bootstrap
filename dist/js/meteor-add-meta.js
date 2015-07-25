6e41d3986dbf735fe9a419d1e41b74774198e3bc
Dean Brettle <dean@brettle.com>
2015-07-24 20:01:43 -0700

    Document how developers can replace meta tags if needed.

--- dist/js/meteor-add-meta.js
+++ dist/js/meteor-add-meta.js
@@ -1,5 +1,9 @@
-Inject.rawHead('bootstrap-meta',
+if (Inject.rawHeads['bootstrap-meta'] === undefined) {
+  Inject.rawHead('bootstrap-meta',
   '  <meta charset="utf-8">\n' +
   '  <meta http-equiv="X-UA-Compatible" content="IE=edge">\n' +
   '  <meta name="viewport" content="width=device-width, initial-scale=1">\n' +
-  '  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->\n');
+  '  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->\n' +
+  '  <!-- If you must replace them, run "meteor add meteorhacks:inject-initial" and add server code like this: -->\n' +
+  '  <!--   Inject.rawHead("bootstrap-meta", "YOUR REPLACEMENT"); -->\n');
+}
