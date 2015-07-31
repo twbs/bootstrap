4e968a57c72f44456264388e728363892ecc81e5
Jonathan Persson <persson.jonathan@gmail.com>
2015-07-31 14:55:02 +0200

    Remove local `that` variable in Carousel.prototype.to and use `$.proxy()` for context binding to stay consistent with the rest of the code base.

--- js/carousel.js
+++ js/carousel.js
@@ -80,12 +80,11 @@
   }
 
   Carousel.prototype.to = function (pos) {
-    var that        = this
     var activeIndex = this.getItemIndex(this.$active = this.$element.find('.item.active'))
 
     if (pos > (this.$items.length - 1) || pos < 0) return
 
-    if (this.sliding)       return this.$element.one('slid.bs.carousel', function () { that.to(pos) }) // yes, "slid"
+    if (this.sliding)       return this.$element.one('slid.bs.carousel', $.proxy(this.to, this, pos)) // yes, "slid"
     if (activeIndex == pos) return this.pause().cycle()
 
     return this.slide(pos > activeIndex ? 'next' : 'prev', this.$items.eq(pos))
