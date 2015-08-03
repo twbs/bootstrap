5b81e49fd945dca342c7e92719c81e4870e45939
Johann-S <johann.servoire@gmail.com>
2015-08-03 22:36:16 +0200

    Fix behaviour of dropup when a click happened

--- js/dropdown.js
+++ js/dropdown.js
@@ -157,7 +157,7 @@
 
   $(document)
     .on('click.bs.dropdown.data-api', clearMenus)
-    .on('click.bs.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
+    .on('click.bs.dropdown.data-api', '.dropdown form, .dropup form', function (e) { e.stopPropagation() })
     .on('click.bs.dropdown.data-api', toggle, Dropdown.prototype.toggle)
     .on('keydown.bs.dropdown.data-api', toggle, Dropdown.prototype.keydown)
     .on('keydown.bs.dropdown.data-api', '.dropdown-menu', Dropdown.prototype.keydown)
