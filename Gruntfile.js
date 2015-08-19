cf8bb8882ed468529f12c616bdf874b9d8f80c8f
Mark Otto <markdotto@gmail.com>
2015-08-18 22:47:26 -0700

    grunt build control for pushing it all

--- Gruntfile.js
+++ Gruntfile.js
@@ -412,6 +412,21 @@ module.exports = function (grunt) {
           }).join(' && ');
         }
       }
+    },
+
+    buildcontrol: {
+      options: {
+        dir: '_gh_pages',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:twbs/derpstrap.git',
+          branch: 'gh-pages'
+        }
+      }
     }
   });
 
