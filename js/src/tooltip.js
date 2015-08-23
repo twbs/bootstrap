2ae5fdf9ddbff98c55e47b81d7150b8d960ad342
Bernardo Gurgel Filho <bernardogfilho@gmail.com>
2015-08-23 01:59:48 -0300

    Convert tooltip's arrows to generated CSS content via :before

--- js/src/tooltip.js
+++ js/src/tooltip.js
@@ -28,7 +28,6 @@ const Tooltip = (($) => {
   const Default = {
     animation   : true,
     template    : '<div class="tooltip" role="tooltip">'
-                + '<div class="tooltip-arrow"></div>'
                 + '<div class="tooltip-inner"></div></div>',
     trigger     : 'hover focus',
     title       : '',
