2aa7cbc1590ee880f847b6057844fc70555d9b77
XhmikosR <xhmikosr@gmail.com>
2015-08-26 17:55:31 +0300

    Gruntfile.js: Remove the now unused htmlmin reference.
    [ci skip]

--- Gruntfile.js
+++ Gruntfile.js
@@ -495,7 +495,7 @@ module.exports = function (grunt) {
   grunt.registerTask('lint-docs-js', ['jscs:assets']);
   grunt.registerTask('docs', ['docs-css', 'docs-js', 'lint-docs-js', 'clean:docs', 'copy:docs']);
 
-  grunt.registerTask('prep-release', ['dist', 'docs', 'jekyll:github', 'htmlmin', 'compress']);
+  grunt.registerTask('prep-release', ['dist', 'docs', 'jekyll:github', 'compress']);
 
   // Publish to GitHub
   grunt.registerTask('publish', ['buildcontrol:pages']);
