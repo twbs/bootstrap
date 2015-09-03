3af4560c672bc3435565499586ae85d4bc159cd1
muzige2000 <muzige2000@gmail.com>
2015-09-03 17:18:53 +0900

    Modals: Fix bad paddingRight calculation; fixes #17399
    We want to sum two numbers, not concatenate their stringifications.
    Closes #17457 by merging a tweaked version of it.

--- js/src/modal.js
+++ js/src/modal.js
@@ -427,7 +427,7 @@ const Modal = (($) => {
 
       if (this._isBodyOverflowing) {
         document.body.style.paddingRight =
-          bodyPadding + `${this._scrollbarWidth}px`
+          `${bodyPadding + this._scrollbarWidth}px`
       }
     }
 
