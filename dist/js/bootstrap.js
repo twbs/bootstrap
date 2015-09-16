30c971af24b962c886f268d3635bd093219b6fb7
Weijing Jay Lin <lwjct@hotmail.com>
2015-09-16 01:34:18 -0700

    Update bootstrap.js
    remove scrollbar padding

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1177,11 +1177,11 @@ if (typeof jQuery === 'undefined') {
   Modal.prototype.setScrollbar = function () {
     var bodyPad = parseInt((this.$body.css('padding-right') || 0), 10)
     this.originalBodyPad = document.body.style.paddingRight || ''
-    if (this.bodyIsOverflowing) this.$body.css('padding-right', bodyPad + this.scrollbarWidth)
+    // if (this.bodyIsOverflowing) this.$body.css('padding-right', bodyPad + this.scrollbarWidth)
   }
 
   Modal.prototype.resetScrollbar = function () {
-    this.$body.css('padding-right', this.originalBodyPad)
+    // this.$body.css('padding-right', this.originalBodyPad)
   }
 
   Modal.prototype.measureScrollbar = function () { // thx walsh
