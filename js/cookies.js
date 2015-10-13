3ce2c4f78bcbc377395b594bd154247c88ea8e21
Gustavo Fulton <ferna194@msu.edu>
2015-10-13 19:59:29 -0400

    Fixed syntax errors

--- js/cookies.js
+++ js/cookies.js
@@ -13,28 +13,32 @@
   	// ================
 
   	function createCookie(name,value,days) {
+  		var expires = '';
 		if (days) {
 			var date = new Date();
 			date.setTime(date.getTime()+(days*24*60*60*1000));
-			var expires = "; expires="+date.toGMTString();
+			expires = '; expires='+date.toGMTString();
 		}
-		else var expires = "";
-		document.cookie = name+"="+value+expires+"; path=/";
+		document.cookie = name+'='+value+expires+'; path=/';
 	}
 
 	function readCookie(name) {
-		var nameEQ = name + "=";
-		var ca = document.cookie.split(';');
-		for(var i=0;i < ca.length;i++) {
-			var c = ca[i];
-			while (c.charAt(0)==' ') c = c.substring(1,c.length);
-			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		var c_name = name + '=';
+		var c_split = document.cookie.split(';');
+		for(var i=0;i < c_split.length;i++) {
+			var c = c_split[i];
+			while (c.charAt(0)==' ') {
+				c = c.substring(1,c.length);
+				if (c.indexOf(c_name) === 0) {
+					return c.substring(c_name.length,c.length);
+				}
+			}
 		}
 		return null;
 	}
 
 	function eraseCookie(name) {
-		createCookie(name,"",-1);
+		createCookie(name,'',-1);
 	}
 
 }
