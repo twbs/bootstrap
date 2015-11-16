add3ce38f78094787634f6a145ad14bdfad57bb2
XhmikosR <xhmikosr@gmail.com>
2015-11-16 09:21:02 +0200

    Revert "Make jQuery 2.0 the minimum supported version."
    This partially reverts commit 9eded912a05082a89ef30b899bbe659cb9ce254c.

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,8 +63,8 @@ module.exports = function (grunt) {
                  '}\n',
     jqueryVersionCheck: '+function ($) {\n' +
                         '  var version = $.fn.jquery.split(\' \')[0].split(\'.\')\n' +
-                        '  if (version[0] !== \'2\') {\n' +
-                        '    throw new Error(\'Bootstrap\\\'s JavaScript requires jQuery version 2.x.x\')\n' +
+                        '  if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1)) {\n' +
+                        '    throw new Error(\'Bootstrap\\\'s JavaScript requires jQuery version 1.9.1 or higher\')\n' +
                         '  }\n' +
                         '}(jQuery);\n\n',
 
