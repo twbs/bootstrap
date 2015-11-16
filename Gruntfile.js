1b645ef686c61fef6be87c5ef15411cbeb869702
XhmikosR <xhmikosr@gmail.com>
2015-11-16 09:32:59 +0200

    Update devDependencies.
    Adapt uglify for the new version.

--- Gruntfile.js
+++ Gruntfile.js
@@ -202,7 +202,7 @@ module.exports = function (grunt) {
           warnings: false
         },
         mangle: true,
-        preserveComments: 'some'
+        preserveComments: /^!|@preserve|@license|@cc_on/i
       },
       core: {
         src: '<%= concat.bootstrap.dest %>',
