cf3f8e0d580888ec9459270ed67dc86c13f5b41a
Chris Rebert <code@chrisrebert.com>
2016-01-01 12:22:03 -0800

    Update copyright years to 2016
    [ci skip]

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 /*!
  * Bootstrap's Gruntfile
  * http://getbootstrap.com
- * Copyright 2013-2015 Twitter, Inc.
+ * Copyright 2013-2016 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
