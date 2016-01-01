631a629eba5e9ea1907df567cb35684732d879af
Chris Rebert <code@chrisrebert.com>
2016-01-01 12:22:03 -0800

    Update copyright years to 2016
    [ci skip]

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,7 +2,7 @@
  * Bootstrap: dropdown.js v3.3.6
  * http://getbootstrap.com/javascript/#dropdowns
  * ========================================================================
- * Copyright 2011-2015 Twitter, Inc.
+ * Copyright 2011-2016 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
