d0d8bf07ae5a3914e7d88df8089fefcded71d24b
Fabrice Daugan <fabrice.daugan@gmail.com>
2016-02-14 17:39:16 +0100

    Update button.js

--- js/button.js
+++ js/button.js
@@ -114,7 +114,6 @@
       if (!($(e.target).is('input[type="radio"]') || $(e.target).is('input[type="checkbox"]'))) {
         // Prevent double click on radios, and the double selections (so cancellation) on checkboxes
         e.preventDefault()
-        
         // The target component still receive the focus
         $(e.target).focus()
       }
