a27dfca9db0b75dde950091e8319ac702e2f3cd9
XhmikosR <xhmikosr@gmail.com>
2016-02-28 04:02:37 +0200

    Bump version in customizer.js.
    [ci skip]

--- docs/assets/js/src/customizer.js
+++ docs/assets/js/src/customizer.js
@@ -1,6 +1,6 @@
 /*!
  * Bootstrap Customizer (http://getbootstrap.com/customize/)
- * Copyright 2011-2015 Twitter, Inc.
+ * Copyright 2011-2016 Twitter, Inc.
  *
  * Licensed under the Creative Commons Attribution 3.0 Unported License. For
  * details, see https://creativecommons.org/licenses/by/3.0/.
@@ -13,7 +13,7 @@ window.onload = function () { // wait for load in a dumb way because B-0
   'use strict';
 
   var cw = '/*!\n' +
-           ' * Bootstrap v3.3.5 (http://getbootstrap.com)\n' +
+           ' * Bootstrap v3.3.6 (http://getbootstrap.com)\n' +
            ' * Copyright 2011-' + new Date().getFullYear() + ' Twitter, Inc.\n' +
            ' * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)\n' +
            ' */\n\n'
