b26377b5b615626079ea8b2ed33e6bfe1b6e405b
XhmikosR <xhmikosr@gmail.com>
2016-03-03 10:18:19 +0200

    Fix JSHint errors.
    [skip sauce]

--- docs/assets/js/src/customizer.js
+++ docs/assets/js/src/customizer.js
@@ -6,8 +6,7 @@
  * details, see https://creativecommons.org/licenses/by/3.0/.
  */
 
-/* jshint es3:false */
-/* global JSZip, less, autoprefixer, saveAs, UglifyJS, __configBridge, __js, __less, __fonts */
+/* global JSON, JSZip, less, autoprefixer, saveAs, UglifyJS, __configBridge, __js, __less, __fonts */
 
 window.onload = function () { // wait for load in a dumb way because B-0
   'use strict';
