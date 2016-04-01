a9efd66b8e5d2f930b4e96a0e7dd54963d0e5131
Greg Sheremeta <gshereme@redhat.com>
2016-04-01 15:15:22 -0400

    fix memory leak in tooltip $element. Fixes #17973

--- js/tooltip.js
+++ js/tooltip.js
@@ -317,9 +317,11 @@
 
     function complete() {
       if (that.hoverState != 'in') $tip.detach()
-      that.$element
-        .removeAttr('aria-describedby')
-        .trigger('hidden.bs.' + that.type)
+      if (that.$element) {
+        that.$element
+          .removeAttr('aria-describedby')
+          .trigger('hidden.bs.' + that.type)
+      }
       callback && callback()
     }
 
@@ -478,6 +480,7 @@
       that.$tip = null
       that.$arrow = null
       that.$viewport = null
+      that.$element = null
     })
   }
 
