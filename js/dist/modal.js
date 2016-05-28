2ce281ee28a3e0871c054b865bd655c8d9f582c3
Pierre-Denis Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2016-05-28 02:39:27 -0400

    Unit tests

--- js/dist/modal.js
+++ js/dist/modal.js
@@ -222,6 +222,7 @@ var Modal = (function ($) {
         }
 
         this._element.style.display = 'block';
+        this._element.removeAttribute('aria-hidden');
         this._element.scrollTop = 0;
 
         if (transition) {
@@ -293,6 +294,7 @@ var Modal = (function ($) {
         var _this5 = this;
 
         this._element.style.display = 'none';
+        this._element.setAttribute('aria-hidden', 'true');
         this._showBackdrop(function () {
           $(document.body).removeClass(ClassName.OPEN);
           _this5._resetAdjustments();
@@ -398,7 +400,7 @@ var Modal = (function ($) {
         }
 
         if (this._isBodyOverflowing && !isModalOverflowing) {
-          this._element.style.paddingRight = this._scrollbarWidth + 'px~';
+          this._element.style.paddingRight = this._scrollbarWidth + 'px';
         }
       }
     }, {
