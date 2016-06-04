590b5d29fa4462b780e3720633d457fad00e1815
Manuellama <manuellamapaniagua@yahoo.es>
2016-06-04 13:27:59 +0200

    Fix bug #20027
    How to Fix Multiple Tabs can become .active at once #20027
    
    Original Fix here in this js:
    https://www.dropbox.com/s/eg235dwr8clpny8/bootstrap.js?dl=0
    
    I changed var into let. I do not know if this is the way to do it. Please check it.

--- js/src/tab.js
+++ js/src/tab.js
@@ -77,6 +77,10 @@ const Tab = (($) => {
     // public
 
     show() {
+    let tablist = this._element.closest('.nav-tabs');
+    if (tablist.getAttribute('role') == 'tablist') {
+    $(tablist).find('.active').removeClass('active');
+    }
       if (this._element.parentNode &&
          (this._element.parentNode.nodeType === Node.ELEMENT_NODE) &&
          ($(this._element).hasClass(ClassName.ACTIVE))) {
@@ -192,8 +196,13 @@ const Tab = (($) => {
         }
 
         active.setAttribute('aria-expanded', false)
-      }
-
+      }         
+   for (let i = 0; i < element.parentNode.children.length; i++) {
+   let tab = element.parentNode.children[i];
+   if (tab.getAttribute('role') == 'tab') {
+   $(tab).removeClass(ClassName.ACTIVE);
+   }
+   }
       $(element).addClass(ClassName.ACTIVE)
       element.setAttribute('aria-expanded', true)
 
