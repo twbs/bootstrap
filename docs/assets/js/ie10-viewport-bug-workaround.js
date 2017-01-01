6b001bb34138672bf3711e6e0d07010a683dff32
Quan You <cquanu@users.noreply.github.com>
2017-01-01 13:25:27 +0800

    Update ie10-viewport-bug-workaround.js year

--- docs/assets/js/ie10-viewport-bug-workaround.js
+++ docs/assets/js/ie10-viewport-bug-workaround.js
@@ -1,7 +1,7 @@
 /*!
  * IE10 viewport hack for Surface/desktop Windows 8 bug
- * Copyright 2014-2015 The Bootstrap Authors
- * Copyright 2014-2015 Twitter, Inc.
+ * Copyright 2014-2017 The Bootstrap Authors
+ * Copyright 2014-2017 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
