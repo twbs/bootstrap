9fc54f89f4d7a15c4118835b7eeae810cc8c5c0a
Mark Otto <markdotto@gmail.com>
2017-01-02 11:54:28 -0800

    grunt

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1699,7 +1699,7 @@ var Modal = function ($) {
     DIALOG: '.modal-dialog',
     DATA_TOGGLE: '[data-toggle="modal"]',
     DATA_DISMISS: '[data-dismiss="modal"]',
-    FIXED_CONTENT: '.navbar-fixed-top, .navbar-fixed-bottom, .is-fixed'
+    FIXED_CONTENT: '.fixed-top, .fixed-bottom, .is-fixed, .sticky-top'
   };
 
   /**
