6a4bd2ef41352e1e00f0e9292ba1b6237a1260f7
Pierre-Denis Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-01-06 17:06:37 -0500

    Revert "Fix mobile backdrop"
    This reverts commit b8541c45dd529c9f5a6898f9da6b1ac6b8241667.

--- js/dist/dropdown.js
+++ js/dist/dropdown.js
@@ -88,6 +88,15 @@ var Dropdown = function ($) {
         return false;
       }
 
+      if ('ontouchstart' in document.documentElement && !$(parent).closest(Selector.NAVBAR_NAV).length) {
+
+        // if mobile we use a backdrop because click events don't delegate
+        var dropdown = document.createElement('div');
+        dropdown.className = ClassName.BACKDROP;
+        $(dropdown).insertBefore(this);
+        $(dropdown).on('click', Dropdown._clearMenus);
+      }
+
       var relatedTarget = {
         relatedTarget: this
       };
@@ -97,16 +106,6 @@ var Dropdown = function ($) {
 
       if (showEvent.isDefaultPrevented()) {
         return false;
-      }
-
-      // set the backdrop only if the dropdown menu will be opened
-      if ('ontouchstart' in document.documentElement && !$(parent).closest(Selector.NAVBAR_NAV).length) {
-
-        // if mobile we use a backdrop because click events don't delegate
-        var dropdown = document.createElement('div');
-        dropdown.className = ClassName.BACKDROP;
-        $(dropdown).insertBefore(this);
-        $(dropdown).on('click', Dropdown._clearMenus);
       }
 
       this.focus();
@@ -155,6 +154,11 @@ var Dropdown = function ($) {
         return;
       }
 
+      var backdrop = $(Selector.BACKDROP)[0];
+      if (backdrop) {
+        backdrop.parentNode.removeChild(backdrop);
+      }
+
       var toggles = $.makeArray($(Selector.DATA_TOGGLE));
 
       for (var i = 0; i < toggles.length; i++) {
@@ -175,12 +179,6 @@ var Dropdown = function ($) {
         $(parent).trigger(hideEvent);
         if (hideEvent.isDefaultPrevented()) {
           continue;
-        }
-
-        // remove backdrop only if the dropdown menu will be hidden
-        var backdrop = $(parent).find(Selector.BACKDROP)[0];
-        if (backdrop) {
-          backdrop.parentNode.removeChild(backdrop);
         }
 
         toggles[i].setAttribute('aria-expanded', 'false');
