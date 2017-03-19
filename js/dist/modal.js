ae50bb784506b4aa6b79598c2000c1d20843b5fd
Mark Otto <markdotto@gmail.com>
2017-03-18 18:47:57 -0700

    grunt

--- js/dist/modal.js
+++ js/dist/modal.js
@@ -436,7 +436,7 @@ var Modal = function ($) {
       var scrollDiv = document.createElement('div');
       scrollDiv.className = ClassName.SCROLLBAR_MEASURER;
       document.body.appendChild(scrollDiv);
-      var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+      var scrollbarWidth = scrollDiv.getBoundingClientRect().width - scrollDiv.clientWidth;
       document.body.removeChild(scrollDiv);
       return scrollbarWidth;
     };
