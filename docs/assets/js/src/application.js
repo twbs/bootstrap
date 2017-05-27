1ba78bf28db471184717a04f27bca145402c5ee1
Mark Otto <markdotto@gmail.com>
2017-05-27 14:36:23 -0700

    move the js to application.js, update the options to use bootstrap's .dropdown-menu classes

--- docs/assets/js/src/application.js
+++ docs/assets/js/src/application.js
@@ -53,6 +53,32 @@
       $(this).siblings('.progress').find('.progress-bar-striped').toggleClass('progress-bar-animated')
     })
 
+    // Algolia search
+    var client = algoliasearch("YHC4ZXLJL7", "250a854084e2ce05558580cf48695b56")
+    var index = client.initIndex('bootstrap_docs')
+
+    autocomplete('#bd-docs-search', {
+      hint: false,
+      debug: true,
+      // templates: {
+      //   dropdownMenu: '#bd-docs-search-template'
+      // },
+      cssClasses: {
+        noPrefix: true,
+        suggestion: 'dropdown-item'
+      }}, {
+      source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+      // value to be displayed in input control after user's suggestion selection
+      displayKey: 'title',
+      //hash of templates used when rendering dataset
+      templates: {
+        //'suggestion' templating function used to render a single suggestion
+        suggestion: function(suggestion) {
+          return '<span>' + suggestion._highlightResult.title.value + '</span>';
+        }
+      }
+    })
+
     // Insert copy to clipboard button before .highlight
     $('.highlight').each(function () {
       var btnHtml = '<div class="bd-clipboard"><button class="btn-clipboard" title="Copy to clipboard">Copy</button></div>'
