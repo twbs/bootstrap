c5af5a91532d1f7ae9d16bf8025721224c6d7f8b
Gwiazda Krzysztof <krzysztof.gwiazda.m@gmail.com>
2017-06-19 18:10:37 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -170,7 +170,8 @@
     var that = this
     this.$element.hide()
     this.backdrop(function () {
-      that.$body.removeClass('modal-open')
+      if ($('.modal:visible').length == 0)
+        that.$body.removeClass('modal-open')
       that.resetAdjustments()
       that.resetScrollbar()
       that.$element.trigger('hidden.bs.modal')
