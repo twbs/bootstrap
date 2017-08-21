a4a1c80a8e84f240276366003e4ace67ede581b2
Johann-S <johann.servoire@gmail.com>
2017-08-21 17:43:04 +0200

    Add CustomEvent polyfill and a working preventDefault for IE

--- js/src/alert.js
+++ js/src/alert.js
@@ -67,7 +67,7 @@ const Alert = (($) => {
 
       const customEvent = this._triggerCloseEvent(rootElement)
 
-      if (customEvent.defaultPrevented) {
+      if (customEvent.isDefaultPrevented()) {
         return
       }
 
