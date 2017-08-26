c456c717286edda897a9f99637b75c7db25a8515
Johann-S <johann.servoire@gmail.com>
2017-08-26 05:43:06 -0400

    Fix XSS in Alert, Carousel, Collapse, Dropdown and Modal
    (cherry picked from commit 29f9237)

--- js/modal.js
+++ js/modal.js
@@ -322,7 +322,10 @@
   $(document).on('click.bs.modal.data-api', '[data-toggle="modal"]', function (e) {
     var $this   = $(this)
     var href    = $this.attr('href')
-    var $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))) // strip for ie7
+    var target  = $this.attr('data-target') ||
+      (href && href.replace(/.*(?=#[^\s]+$)/, '')) // strip for ie7
+
+    var $target = $(document).find(target)
     var option  = $target.data('bs.modal') ? 'toggle' : $.extend({ remote: !/#/.test(href) && href }, $target.data(), $this.data())
 
     if ($this.is('a')) e.preventDefault()
