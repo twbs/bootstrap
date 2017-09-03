27c4f3eb3c3d2c1bdd8701a00a46218031aa00a8
Johann-S <johann.servoire@gmail.com>
2017-09-03 16:15:03 +0200

    Button - divide focus and blur events

--- js/src/button.js
+++ js/src/button.js
@@ -38,8 +38,8 @@ const Button = (() => {
 
   const Event = {
     CLICK_DATA_API      : `click${EVENT_KEY}${DATA_API_KEY}`,
-    FOCUS_BLUR_DATA_API : `focus${EVENT_KEY}${DATA_API_KEY} ` +
-                            `blur${EVENT_KEY}${DATA_API_KEY}`
+    FOCUS_DATA_API      : `focus${EVENT_KEY}${DATA_API_KEY}`,
+    BLUR_DATA_API       : `blur${EVENT_KEY}${DATA_API_KEY}`
   }
 
   /**
@@ -161,13 +161,15 @@ const Button = (() => {
     })
 
   EventHandler.on(document,
-    Event.FOCUS_BLUR_DATA_API, Selector.DATA_TOGGLE_CARROT, (event) => {
+    Event.FOCUS_DATA_API, Selector.DATA_TOGGLE_CARROT, (event) => {
       const button = SelectorEngine.closest(event.target, Selector.BUTTON)
-      if (/^focus(in)?$/.test(event.type)) {
-        button.classList.add(ClassName.FOCUS)
-      } else {
-        button.classList.remove(ClassName.FOCUS)
-      }
+      button.classList.add(ClassName.FOCUS)
+    })
+
+  EventHandler.on(document,
+    Event.BLUR_DATA_API, Selector.DATA_TOGGLE_CARROT, (event) => {
+      const button = SelectorEngine.closest(event.target, Selector.BUTTON)
+      button.classList.remove(ClassName.FOCUS)
     })
 
   /**
