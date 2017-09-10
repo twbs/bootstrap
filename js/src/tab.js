9af2ffb8383d4762dd8aa647b1e1e77b71861a76
patrickhlauke <redux@splintered.co.uk>
2017-09-10 21:11:33 +0100

    Change tab.js to use aria-selected rather than aria-expanded

--- js/src/tab.js
+++ js/src/tab.js
@@ -196,11 +196,11 @@ const Tab = (() => {
           $(dropdownChild).removeClass(ClassName.ACTIVE)
         }
 
-        active.setAttribute('aria-expanded', false)
+        active.setAttribute('aria-selected', false)
       }
 
       $(element).addClass(ClassName.ACTIVE)
-      element.setAttribute('aria-expanded', true)
+      element.setAttribute('aria-selected', true)
 
       if (isTransitioning) {
         Util.reflow(element)
