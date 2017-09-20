7c1d0a1097657fb604a69cd47375a7e94c0ced93
Johann-S <johann.servoire@gmail.com>
2017-09-20 14:19:10 +0200

    Wrap our objects into IIFE

--- js/src/dom/data.js
+++ js/src/dom/data.js
@@ -5,56 +5,66 @@
  * --------------------------------------------------------------------------
  */
 
-const mapData = (() => {
-  const storeData = {}
-  let id = 1
-  return {
-    set(element, key, data) {
-      if (typeof element.key === 'undefined') {
-        element.key = {
-          key,
-          id
+const Data = (() => {
+
+  /**
+   * ------------------------------------------------------------------------
+   * Constants
+   * ------------------------------------------------------------------------
+   */
+
+
+  const mapData = (() => {
+    const storeData = {}
+    let id = 1
+    return {
+      set(element, key, data) {
+        if (typeof element.key === 'undefined') {
+          element.key = {
+            key,
+            id
+          }
+        }
+
+        storeData[id] = data
+        id++
+      },
+      get(element, key) {
+        if (typeof element === 'undefined' || typeof element.key === 'undefined') {
+          return null
+        }
+
+        const keyProperties = element.key
+        if (keyProperties.key === key) {
+          return storeData[keyProperties.id]
+        }
+        return null
+      },
+      delete(element, key) {
+        if (typeof element.key === 'undefined') {
+          return
+        }
+
+        const keyProperties = element.key
+        if (keyProperties.key === key) {
+          delete storeData[keyProperties.id]
+          delete element.key
         }
       }
+    }
+  })()
 
-      storeData[id] = data
-      id++
+  return {
+    setData(instance, key, data) {
+      mapData.set(instance, key, data)
     },
-    get(element, key) {
-      if (typeof element === 'undefined' || typeof element.key === 'undefined') {
-        return null
-      }
-
-      const keyProperties = element.key
-      if (keyProperties.key === key) {
-        return storeData[keyProperties.id]
-      }
-      return null
+    getData(instance, key) {
+      return mapData.get(instance, key)
     },
-    delete(element, key) {
-      if (typeof element.key === 'undefined') {
-        return
-      }
-
-      const keyProperties = element.key
-      if (keyProperties.key === key) {
-        delete storeData[keyProperties.id]
-        delete element.key
-      }
+    removeData(instance, key) {
+      mapData.delete(instance, key)
     }
   }
 })()
-
-const Data = {
-  setData(instance, key, data) {
-    mapData.set(instance, key, data)
-  },
-  getData(instance, key) {
-    return mapData.get(instance, key)
-  },
-  removeData(instance, key) {
-    mapData.delete(instance, key)
-  }
-}
 
 export default Data
