54d45072810f5cda4f3c75eba6d9acd70274a963
Johann-S <johann.servoire@gmail.com>
2017-09-28 18:18:20 +0300

    Use sauce_browsers.json.

--- build/saucelabs-unit-test.js
+++ build/saucelabs-unit-test.js
@@ -1,4 +1,12 @@
+'use strict'
+
+const path = require('path')
 const JSUnitSaucelabs = require('jsunitsaucelabs')
+
+// Docs: https://wiki.saucelabs.com/display/DOCS/Platform+Configurator
+// Mac Opera is not currently supported by Sauce Labs
+// Win Opera 15+ is not currently supported by Sauce Labs
+// iOS Chrome is not currently supported by Sauce Labs
 
 const jsUnitSaucelabs = new JSUnitSaucelabs({
   username: process.env.SAUCE_USERNAME,
@@ -6,49 +14,73 @@ const jsUnitSaucelabs = new JSUnitSaucelabs({
   build:    process.env.TRAVIS_JOB_ID
 })
 
+const testURL      = 'http://localhost:3000/js/tests/index.html?hidepassed'
+const browsersFile = require(path.resolve(__dirname, './sauce_browsers.json'))
+let jobsDone       = 0
+let jobsSuccess    = 0
 
-// TODO : get all the browsers in sauce_browsers.yml
-// Docs: https://wiki.saucelabs.com/display/DOCS/Platform+Configurator
-// Mac Opera is not currently supported by Sauce Labs
-// Win Opera 15+ is not currently supported by Sauce Labs
-// iOS Chrome is not currently supported by Sauce Labs
-
-const testURL = 'http://localhost:3000/js/tests/index.html?hidepassed'
-
-jsUnitSaucelabs.start([
-  ['Windows 8', 'internet explorer', '10']
-], testURL, 'qunit', (error, success) => {
-  if (typeof success !== 'undefined') {
-    const taskIds = success['js tests']
-    if (!taskIds || !taskIds.length) {
-      throw new Error('Error starting tests through SauceLabs API')
-    }
-
-    const waitingCallback = (error, success) => {
-      if (error) {
-        console.error(error)
-        return
-      }
-
-      if (typeof success !== 'undefined') {
-        if (!success.completed) {
-          jsUnitSaucelabs.getStatus(taskIds[0], waitingCallback)
-        } else {
-          const test = success['js tests'][0]
-          let passed = false
-          if (test.result !== null) {
-            passed = test.result.total === test.result.passed
-          }
-          console.log(`Tested ${testURL}`)
-          console.log(`Platform: ${test.platform.join(',')}`)
-          console.log(`Passed: ${passed.toString()}`)
-          console.log(`Url ${test.url}`)
-        }
-      }
-    }
-
-    taskIds.forEach((id) => {
-      jsUnitSaucelabs.getStatus(id, waitingCallback)
-    })
+const waitingCallback = (error, body, id) => {
+  if (error) {
+    console.error(error)
+    process.exit(1)
+    return
   }
+
+  if (typeof body !== 'undefined') {
+    if (!body.completed) {
+      setTimeout(() => {
+        jsUnitSaucelabs.getStatus(id, (error, body) => {
+          waitingCallback(error, body, id)
+        })
+      }, 2000)
+    } else {
+      const test = body['js tests'][0]
+      let passed = false
+
+      if (test.result !== null) {
+        passed = test.result.total === test.result.passed
+      }
+
+      console.log(`Tested ${testURL}`)
+      console.log(`Platform: ${test.platform.join(',')}`)
+      console.log(`Passed: ${passed.toString()}`)
+      console.log(`Url ${test.url} \n`)
+
+      if (passed) {
+        jobsSuccess++
+      }
+      jobsDone++
+
+      // Exit
+      if (jobsDone === browsersFile.length - 1) {
+        jsUnitSaucelabs.stop()
+        process.exit(jobsDone === jobsSuccess ? 0 : 1)
+      }
+    }
+  }
+}
+
+jsUnitSaucelabs.on('tunnelCreated', () => {
+  browsersFile.forEach((tmpBrowser) => {
+    const broPlatform = typeof tmpBrowser.platform === 'undefined' ? tmpBrowser.platformName : tmpBrowser.platform
+    const arrayBro    = [broPlatform, tmpBrowser.browserName, tmpBrowser.version]
+    jsUnitSaucelabs.start([arrayBro], testURL, 'qunit', (error, success) => {
+      if (typeof success !== 'undefined') {
+        const taskIds = success['js tests']
+
+        if (!taskIds || !taskIds.length) {
+          throw new Error('Error starting tests through SauceLabs API')
+        }
+
+        taskIds.forEach((id) => {
+          jsUnitSaucelabs.getStatus(id, (error, body) => {
+            waitingCallback(error, body, id)
+          })
+        })
+      } else {
+        console.error(error)
+      }
+    })
+  })
 })
+jsUnitSaucelabs.initTunnel()
