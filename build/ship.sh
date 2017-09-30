a455a3e37414c00b0f100496d5e31f457db476b2
Mark Otto <markdotto@gmail.com>
2017-09-30 14:05:45 -0700

    tweak output

--- build/ship.sh
+++ build/ship.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 #
-# Before using, clone second version of Bootstrap into `bs-docs` folder and switch to `gh-pages` branch there.
+# Usage
+# ---------------
+# 1. Clone second version of Bootstrap in sibling directory named `bs-docs`.
+# 2. Within `bs-docs` copy, switch to `gh-pages` branch.
+# 3. Pull latest, re-bundle, re-npm.
+# 4. Run script.
 
 red=$'\e[1;31m'
 green=$'\e[1;32m'
@@ -18,27 +23,42 @@ if [[ $# -lt 1 ]]; then
 fi
 
 # Pulling latest changes, just to be sure
-printf "\n${magenta}🚧  Pulling latest changes...\n\n${end}"
+printf "\n${magenta}=======================================================${end}"
+printf "\n${magenta}Pulling latest changes...${end}"
+printf "\n${magenta}=======================================================\n\n${end}"
 git pull origin v4-dev
 
 # Update version number
-printf "\n${magenta}🚧  Updating version number...\n${end}"
+printf "\n${magenta}=======================================================${end}"
+printf "\n${magenta}Updating version number...${end}"
+printf "\n${magenta}=======================================================\n${end}"
 npm run release-version $current_version $1
 
 # Compile latest CSS and JS
-printf "\n${magenta}🚧  Compile latest CSS and JS...\n${end}"
+printf "\n${magenta}=======================================================${end}"
+printf "\n${magenta}Compile latest CSS and JS...${end}"
+printf "\n${magenta}=======================================================\n${end}"
 npm run dist
 
 # Compress the dist files
-printf "\n${magenta}🚧  Compressing the dist files...\n${end}"
+printf "\n${magenta}=======================================================${end}"
+printf "\n${magenta}Compressing the dist files...${end}"
+printf "\n${magenta}=======================================================\n${end}"
 npm run release-zip
 
 # Compile the docs
-printf "\n${magenta}🚧  Compile hosted documentation...\n${end}"
+printf "\n${magenta}=======================================================${end}"
+printf "\n${magenta}Compile hosted documentation...${end}"
+printf "\n${magenta}=======================================================\n${end}"
 npm run docs-github
 
 # Copy the contents of the built docs site over to `bs-docs` repo
-printf "\n${magenta}🚧  Copy it over...\n${end}"
-cp -rf _gh_pages/. ../alt-repo/
+printf "\n${magenta}=======================================================${end}"
+printf "\n${magenta}Copy it over...${end}"
+printf "\n${magenta}=======================================================\n${end}"
+cp -rf _gh_pages/. ../bs-docs/
+printf "\nDone!\n"
 
-printf "\n${green}🏁  Success, $1 is ready to review and publish.\n${end}"
+printf "\n${green}=======================================================${end}"
+printf "\n${green}Success, $1 is ready to review and publish.${end}"
+printf "\n${green}=======================================================\n\n${end}"
