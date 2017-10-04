47968c8126027c079e9a54c71a63b5dfaf3c0bda
lucascono <lucasconobanegas@gmail.com>
2017-10-04 08:55:40 -0300

    Unit test for modal bug fix
    Unit test for pull request #24240

--- js/tests/unit/modal.js
+++ js/tests/unit/modal.js
@@ -673,4 +673,17 @@ $(function () {
     $toggleBtn.trigger('click')
     setTimeout(done, 500)
   })
+
+  QUnit.test('should not try to open a modal which is already visible', function (assert) {
+    assert.expect(1)
+    var done = assert.async()
+    var count = 0
+
+    $('<div id="modal-test"/>').on('shown.bs.modal', function () {
+      count++
+    }).on('hidden.bs.modal', function () {
+      assert.strictEqual(count, 1, 'show() runs only once')
+      done()
+    }).bootstrapModal('show').bootstrapModal('show').bootstrapModal('hide')
+  })
 })
