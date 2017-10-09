6cc1555b998331ebbd2f1849744552505b3b84e2
XhmikosR <xhmikosr@gmail.com>
2017-10-09 10:48:35 +0300

    Remove unused code from postcss.config.js.
    We actually want to generate a sourcemap for docs.min.css.

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = (ctx) => ({
-  map: ctx.file.dirname.startsWith('docs') || ctx.file.dirname.includes('examples') ? false : {
+  map: ctx.file.dirname.includes('examples') ? false : {
     inline: false,
     annotation: true,
     sourcesContent: true
