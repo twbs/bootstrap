794ad407a58329e88adce794a36aa368251bc6a1
Stefanie Axinger <sa@it-agenten.com>
2017-10-12 11:07:58 +0200

    Version bump

--- dist/js/bootstrap.min.js
+++ dist/js/bootstrap.min.js
@@ -1,5 +1,5 @@
 /*!
- * Bootstrap v4.0.0-alpha.6-joblocal.31 (https://getbootstrap.com)
+ * Bootstrap v4.0.0-alpha.6-joblocal.32 (https://getbootstrap.com)
  * Copyright 2011-2017 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
