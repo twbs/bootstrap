dfc01055c6d48431ccbf6e60a60a9d9e94894a14
Johannes Hofmann <johannes_hofmann@me.com>
2017-10-23 10:26:35 +0200

    Version bump

--- dist/js/bootstrap.min.js
+++ dist/js/bootstrap.min.js
@@ -1,5 +1,5 @@
 /*!
- * Bootstrap v4.0.0-alpha.6-joblocal.33 (https://getbootstrap.com)
+ * Bootstrap v4.0.0-alpha.6-joblocal.34 (https://getbootstrap.com)
  * Copyright 2011-2017 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
