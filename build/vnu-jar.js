930783cb242a8b94ac14bc93d1a1974c6237f218
XhmikosR <xhmikosr@gmail.com>
2017-10-30 17:34:02 +0200

    Re-enable vnu-jar's warnings and suppress the ones we don't need now.

--- build/vnu-jar.js
+++ build/vnu-jar.js
@@ -18,21 +18,27 @@ childProcess.exec('java -version', (error) => {
     return
   }
 
+  // vnu-jar accepts multiple ignores joined with a `|`
   const ignores = [
     'Attribute “autocomplete” is only allowed when the input type is “color”, “date”, “datetime-local”, “email”, “hidden”, “month”, “number”, “password”, “range”, “search”, “tel”, “text”, “time”, “url”, or “week”.',
     'Attribute “autocomplete” not allowed on element “button” at this point.',
     'Attribute “title” not allowed on element “circle” at this point.',
     'Bad value “tablist” for attribute “role” on element “nav”.',
+    // We use holder.js with `data-src` and no `src`; we could work around this, not sure it's worth it.
     'Element “img” is missing required attribute “src”.',
-    'Element “legend” not allowed as child of element “div” in this context.*'
+    'Element “legend” not allowed as child of element “div” in this context.*',
+    'The “datetime-local” input type is not supported in all browsers. Please be sure to test, and consider using a polyfill.',
+    // The next one we are using it because IE11 doesn't recognise <main>.
+    // So, redundant for modern browsers, but not invalid.
+    'The “main” role is unnecessary for element “main”.'
   ].join('|')
 
   const args = [
     '-jar',
     vnu,
     '--asciiquotes',
-    '--errors-only',
     '--skip-non-html',
+    '--Werror',
     `--filterpattern "${ignores}"`,
     '_gh_pages/',
     'js/tests/'
