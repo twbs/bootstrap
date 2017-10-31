2bbb9e0efc9ea70c27dc0d9ba70f32a72b0977c0
XhmikosR <xhmikosr@gmail.com>
2017-10-31 18:46:26 +0200

    Shorten ignores - add more comments.

--- build/vnu-jar.js
+++ build/vnu-jar.js
@@ -14,28 +14,31 @@ const vnu = require('vnu-jar')
 
 childProcess.exec('java -version', (error, stdout, stderr) => {
   if (error) {
-    console.error('Skipping HTML lint test; Java is missing.')
+    console.error('Skipping vnu-jar test; Java is missing.')
     return
   }
 
   const is32bitJava = !stderr.match(/64-Bit/)
 
-  // vnu-jar accepts multiple ignores joined with a `|`
+  // vnu-jar accepts multiple ignores joined with a `|`.
+  // Also note that the ignores are regular expressions.
   const ignores = [
     // "autocomplete" is included in <button> and checkboxes and radio <input>s due to
     // Firefox's non-standard autocomplete behavior - see https://bugzilla.mozilla.org/show_bug.cgi?id=654072
-    'Attribute “autocomplete” is only allowed when the input type is “color”, “date”, “datetime-local”, “email”, “hidden”, “month”, “number”, “password”, “range”, “search”, “tel”, “text”, “time”, “url”, or “week”.',
+    'Attribute “autocomplete” is only allowed when the input type is.*',
     'Attribute “autocomplete” not allowed on element “button” at this point.',
     'Bad value “tablist” for attribute “role” on element “nav”.',
-    // We use holder.js with `data-src` and no `src`; we could work around this, not sure it's worth it.
+    // We use holder.js with `data-src` and no `src` to avoid 404 errors;
+    // we could work around this, but I'm not sure it's worth it.
     'Element “img” is missing required attribute “src”.',
-    // Markup used in Components > Forms > Layout > Form grid > Horizontal form is currently invalid,
+    // Markup used in Components → Forms → Layout → Form grid → Horizontal form is currently invalid,
     // but used this way due to lack of support for flexbox layout on <fieldset> element in most browsers
     'Element “legend” not allowed as child of element “div” in this context.*',
-    // Content > Reboot uses various date/time inputs as a visual example. Documentation does not rely on them being usable.
+    // Content → Reboot uses various date/time inputs as a visual example.
+    // Documentation does not rely on them being usable.
     'The “date” input type is not supported in all browsers.*',
     'The “time” input type is not supported in all browsers.*',
-    // The next one we are using it because IE11 doesn't recognise <main>.
+    // The next one we are suppressing it because IE11 doesn't recognise <main>.
     // So, redundant for modern browsers, but not invalid.
     'The “main” role is unnecessary for element “main”.'
   ].join('|')
