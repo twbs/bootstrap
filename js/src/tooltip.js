3cf3412595e48bdc10ef64c3306b0bde2c3e9b00
Johann-S <johann.servoire@gmail.com>
2017-11-01 12:28:03 +0100

    Return a new function with Util.noop

--- js/src/tooltip.js
+++ js/src/tooltip.js
@@ -319,7 +319,7 @@ const Tooltip = (() => {
         // https://www.quirksmode.org/blog/archives/2014/02/mouse_event_bub.html
         if ('ontouchstart' in document.documentElement) {
           Util.makeArray(document.body.children).forEach((element) => {
-            EventHandler.on(element, 'mouseover', Util.noop)
+            EventHandler.on(element, 'mouseover', Util.noop())
           })
         }
 
@@ -376,7 +376,7 @@ const Tooltip = (() => {
       // empty mouseover listeners we added for iOS support
       if ('ontouchstart' in document.documentElement) {
         Util.makeArray(document.body.children).forEach((element) => {
-          EventHandler.off(element, 'mouseover', Util.noop)
+          EventHandler.off(element, 'mouseover', Util.noop())
         })
       }
 
--- js/src/util.js
+++ js/src/util.js
@@ -190,8 +190,9 @@ const Util = (() => {
       return false
     },
 
-    // eslint-disable-next-line no-empty-function
     noop() {
+      // eslint-disable-next-line no-empty-function
+      return function () {}
     },
 
     get jQuery() {
