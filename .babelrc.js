83fd91cdf87dd797c719a4ecea798083c92f4b3a
Johann-S <johann.servoire@gmail.com>
2017-11-05 19:03:33 +0100

    Update to babel beta 5

--- .babelrc.js
+++ .babelrc.js
@@ -1,16 +1,15 @@
 module.exports = {
   presets: [
     [
-      'env',
+      '@babel/env',
       {
         loose: true,
         modules: false,
-        exclude: ['transform-es2015-typeof-symbol']
+        exclude: ['transform-typeof-symbol']
       }
     ]
   ],
   plugins: [
-    process.env.ROLLUP && 'external-helpers',
     process.env.PLUGINS && 'transform-es2015-modules-strip'
   ].filter(Boolean)
 };
