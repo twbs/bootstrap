f4051abc122144e39967b49557714c103bc2dd81
Léo Andrès <leo@ndrs.fr>
2018-03-12 17:59:57 +0100

    Clean shell scripts (#25704)

--- build/upload-preview.sh
+++ build/upload-preview.sh
@@ -5,7 +5,7 @@
 sed -i "/^current_version:/ s/\$/+pr.${TRAVIS_COMMIT}/" _config.yml
 bundle exec jekyll build --destination "$TRAVIS_COMMIT" --baseurl "/c/${TRAVIS_COMMIT}"
 
-openssl aes-256-cbc -K $encrypted_2b749c8e6327_key -iv $encrypted_2b749c8e6327_iv -in build/gcp-key.json.enc -out build/gcp-key.json -d
+openssl aes-256-cbc -K "${encrypted_2b749c8e6327_key:?}" -iv "${encrypted_2b749c8e6327_iv:?}" -in build/gcp-key.json.enc -out build/gcp-key.json -d
 gcloud auth activate-service-account "$GCP_SERVICE_ACCOUNT" --key-file build/gcp-key.json &> /dev/null || (echo 'GCP login failed!'; exit 1)
 
 echo "Uploading to http://preview.twbsapps.com/c/${TRAVIS_COMMIT} ..."
