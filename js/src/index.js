6333fbb3a6fc023e78bb388b9ede7f83e0f225ce
Johann-S <johann.servoire@gmail.com>
2018-06-11 21:44:21 +0200

    fix(event-handler): remove polyfill and move it to index

--- js/src/index.js
+++ js/src/index.js
@@ -3,7 +3,9 @@ import Button from './button'
 import Carousel from './carousel'
 import Collapse from './collapse'
 import Dropdown from './dropdown'
+import EventHandler from './dom/eventHandler'
 import Modal from './modal'
+import Polyfill from './dom/polyfill'
 import Popover from './popover'
 import ScrollSpy from './scrollspy'
 import Tab from './tab'
@@ -17,6 +19,21 @@ import Util from './util'
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
+
+/* istanbul ignore next */
+// focusin and focusout polyfill
+if (Polyfill.focusIn) {
+  (() => {
+    function listenerFocus(event) {
+      EventHandler.trigger(event.target, 'focusin')
+    }
+    function listenerBlur(event) {
+      EventHandler.trigger(event.target, 'focusout')
+    }
+    EventHandler.on(document, 'focus', 'input', listenerFocus)
+    EventHandler.on(document, 'blur', 'input', listenerBlur)
+  })()
+}
 
 export {
   Util,
