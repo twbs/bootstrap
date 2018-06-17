b09d2523fa7c2ef160e443b7da1896d3152f3fa3
Johann-S <johann.servoire@gmail.com>
2018-06-17 15:28:42 +0200

    fix(unit-test): dropdown, collapse and carousel

--- js/src/dropdown.js
+++ js/src/dropdown.js
@@ -472,7 +472,7 @@ class Dropdown {
         EventHandler.trigger(SelectorEngine.findOne(Selector.DATA_TOGGLE, parent), 'focus')
       }
 
-      EventHandler.trigger(this, 'click')
+      Dropdown._clearMenus()
       return
     }
 
