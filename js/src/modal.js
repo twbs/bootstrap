7268f1bc3e6b304f8f84ad7639ebd484f741954b
Johann-S <johann.servoire@gmail.com>
2018-06-24 22:02:03 +0200

    fix(plugins): save instance in constructor

--- js/src/modal.js
+++ js/src/modal.js
@@ -86,6 +86,7 @@ const Modal = (() => {
       this._isBodyOverflowing   = false
       this._ignoreBackdropClick = false
       this._scrollbarWidth      = 0
+      Data.setData(element, DATA_KEY, this)
     }
 
     // Getters
@@ -508,7 +509,6 @@ const Modal = (() => {
 
         if (!data) {
           data = new Modal(this, _config)
-          Data.setData(this, DATA_KEY, data)
         }
 
         if (typeof config === 'string') {
@@ -572,7 +572,6 @@ const Modal = (() => {
     let data = Data.getData(target, DATA_KEY)
     if (!data) {
       data = new Modal(target, config)
-      Data.setData(target, DATA_KEY, data)
     }
 
     data.show(this)
