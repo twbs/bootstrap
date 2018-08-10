5c71ecfcfa0a45583597e700b1dfd19f017f6f4b
Dominik Speicher <github@verdestron.de>
2018-08-10 12:52:50 +0200

    fix(tooltip): XSS on data-viewport attribute
    Fixes #27044

--- js/tooltip.js
+++ js/tooltip.js
@@ -51,7 +51,7 @@
     this.type      = type
     this.$element  = $(element)
     this.options   = this.getOptions(options)
-    this.$viewport = this.options.viewport && $($.isFunction(this.options.viewport) ? this.options.viewport.call(this, this.$element) : (this.options.viewport.selector || this.options.viewport))
+    this.$viewport = this.options.viewport && $(document).find($.isFunction(this.options.viewport) ? this.options.viewport.call(this, this.$element) : (this.options.viewport.selector || this.options.viewport))
     this.inState   = { click: false, hover: false, focus: false }
 
     if (this.$element[0] instanceof document.constructor && !this.options.selector) {
