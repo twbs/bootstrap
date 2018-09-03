b0720cf2b998552dccf6ae5b8fee87457733d4c9
Herst <Herst@users.noreply.github.com>
2018-09-03 17:37:51 +0200

    Added type="button" to <button>s without any type
    (again)
    
    https://github.com/twbs/bootlint/wiki/W007

--- site/docs/4.1/assets/js/src/application.js
+++ site/docs/4.1/assets/js/src/application.js
@@ -54,7 +54,7 @@
 
     // Insert copy to clipboard button before .highlight
     $('figure.highlight, div.highlight').each(function () {
-      var btnHtml = '<div class="bd-clipboard"><button class="btn-clipboard" title="Copy to clipboard">Copy</button></div>'
+      var btnHtml = '<div class="bd-clipboard"><button type="button" class="btn-clipboard" title="Copy to clipboard">Copy</button></div>'
       $(this).before(btnHtml)
       $('.btn-clipboard')
         .tooltip()
