bfc052ad5a6854363cb40c8b9bdd55c35b151321
XhmikosR <xhmikosr@gmail.com>
2018-09-19 10:41:16 +0300

    Fix HTML validation errors and lint tests too.

--- Gruntfile.js
+++ Gruntfile.js
@@ -348,12 +348,10 @@ module.exports = function (grunt) {
     htmllint: {
       options: {
         ignore: [
-          'Attribute "autocomplete" not allowed on element "button" at this point.',
-          'Attribute "autocomplete" is only allowed when the input type is "color", "date", "datetime", "datetime-local", "email", "hidden", "month", "number", "password", "range", "search", "tel", "text", "time", "url", or "week".',
           'Element "img" is missing required attribute "src".'
         ]
       },
-      src: '_gh_pages/**/*.html'
+      src: ['_gh_pages/**/*.html', 'js/tests/**/*.html']
     },
 
     watch: {
