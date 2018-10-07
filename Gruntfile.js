a9435b996c5276e9eda8d799b8437d164dd96aa3
XhmikosR <xhmikosr@gmail.com>
2018-10-07 09:25:39 +0300

    Move the whole autoprefixer config to configBridge.json.
    Reduces duplication.

--- Gruntfile.js
+++ Gruntfile.js
@@ -193,10 +193,7 @@ module.exports = function (grunt) {
           sourcesContent: true
         },
         processors: [
-          require('autoprefixer')({
-            browsers: configBridge.config.autoprefixerBrowsers,
-            cascade: false
-          })
+          require('autoprefixer')(configBridge.config.autoprefixer)
         ]
       },
       core: {
