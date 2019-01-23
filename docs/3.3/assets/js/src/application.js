7bf0a83469ccf52161371b238fc9c62593d9de3e
XhmikosR <xhmikosr@gmail.com>
2019-01-23 05:46:55 +0200

    v3.3 docs: fix path to ZeroClipboard.swf.

--- docs/3.3/assets/js/src/application.js
+++ docs/3.3/assets/js/src/application.js
@@ -127,7 +127,7 @@
 
     // Config ZeroClipboard
     ZeroClipboard.config({
-      moviePath: '/assets/flash/ZeroClipboard.swf',
+      moviePath: '/docs/3.3/assets/flash/ZeroClipboard.swf',
       hoverClass: 'btn-clipboard-hover'
     })
 
