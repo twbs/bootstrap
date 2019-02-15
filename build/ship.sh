319831ec43cc00d82010402618505dcabc2abec1
XhmikosR <xhmikosr@gmail.com>
2019-02-15 15:55:39 +0200

    Revert "Replace `v4-dev` with `master` after the branch change. (#28272)"
    This reverts commit 9baf8537a7ee447035eae65270671a4cf1fc0549.
    
    This doesn't apply in this branch.

--- build/ship.sh
+++ build/ship.sh
@@ -26,7 +26,7 @@ fi
 printf "\n%s=======================================================%s" $magenta $end
 printf "\n%sPulling latest changes...%s" $magenta $end
 printf "\n%s=======================================================\n\n%s" $magenta $end
-git pull origin master
+git pull origin v4-dev
 
 # Update version number
 printf "\n%s=======================================================%s" $magenta $end
