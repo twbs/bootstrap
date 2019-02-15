9baf8537a7ee447035eae65270671a4cf1fc0549
XhmikosR <xhmikosr@gmail.com>
2019-02-15 11:58:41 +0200

    Replace `v4-dev` with `master` after the branch change. (#28272)

--- build/ship.sh
+++ build/ship.sh
@@ -26,7 +26,7 @@ fi
 printf "\n%s=======================================================%s" $magenta $end
 printf "\n%sPulling latest changes...%s" $magenta $end
 printf "\n%s=======================================================\n\n%s" $magenta $end
-git pull origin v4-dev
+git pull origin master
 
 # Update version number
 printf "\n%s=======================================================%s" $magenta $end
