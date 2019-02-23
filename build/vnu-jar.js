dd654de91a25e40ac0fceb7bd0d9d48a8869fe75
XhmikosR <xhmikosr@gmail.com>
2019-02-23 08:42:40 +0200

    Remove `role="main"` from `main` element.
    This was needed for Internet Explorer 11.

--- build/vnu-jar.js
+++ build/vnu-jar.js
@@ -34,9 +34,6 @@ childProcess.exec('java -version', (error, stdout, stderr) => {
     // Documentation does not rely on them being usable.
     'The “date” input type is not supported in all browsers.*',
     'The “time” input type is not supported in all browsers.*',
-    // IE11 doesn't recognise <main> / give the element an implicit "main" landmark.
-    // Explicit role="main" is redundant for other modern browsers, but still valid.
-    'The “main” role is unnecessary for element “main”.',
     // Ignore the wrong lanuage code warnings for now; they happen randomly.
     'This document appears to be written in.*'
   ].join('|')
