23e7b998aff719fa494f0d417be3488b2a52efc5
Johann-S <johann.servoire@gmail.com>
2019-02-24 11:45:01 +0100

    remove tests for old jQuery 1.9.x compatibility

--- js/tests/karma.conf.js
+++ js/tests/karma.conf.js
@@ -8,7 +8,7 @@ const {
   browsersKeys
 } = require('./browsers')
 
-const jqueryFile = process.env.USE_OLD_JQUERY ? 'https://code.jquery.com/jquery-1.9.1.min.js' : 'node_modules/jquery/dist/jquery.slim.min.js'
+const jqueryFile = 'node_modules/jquery/dist/jquery.slim.min.js'
 const bundle = process.env.BUNDLE === 'true'
 const browserStack = process.env.BROWSER === 'true'
 const debug = process.env.DEBUG === 'true'
@@ -94,7 +94,7 @@ if (bundle) {
   conf.browsers = browsersKeys
   reporters.push('BrowserStack')
   files = files.concat([
-    'node_modules/jquery/dist/jquery.slim.min.js',
+    jqueryFile,
     'js/coverage/dist/util/util.js',
     'js/coverage/dist/util/sanitizer.js',
     'js/coverage/dist/dom/polyfill.js',
