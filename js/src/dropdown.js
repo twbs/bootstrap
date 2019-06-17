1da3aa310314dd8a58e4ab056fdce5650c8525b7
Patrick H. Lauke <redux@splintered.co.uk>
2019-06-17 14:50:43 +0100

    Don't open dropdown on ESC on trigger element (#28912)
    * Don't open dropdown on ESC on trigger element
    
    Closes #28751

--- js/src/dropdown.js
+++ js/src/dropdown.js
@@ -475,6 +475,10 @@ class Dropdown {
     const parent   = Dropdown._getParentFromElement(this)
     const isActive = $(parent).hasClass(ClassName.SHOW)
 
+    if (!isActive && event.which === ESCAPE_KEYCODE) {
+      return
+    }
+
     if (!isActive || isActive && (event.which === ESCAPE_KEYCODE || event.which === SPACE_KEYCODE)) {
       if (event.which === ESCAPE_KEYCODE) {
         const toggle = parent.querySelector(Selector.DATA_TOGGLE)
