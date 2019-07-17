e4dc6b84b1abab55971bda4802bf875b6a38aed9
XhmikosR <xhmikosr@gmail.com>
2019-07-17 11:29:30 +0300

    Move inline JS to application.js.

--- site/static/docs/4.3/assets/js/src/application.js
+++ site/static/docs/4.3/assets/js/src/application.js
@@ -19,6 +19,16 @@
     return [].slice.call(list)
   }
 
+  (function () {
+    var checkbox = document.getElementById('flexCheckIndeterminate')
+
+    if (!checkbox) {
+      return
+    }
+
+    checkbox.indeterminate = true
+  })()
+
   makeArray(document.querySelectorAll('.js-sidenav-group'))
     .forEach(function (sidenavGroup) {
       var groupHasLinks = Boolean(sidenavGroup.querySelector('li'))
