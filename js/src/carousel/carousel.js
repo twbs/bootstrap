c21465298e62619a6768e2df8f02423a76d647e2
mhatvan <markus_hatvan@aon.at>
2019-08-21 22:28:11 +0200

    Fix accidentally misplaced braces in carousel.js
    Signed-off-by: mhatvan <markus_hatvan@aon.at>

--- js/src/carousel/carousel.js
+++ js/src/carousel/carousel.js
@@ -424,8 +424,8 @@ class Carousel {
   _slide(direction, element) {
     const activeElement = SelectorEngine.findOne(Selector.ACTIVE_ITEM, this._element)
     const activeElementIndex = this._getItemIndex(activeElement)
-    const nextElement = (element || activeElement) &&
-      this._getItemByDirection(direction, activeElement)
+    const nextElement = element || (activeElement &&
+      this._getItemByDirection(direction, activeElement))
 
     const nextElementIndex = this._getItemIndex(nextElement)
     const isCycling = Boolean(this._interval)
