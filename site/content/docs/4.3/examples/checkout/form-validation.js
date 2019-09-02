d9ba8e59b8bd1b3fa02227e29e0d9208fc348c2e
XhmikosR <xhmikosr@gmail.com>
2019-09-02 14:56:57 +0300

    Form validation: remove load event.

--- site/content/docs/4.3/examples/checkout/form-validation.js
+++ site/content/docs/4.3/examples/checkout/form-validation.js
@@ -2,20 +2,18 @@
 (function () {
   'use strict'
 
-  window.addEventListener('load', function () {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation')
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.getElementsByClassName('needs-validation')
 
-    // Loop over them and prevent submission
-    Array.prototype.filter.call(forms, function (form) {
-      form.addEventListener('submit', function (event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
+  // Loop over them and prevent submission
+  Array.prototype.filter.call(forms, function (form) {
+    form.addEventListener('submit', function (event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
 
-        form.classList.add('was-validated')
-      }, false)
-    })
-  }, false)
+      form.classList.add('was-validated')
+    }, false)
+  })
 })()
