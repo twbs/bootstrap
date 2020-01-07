9c2b9ac74d4abb5ec8b7864054315de3409d5f3a
XhmikosR <xhmikosr@gmail.com>
2020-01-07 22:07:51 +0200

    Use regex.test() when we want to check for a Boolean. (#29969)

--- build/vnu-jar.js
+++ build/vnu-jar.js
@@ -18,7 +18,7 @@ childProcess.exec('java -version', (error, stdout, stderr) => {
     return
   }
 
-  const is32bitJava = !stderr.match(/64-Bit/)
+  const is32bitJava = !/64-Bit/.test(stderr)
 
   // vnu-jar accepts multiple ignores joined with a `|`.
   // Also note that the ignores are regular expressions.
