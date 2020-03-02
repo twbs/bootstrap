14b6b22a9af9e464c18add7acc9bbe829ff7c5ef
XhmikosR <xhmikosr@gmail.com>
2020-03-02 21:18:48 +0200

    Update modal.js

--- js/src/modal.js
+++ js/src/modal.js
@@ -410,8 +410,7 @@ class Modal {
         return
       }
 
-      const isModalOverflowing =
-        this._element.scrollHeight > document.documentElement.clientHeight
+      const isModalOverflowing = this._element.scrollHeight > document.documentElement.clientHeight
 
       if (!isModalOverflowing) {
         this._element.style.overflowY = 'hidden'
