f15e3681ff40137d060152135ccb5d364e56d8d9
XhmikosR <xhmikosr@gmail.com>
2020-05-26 19:20:15 +0300

    Backport #30742
    Update to `@rollup/plugin-babel`

--- js/tests/integration/rollup.bundle.js
+++ js/tests/integration/rollup.bundle.js
@@ -2,7 +2,7 @@
 
 const resolve = require('@rollup/plugin-node-resolve')
 const commonjs = require('@rollup/plugin-commonjs')
-const babel = require('rollup-plugin-babel')
+const { babel } = require('@rollup/plugin-babel')
 
 module.exports = {
   input: 'js/tests/integration/bundle.js',
@@ -14,7 +14,8 @@ module.exports = {
     resolve(),
     commonjs(),
     babel({
-      exclude: 'node_modules/**'
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled'
     })
   ]
 }
