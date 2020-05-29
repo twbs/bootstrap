b71c1e8a9a782792dcb97531b50e62ec09ce79a3
XhmikosR <xhmikosr@gmail.com>
2020-05-29 08:11:41 +0300

    Update package.js

--- package.js
+++ package.js
@@ -2,20 +2,19 @@
 
 Package.describe({
   name: 'twbs:bootstrap', // https://atmospherejs.com/twbs/bootstrap
-  summary:
-    'The most popular front-end framework for developing responsive, mobile first projects on the web.',
+  summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
   version: '5.0.0-alpha1',
   git: 'https://github.com/twbs/bootstrap.git'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.0');
-  api.addFiles(
-    ['dist/css/bootstrap.min.css', 'dist/js/bootstrap.bundle.min.js'],
-    'client'
-  );
-  api.addAssets(
-    ['dist/css/bootstrap.min.css.map', 'dist/js/bootstrap.bundle.min.js.map'],
-    'client'
-  );
+  api.addFiles([
+    'dist/css/bootstrap.min.css',
+    'dist/js/bootstrap.bundle.min.js'
+  ], 'client');
+  api.addAssets([
+    'dist/css/bootstrap.min.css.map',
+    'dist/js/bootstrap.bundle.min.js.map'
+  ], 'client');
 });
