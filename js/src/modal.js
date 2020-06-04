5faf41eb4837491fa8193910c5816efadb4dbc5a
Rohit Sharma <rohit2sharma95@gmail.com>
2020-06-04 13:05:09 +0530

    Add role="dialog" in modals via JavaScript (#30936)

--- js/src/modal.js
+++ js/src/modal.js
@@ -248,6 +248,7 @@ class Modal {
     this._element.style.display = 'block'
     this._element.removeAttribute('aria-hidden')
     this._element.setAttribute('aria-modal', true)
+    this._element.setAttribute('role', 'dialog')
     this._element.scrollTop = 0
 
     if (modalBody) {
@@ -323,6 +324,7 @@ class Modal {
     this._element.style.display = 'none'
     this._element.setAttribute('aria-hidden', true)
     this._element.removeAttribute('aria-modal')
+    this._element.removeAttribute('role')
     this._isTransitioning = false
     this._showBackdrop(() => {
       document.body.classList.remove(CLASS_NAME_OPEN)
