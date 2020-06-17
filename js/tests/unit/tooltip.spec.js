4b503328824e68d16cc8c4047586a6cbd4e52ed7
XhmikosR <xhmikosr@gmail.com>
2020-06-17 21:56:45 +0300

    Update tooltip.spec.js

--- js/tests/unit/tooltip.spec.js
+++ js/tests/unit/tooltip.spec.js
@@ -327,9 +327,9 @@ describe('Tooltip', () => {
     it('should show a tooltip when hovering a children element', done => {
       fixtureEl.innerHTML =
         '<a href="#" rel="tooltip" title="Another tooltip">' +
-          '<svg viewBox="0 0 100 100" width="50" height="50">' +
-            '<rect width="100%" fill="#563d7c" />' +
-            '<circle cx="50" cy="50" r="30" fill="white" />' +
+          '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 100 100">' +
+            '<rect width="100%" fill="#563d7c"/>' +
+            '<circle cx="50" cy="50" r="30" fill="#fff"/>' +
           '</svg>' +
         '</a>'
 
