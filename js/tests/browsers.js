786208ea15626ebc7769375e4b98bbb6fcd3428f
XhmikosR <xhmikosr@gmail.com>
2020-06-25 11:42:55 +0300

    BrowserStack: stop testing on latest Edge. (#31125)
    It's Chromium-based so it seems redundant.

--- js/tests/browsers.js
+++ js/tests/browsers.js
@@ -30,13 +30,6 @@ const browsers = {
     browser: 'Edge',
     browser_version: '16'
   },
-  edgeWin10Latest: {
-    base: 'BrowserStack',
-    os: 'Windows',
-    os_version: '10',
-    browser: 'Edge',
-    browser_version: 'latest'
-  },
   chromeWin10: {
     base: 'BrowserStack',
     os: 'Windows',
