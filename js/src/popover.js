a67231ae1c352c37a278ac501d7ffb91b1eb20c7
XhmikosR <xhmikosr@gmail.com>
2020-06-26 18:40:54 +0300

    Move function

--- js/src/popover.js
+++ js/src/popover.js
@@ -116,11 +116,11 @@ class Popover extends Tooltip {
     tip.classList.remove(CLASS_NAME_FADE, CLASS_NAME_SHOW)
   }
 
+  // Private
+
   _addAttachmentClass(attachment) {
     this.getTipElement().classList.add(`${CLASS_PREFIX}-${attachment}`)
   }
-
-  // Private
 
   _getContent() {
     return this.element.getAttribute('data-content') ||
