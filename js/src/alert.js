3a93053bd45a569b1af5d0d6bcb75e4b693319e4
Asif Uran <asif@sms-speedway.com>
2020-07-02 12:26:09 +0530

    change delay to 3 seconds

--- js/src/alert.js
+++ js/src/alert.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v5.0.0-alpha1): alert.js
+ * Bootstrap (v5.0.0-alpha221): alert.js
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
