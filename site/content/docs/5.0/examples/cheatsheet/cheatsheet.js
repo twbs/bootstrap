0b8039c23183116c294c85bd54a1215256c47425
Gaël Poupard <gael.poupard@orange.com>
2020-07-28 16:49:05 +0200

    docs(cheatsheet): move to examples

---
+++ site/content/docs/5.0/examples/cheatsheet/cheatsheet.js
@@ -0,0 +1,36 @@
+(function () {
+  'use strict'
+
+  function setActiveItem() {
+    var hash = window.location.hash
+
+    if (hash === '') {
+      return
+    }
+
+    var link = document.querySelector('.bd-aside a[href="' + hash + '"]')
+    var active = document.querySelector('.bd-aside .active')
+    var parent = link.parentNode.parentNode.previousElementSibling
+
+    link.classList.add('active')
+
+    if (parent.classList.contains('collapsed')) {
+      parent.click()
+    }
+
+    if (!active) {
+      return
+    }
+
+    var expanded = active.parentNode.parentNode.previousElementSibling
+
+    active.classList.remove('active')
+
+    if (expanded && parent !== expanded) {
+      expanded.click()
+    }
+  }
+
+  window.addEventListener('DOMContentLoaded', setActiveItem)
+  window.addEventListener('hashchange', setActiveItem)
+})()
