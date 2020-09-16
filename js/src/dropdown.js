a4a62fd74004d55563bb864a691a170072494f6a
XhmikosR <xhmikosr@gmail.com>
2020-09-16 17:21:07 +0300

    Dropdown: fix variable always evaluating to true

--- js/src/dropdown.js
+++ js/src/dropdown.js
@@ -469,7 +469,7 @@ class Dropdown {
     event.preventDefault()
     event.stopPropagation()
 
-    if (!isActive || isActive && (event.which === ESCAPE_KEYCODE || event.which === SPACE_KEYCODE)) {
+    if (!isActive || (event.which === ESCAPE_KEYCODE || event.which === SPACE_KEYCODE)) {
       if (event.which === ESCAPE_KEYCODE) {
         $(parent.querySelector(SELECTOR_DATA_TOGGLE)).trigger('focus')
       }
