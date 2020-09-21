8155bfc5b51c8c1281a06c1ae4c93c2c7f3c5433
Sarah Mattar <sarah.mattar@mac.com>
2020-09-20 23:46:25 -0700

    Updated jquery minimum dependency to 3.0.0, as required by Meteor versions 1.9 and above. - This commit should be pushed through to https://atmospherejs.com/twbs/bootstrap to ensure that Bootstrap 4.5.2 is installed when users add the `twbs:bootstrap` package. - Resolves issue #30843

--- package.js
+++ package.js
@@ -1,17 +1,15 @@
 // package metadata file for Meteor.js
 
 Package.describe({
-  name: 'twbs:bootstrap', // https://atmospherejs.com/twbs/bootstrap
-  summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
-  version: '4.5.2',
-  git: 'https://github.com/twbs/bootstrap.git'
+  name: "twbs:bootstrap", // https://atmospherejs.com/twbs/bootstrap
+  summary:
+    "The most popular front-end framework for developing responsive, mobile first projects on the web.",
+  version: "4.5.2",
+  git: "https://github.com/twbs/bootstrap.git",
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.0');
-  api.use('jquery', 'client');
-  api.addFiles([
-    'dist/css/bootstrap.css',
-    'dist/js/bootstrap.js'
-  ], 'client');
+  api.versionsFrom("METEOR@1.9");
+  api.use("jquery@3.0.0", "client");
+  api.addFiles(["dist/css/bootstrap.css", "dist/js/bootstrap.js"], "client");
 });
