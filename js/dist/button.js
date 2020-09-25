a7330a61c1962677cb8cd64988a936346702b65f
XhmikosR <xhmikosr@gmail.com>
2020-09-25 18:32:52 +0300

    Prepare v5.0.0-alpha2.

--- js/dist/button.js
+++ js/dist/button.js
@@ -1,5 +1,5 @@
 /*!
-  * Bootstrap button.js v5.0.0-alpha1 (https://getbootstrap.com/)
+  * Bootstrap button.js v5.0.0-alpha2 (https://getbootstrap.com/)
   * Copyright 2011-2020 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
@@ -16,7 +16,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v5.0.0-alpha1): util/index.js
+   * Bootstrap (v5.0.0-alpha2): util/index.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -42,7 +42,7 @@
    */
 
   var NAME = 'button';
-  var VERSION = '5.0.0-alpha1';
+  var VERSION = '5.0.0-alpha2';
   var DATA_KEY = 'bs.button';
   var EVENT_KEY = "." + DATA_KEY;
   var DATA_API_KEY = '.data-api';
