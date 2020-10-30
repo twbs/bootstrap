d98c317b7369a22722381230b8bbf5b9f866492b
XhmikosR <xhmikosr@gmail.com>
2020-10-30 09:41:00 +0200

    Actually, stop excluding polyfill.js from istanbul

--- js/tests/karma.conf.js
+++ js/tests/karma.conf.js
@@ -134,9 +134,19 @@ if (browserStack) {
       emitWarning: false,
       global: {
         statements: 90,
-        branches: 90,
+        branches: 89,
         functions: 90,
         lines: 90
+      },
+      each: {
+        overrides: {
+          'js/src/dom/polyfill.js': {
+            statements: 30,
+            lines: 30,
+            branches: 8,
+            functions: 30
+          }
+        }
       }
     }
   }
