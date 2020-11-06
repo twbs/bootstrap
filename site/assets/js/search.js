40af85e0f2172102611bbab536c13f1eef198ec9
shortcuts <vannicattec@gmail.com>
2020-11-06 16:54:55 +0100

    Implementation of DocSearch v3 with bootstrap v5 and icon support

--- site/assets/js/search.js
+++ site/assets/js/search.js
@@ -32,35 +32,148 @@
     return origin
   }
 
+  function isIconHit(hit) {
+    return hit.hierarchy.lvl0 === 'Bootstrap Icons'
+  }
+
+  function IconName(hit) {
+    return hit.url
+      .replace(/https:\/\/icons.getbootstrap.com\/icons\//g, '')
+      .replace(/\/#content/g, '')
+  }
+
+  function IconSVG(hit) {
+    return {
+      type: 'svg',
+      ref: undefined,
+      constructor: undefined,
+      key: undefined,
+      props: {
+        width: 20,
+        height: 20,
+        fill: 'currentColor',
+        children: {
+          type: 'use',
+          ref: undefined,
+          constructor: undefined,
+          key: undefined,
+          props: {
+            xlinkHref:
+              '/docs/5.0/assets/img/bootstrap-icons.svg#' + IconName(hit),
+          },
+        },
+      },
+    }
+  }
+
+  function Hits({ hit, children }) {
+    if (isIconHit(hit)) {
+      return {
+        type: 'a',
+        ref: undefined,
+        constructor: undefined,
+        key: undefined,
+        props: {
+          href: hit.url,
+          children: {
+            type: 'div',
+            ref: undefined,
+            constructor: undefined,
+            key: undefined,
+            props: {
+              className: 'DocSearch-Hit-Container',
+              children: [
+                {
+                  type: 'div',
+                  ref: undefined,
+                  constructor: undefined,
+                  key: undefined,
+                  props: {
+                    className: 'DocSearch-Hit-icon',
+                    children: IconSVG(hit),
+                  },
+                },
+                {
+                  type: 'div',
+                  ref: undefined,
+                  constructor: undefined,
+                  key: undefined,
+                  props: {
+                    className: 'DocSearch-Hit-content-wrapper',
+                    children: [
+                      {
+                        type: 'span',
+                        ref: undefined,
+                        constructor: undefined,
+                        key: undefined,
+                        props: {
+                          className: 'DocSearch-Hit-title',
+                          children: hit.hierarchy.lvl1,
+                        },
+                      },
+                      {
+                        type: 'span',
+                        ref: undefined,
+                        constructor: undefined,
+                        key: undefined,
+                        props: {
+                          className: 'DocSearch-Hit-path',
+                          children: 'Icon',
+                        },
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        },
+      }
+    }
+
+    return {
+      type: 'a',
+      ref: undefined,
+      constructor: undefined,
+      key: undefined,
+      props: {
+        href: hit.url,
+        children,
+      },
+    }
+  }
+
   window.docsearch({
-    apiKey: '5990ad008512000bba2cf951ccf0332f',
+    appId: '4Y8GPLL6DU',
+    apiKey: 'b52e904f627693602a90f2f6d64b260c',
     indexName: 'bootstrap',
-    inputSelector: '#search-input',
-    algoliaOptions: {
-      facetFilters: ['version:' + siteDocsVersion]
-    },
-    transformData: function (hits) {
-      return hits.map(function (hit) {
-        var currentUrl = getOrigin()
-        var liveUrl = 'https://getbootstrap.com/'
+    container: '#search-input',
+    hitComponent: Hits,
+    // algoliaOptions: {
+    //   facetFilters: ['version:' + siteDocsVersion]
+    // },
+    // transformData: function (hits) {
+    //   return hits.map(function (hit) {
+    //     var currentUrl = getOrigin()
+    //     var liveUrl = 'https://getbootstrap.com/'
 
-        hit.url = currentUrl.lastIndexOf(liveUrl, 0) === 0 ?
-          // On production, return the result as is
-          hit.url :
-          // On development or Netlify, replace `hit.url` with a trailing slash,
-          // so that the result link is relative to the server root
-          hit.url.replace(liveUrl, '/')
+    //     hit.url = currentUrl.lastIndexOf(liveUrl, 0) === 0 ?
+    //       // On production, return the result as is
+    //       hit.url :
+    //       // On development or Netlify, replace `hit.url` with a trailing slash,
+    //       // so that the result link is relative to the server root
+    //       hit.url.replace(liveUrl, '/')
 
-        // Prevent jumping to first header
-        if (hit.anchor === 'content') {
-          hit.url = hit.url.replace(/#content$/, '')
-          hit.anchor = null
-        }
+    //     // Prevent jumping to first header
+    //     if (hit.anchor === 'content') {
+    //       hit.url = hit.url.replace(/#content$/, '')
+    //       hit.anchor = null
+    //     }
 
-        return hit
-      })
-    },
+    //     return hit
+    //   })
+    // },
     // Set debug to `true` if you want to inspect the dropdown
-    debug: false
+    debug: false,
   })
 })()
