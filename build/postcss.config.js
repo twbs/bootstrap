08bebb5dddb3d2cfa0a853ed3f61d3380921b6e3
XhmikosR <xhmikosr@gmail.com>
2020-11-23 16:11:16 +0200

    postcss.config.js: remove arrow return (#32235)

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,14 +1,18 @@
 'use strict'
 
-module.exports = ctx => ({
-  map: ctx.file.dirname.includes('examples') ? false : {
-    inline: false,
-    annotation: true,
-    sourcesContent: true
-  },
-  plugins: {
-    autoprefixer: {
-      cascade: false
+module.exports = ctx => {
+  return {
+    map: ctx.file.dirname.includes('examples') ?
+      false :
+      {
+        inline: false,
+        annotation: true,
+        sourcesContent: true
+      },
+    plugins: {
+      autoprefixer: {
+        cascade: false
+      }
     }
   }
-})
+}
