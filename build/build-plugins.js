3b3354341381a395da8fe809e9952371b1a51292
XhmikosR <xhmikosr@gmail.com>
2020-12-02 09:28:42 +0200

    Update build-plugins.js

--- build/build-plugins.js
+++ build/build-plugins.js
@@ -18,7 +18,7 @@ const plugins = [
   babel({
     // Only transpile our source code
     exclude: 'node_modules/**',
-    // Include the helpers in the bundle, at most one copy of each
+    // Include the helpers in each file bundle, at most one copy of each
     babelHelpers: 'bundled'
   })
 ]
