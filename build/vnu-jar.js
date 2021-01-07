6e1f825b6f92510204c10a6eacaa81136d68eda3
XhmikosR <xhmikosr@gmail.com>
2021-01-07 10:31:02 +0200

    Bump copyright year to 2021

--- build/vnu-jar.js
+++ build/vnu-jar.js
@@ -2,8 +2,8 @@
 
 /*!
  * Script to run vnu-jar if Java is available.
- * Copyright 2017-2020 The Bootstrap Authors
- * Copyright 2017-2020 Twitter, Inc.
+ * Copyright 2017-2021 The Bootstrap Authors
+ * Copyright 2017-2021 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  */
 
