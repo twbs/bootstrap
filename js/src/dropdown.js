af771292cf324f6bf4f6dfd6620b397f50929e25
Siju Samson <sijusamson@gmail.com>
2021-01-10 12:50:30 +0530

    Fixes for:
    Up/Down Key not working for dropdown

--- js/src/dropdown.js
+++ js/src/dropdown.js
@@ -463,12 +463,17 @@ class Dropdown extends BaseComponent {
     const parent = Dropdown.getParentFromElement(this)
     const isActive = this.classList.contains(CLASS_NAME_SHOW)
 
-    if (event.key === ESCAPE_KEY) {
+    if ((!isActive) || (event.key === ESCAPE_KEY || event.key || SPACE_KEY)) {
       const button = this.matches(SELECTOR_DATA_TOGGLE) ? this : SelectorEngine.prev(this, SELECTOR_DATA_TOGGLE)[0]
-      button.focus()
-      Dropdown.clearMenus()
+      if (event.key === ESCAPE_KEY) {
+        button.focus()
+        Dropdown.clearMenus()
+        return
+      }
+      button.click()
       return
     }
+
 
     if (!isActive || event.key === SPACE_KEY) {
       Dropdown.clearMenus()
