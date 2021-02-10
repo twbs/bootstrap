0e22eedde3973d57a5fa63e6d6c6c7694257dffc
XhmikosR <xhmikosr@gmail.com>
2021-02-10 07:42:02 +0200

    Bump version to 5.0.0-beta2

--- js/src/toast.js
+++ js/src/toast.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v5.0.0-beta1): toast.js
+ * Bootstrap (v5.0.0-beta2): toast.js
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
