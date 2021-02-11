4fb311484caa565941d5210d1f018ac3749133aa
XhmikosR <xhmikosr@gmail.com>
2021-02-11 17:59:12 +0200

    Improve vnu-jar.js
    * switch to `execFile` for the java version command
    * simplify our ignores and removed no longer needed ones
    * remove `--no-langdetect` since it doesn't seem to trigger any issues any more

--- build/vnu-jar.js
+++ build/vnu-jar.js
@@ -9,10 +9,10 @@
 
 'use strict'
 
-const childProcess = require('child_process')
+const { execFile, spawn } = require('child_process')
 const vnu = require('vnu-jar')
 
-childProcess.exec('java -version', (error, stdout, stderr) => {
+execFile('java', ['-version'], (error, stdout, stderr) => {
   if (error) {
     console.error('Skipping vnu-jar test; Java is missing.')
     return
@@ -21,23 +21,15 @@ childProcess.exec('java -version', (error, stdout, stderr) => {
   const is32bitJava = !/64-Bit/.test(stderr)
 
   // vnu-jar accepts multiple ignores joined with a `|`.
-  // Also note that the ignores are regular expressions.
+  // Also note that the ignores are string regular expressions.
   const ignores = [
     // "autocomplete" is included in <button> and checkboxes and radio <input>s due to
     // Firefox's non-standard autocomplete behavior - see https://bugzilla.mozilla.org/show_bug.cgi?id=654072
     'Attribute “autocomplete” is only allowed when the input type is.*',
     'Attribute “autocomplete” not allowed on element “button” at this point.',
-    // Markup used in Components → Forms → Layout → Form grid → Horizontal form is currently invalid,
-    // but used this way due to lack of support for flexbox layout on <fieldset> element in most browsers
-    'Element “legend” not allowed as child of element “div” in this context.*',
     // Content → Reboot uses various date/time inputs as a visual example.
     // Documentation does not rely on them being usable.
-    'The “date” input type is not supported in all browsers.*',
-    'The “week” input type is not supported in all browsers.*',
-    'The “month” input type is not supported in all browsers.*',
-    'The “color” input type is not supported in all browsers.*',
-    'The “datetime-local” input type is not supported in all browsers.*',
-    'The “time” input type is not supported in all browsers.*'
+    'The “(?:date|week|month|color|datetime-local|time)” input type is not supported in all browsers.*'
   ].join('|')
 
   const args = [
@@ -45,8 +37,6 @@ childProcess.exec('java -version', (error, stdout, stderr) => {
     `"${vnu}"`,
     '--asciiquotes',
     '--skip-non-html',
-    // Ignore the language code warnings
-    '--no-langdetect',
     '--Werror',
     `--filterpattern "${ignores}"`,
     '_gh_pages/',
@@ -58,7 +48,7 @@ childProcess.exec('java -version', (error, stdout, stderr) => {
     args.splice(0, 0, '-Xss512k')
   }
 
-  return childProcess.spawn('java', args, {
+  return spawn('java', args, {
     shell: true,
     stdio: 'inherit'
   })
