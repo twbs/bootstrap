70869908ab441a29b255956a1454472514ebc9da
XhmikosR <xhmikosr@gmail.com>
2021-03-02 16:25:02 +0200

    Update data.js

--- js/src/dom/data.js
+++ js/src/dom/data.js
@@ -25,7 +25,7 @@ export default {
     // can be removed later when multiple key/instances are fine to be used
     if (!instanceMap.has(key) && instanceMap.size !== 0) {
       // eslint-disable-next-line no-console
-      console.error(`Notice: The design of Bootstrap disallows to use more than one instance per element. Bound instance: ${Array.from(instanceMap.keys())[0]}.`)
+      console.error(`Bootstrap doesn't allow more than one instance per element. Bound instance: ${Array.from(instanceMap.keys())[0]}.`)
       return
     }
 
