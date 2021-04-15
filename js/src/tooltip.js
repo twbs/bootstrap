3aca87e6fac30b53deef6ddcb5f648d2ab8a4193
XhmikosR <xhmikosr@gmail.com>
2021-04-15 16:41:02 +0300

    Bump version to 5.0.0

--- js/src/tooltip.js
+++ js/src/tooltip.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v5.0.0-beta3): tooltip.js
+ * Bootstrap (v5.0.0): tooltip.js
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
