92c7056619293a581626c37ef2c0095c6f1abceb
GeoSot <geo.sotis@gmail.com>
2021-06-10 10:52:35 +0300

    `_getDelegateConfig()`: add a comment and remove an unneeded config check

--- js/src/tooltip.js
+++ js/src/tooltip.js
@@ -685,14 +685,15 @@ class Tooltip extends BaseComponent {
   _getDelegateConfig() {
     const config = {}
 
-    if (this._config) {
-      for (const key in this._config) {
-        if (this.constructor.Default[key] !== this._config[key]) {
-          config[key] = this._config[key]
-        }
+    for (const key in this._config) {
+      if (this.constructor.Default[key] !== this._config[key]) {
+        config[key] = this._config[key]
       }
     }
 
+    // In the future can be replaced with:
+    // const keysWithDifferentValues = Object.entries(this._config).filter(entry => this.constructor.Default[entry[0]] !== this._config[entry[0]])
+    // `Object.fromEntries(keysWithDifferentValues)`
     return config
   }
 
