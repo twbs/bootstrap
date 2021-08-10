57d5cf6354e8f24a260c2d44a86d277314d8e172
XhmikosR <xhmikosr@gmail.com>
2021-08-10 17:52:39 +0300

    Update modal.js

--- js/src/modal.js
+++ js/src/modal.js
@@ -412,7 +412,7 @@ EventHandler.on(document, EVENT_CLICK_DATA_API, SELECTOR_DATA_TOGGLE, function (
     })
   })
 
-  // avoid conflict when clicking a toggler of an modal, while another is open
+  // avoid conflict when clicking moddal toggler while another one is open
   const allReadyOpen = SelectorEngine.findOne(OPEN_SELECTOR)
   if (allReadyOpen) {
     Modal.getInstance(allReadyOpen).hide()
