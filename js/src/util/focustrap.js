ac2c3739f19520bf1ad93aeb55fe781032b59a7f
XhmikosR <xhmikosr@gmail.com>
2021-09-02 09:29:04 +0300

    Prepare v5.1.1.

--- js/src/util/focustrap.js
+++ js/src/util/focustrap.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v5.1.0): util/focustrap.js
+ * Bootstrap (v5.1.1): util/focustrap.js
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
