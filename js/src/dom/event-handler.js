5a2b8b704a1468569ac21d84ef3da375dddf4d85
XhmikosR <xhmikosr@gmail.com>
2021-10-29 10:38:52 +0300

    event-handler.js: use `for...of`

--- js/src/dom/event-handler.js
+++ js/src/dom/event-handler.js
@@ -104,8 +104,8 @@ function bootstrapDelegationHandler(element, selector, fn) {
     const domElements = element.querySelectorAll(selector)
 
     for (let { target } = event; target && target !== this; target = target.parentNode) {
-      for (let i = domElements.length; i--;) {
-        if (domElements[i] === target) {
+      for (const domElement of domElements) {
+        if (domElement === target) {
           event.delegateTarget = target
 
           if (handler.oneOff) {
