331a935902e509c58e92228bc78fdece9bf633d7
XhmikosR <xhmikosr@gmail.com>
2021-11-02 13:04:29 +0200

    Move ESLint config to an override

--- js/tests/integration/bundle-modularity.js
+++ js/tests/integration/bundle-modularity.js
@@ -1,7 +1,6 @@
 import Tooltip from '../../dist/tooltip'
 import '../../dist/carousel'
 
-// eslint-disable-next-line ssr-friendly/no-dom-globals-in-module-scope
 window.addEventListener('load', () => {
   [].concat(...document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     .map(tooltipNode => new Tooltip(tooltipNode))
