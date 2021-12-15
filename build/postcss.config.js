a22694da130e0c4e3e9488769d6b3a120e859f83
XhmikosR <xhmikosr@gmail.com>
2021-12-15 10:52:49 +0200

    build/postcss.config.js: minor tweaks (#35506)

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,19 +1,19 @@
 'use strict'
 
-module.exports = ctx => {
+const mapConfig = {
+  inline: false,
+  annotation: true,
+  sourcesContent: true
+}
+
+module.exports = context => {
   return {
-    map: ctx.file.dirname.includes('examples') ?
-      false :
-      {
-        inline: false,
-        annotation: true,
-        sourcesContent: true
-      },
+    map: context.file.dirname.includes('examples') ? false : mapConfig,
     plugins: {
       autoprefixer: {
         cascade: false
       },
-      rtlcss: ctx.env === 'RTL' ? {} : false
+      rtlcss: context.env === 'RTL'
     }
   }
 }
