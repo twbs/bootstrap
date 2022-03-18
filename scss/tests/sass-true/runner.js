5dd8cbf4a417b9ba5e149db3677b3d0a286c0867
Romaric Pascal <hello@romaricpascal.is>
2022-03-18 09:05:51 +0000

    Remove irrelevant option for sass-true

--- scss/tests/sass-true/runner.js
+++ scss/tests/sass-true/runner.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 
-const sassTrue = require('sass-true')
+const { runSass } = require('sass-true')
 const fs = require('fs')
 const path = require('path')
 
@@ -8,8 +8,8 @@ module.exports = function (filename, { describe, it }) {
   const data = fs.readFileSync(filename, { encoding: 'utf8' })
   const TRUE_SETUP = '$true-terminal-output: false; @import \'true\';'
 
-  sassTrue.runSass({
+  runSass({
     data: TRUE_SETUP + data,
-    includePaths: [path.dirname(filename)],
-    sass: require('sass') }, { describe, it })
+    includePaths: [path.dirname(filename)]
+  }, { describe, it })
 }
