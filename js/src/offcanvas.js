63d4046026737ffa03eca7b479519126b89f3fdf
GeoSot <geo.sotis@gmail.com>
2022-04-06 02:39:54 +0300

    Feat: add resize handler to Offcanvas.js.
    If we could use as default the `.offcanvas` class without modifiers, we then, could add a simplified selector
    The selector itself, ignores the .offcanvas class as it doesn't have any responsive behavior
    The `aria-modal` addon is to protect us, selection backdrop elements

--- js/src/offcanvas.js
+++ js/src/offcanvas.js
@@ -41,6 +41,7 @@ const EVENT_SHOWN = `shown${EVENT_KEY}`
 const EVENT_HIDE = `hide${EVENT_KEY}`
 const EVENT_HIDE_PREVENTED = `hidePrevented${EVENT_KEY}`
 const EVENT_HIDDEN = `hidden${EVENT_KEY}`
+const EVENT_RESIZE = `resize${EVENT_KEY}`
 const EVENT_CLICK_DATA_API = `click${EVENT_KEY}${DATA_API_KEY}`
 const EVENT_KEYDOWN_DISMISS = `keydown.dismiss${EVENT_KEY}`
 
@@ -205,14 +206,6 @@ class Offcanvas extends BaseComponent {
 
       this.hide()
     })
-
-    EventHandler.on(window, 'resize', () => {
-      // Add this check to help js be aligned with css changes on responsive offcanvas
-      if (this._isShown && getComputedStyle(this._element).position !== 'fixed') {
-        // this._backdrop.hide()
-        this.hide()
-      }
-    })
   }
 
   // Static
@@ -268,6 +261,14 @@ EventHandler.on(document, EVENT_CLICK_DATA_API, SELECTOR_DATA_TOGGLE, function (
 EventHandler.on(window, EVENT_LOAD_DATA_API, () => {
   for (const selector of SelectorEngine.find(OPEN_SELECTOR)) {
     Offcanvas.getOrCreateInstance(selector).show()
+  }
+})
+
+EventHandler.on(window, EVENT_RESIZE, () => {
+  for (const element of SelectorEngine.find('[aria-modal][class*=show][class*=offcanvas-]')) {
+    if (getComputedStyle(element).position !== 'fixed') {
+      Offcanvas.getOrCreateInstance(element).hide()
+    }
   }
 })
 
