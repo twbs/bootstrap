bf4f4c320af1d0188ba8d739426917fc7274124e
CtrlAltLilith <81534108+CtrlAltLilith@users.noreply.github.com>
2022-05-27 19:58:45 -0400

    fix scrollbar padding

--- js/src/util/scrollbar.js
+++ js/src/util/scrollbar.js
@@ -15,7 +15,6 @@ import { isElement } from './index'
 
 const SELECTOR_FIXED_CONTENT = '.fixed-top, .fixed-bottom, .is-fixed, .sticky-top'
 const SELECTOR_STICKY_CONTENT = '.sticky-top'
-const PROPERTY_PADDING = 'padding-right'
 const PROPERTY_MARGIN = 'margin-right'
 
 /**
@@ -37,17 +36,12 @@ class ScrollBarHelper {
   hide() {
     const width = this.getWidth()
     this._disableOverFlow()
-    // give padding to element to balance the hidden scrollbar width
-    this._setElementAttributes(this._element, PROPERTY_PADDING, calculatedValue => calculatedValue + width)
     // trick: We adjust positive paddingRight and negative marginRight to sticky-top elements to keep showing fullwidth
-    this._setElementAttributes(SELECTOR_FIXED_CONTENT, PROPERTY_PADDING, calculatedValue => calculatedValue + width)
     this._setElementAttributes(SELECTOR_STICKY_CONTENT, PROPERTY_MARGIN, calculatedValue => calculatedValue - width)
   }
 
   reset() {
     this._resetElementAttributes(this._element, 'overflow')
-    this._resetElementAttributes(this._element, PROPERTY_PADDING)
-    this._resetElementAttributes(SELECTOR_FIXED_CONTENT, PROPERTY_PADDING)
     this._resetElementAttributes(SELECTOR_STICKY_CONTENT, PROPERTY_MARGIN)
   }
 
