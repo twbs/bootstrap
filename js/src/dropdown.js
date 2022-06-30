2d9765aa3eca5d4e459bd4296781da2aa566c0ba
GeoSot <geo.sotis@gmail.com>
2022-07-01 01:59:32 +0300

    feat: boost execute function, being able to use arguments

--- js/src/dropdown.js
+++ js/src/dropdown.js
@@ -8,6 +8,7 @@
 import * as Popper from '@popperjs/core'
 import {
   defineJQueryPlugin,
+  execute,
   getElement,
   getNextActiveElement,
   isDisabled,
@@ -319,7 +320,7 @@ class Dropdown extends BaseComponent {
 
     return {
       ...defaultBsPopperConfig,
-      ...(typeof this._config.popperConfig === 'function' ? this._config.popperConfig(defaultBsPopperConfig) : this._config.popperConfig)
+      ...execute(this._config.popperConfig, [defaultBsPopperConfig])
     }
   }
 
