a95f4d3c8191409afffee3085105bcf13c70f796
HARPHUNA <109001160+HARPHUNA@users.noreply.github.com>
2022-08-18 23:39:04 +0200

    Delete site/content directory

--- site/content/docs/5.2/examples/offcanvas-navbar/offcanvas.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  'use strict'
-
-  document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
-    document.querySelector('.offcanvas-collapse').classList.toggle('open')
-  })
-})()
