916e2fc7132b441ec7f674871cca467ec47579bf
XhmikosR <xhmikosr@gmail.com>
2022-09-07 13:35:40 +0300

    Dist

--- js/dist/util/focustrap.js
+++ js/dist/util/focustrap.js
@@ -1,5 +1,5 @@
 /*!
-  * Bootstrap focustrap.js v5.2.0 (https://getbootstrap.com/)
+  * Bootstrap focustrap.js v5.2.1 (https://getbootstrap.com/)
   * Copyright 2011-2022 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
@@ -17,7 +17,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v5.2.0): util/focustrap.js
+   * Bootstrap (v5.2.1): util/focustrap.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
