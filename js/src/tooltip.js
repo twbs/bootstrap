6bedbe344ce557350d821c1b51e783ff299117a0
XhmikosR <xhmikosr@gmail.com>
2022-10-01 21:05:37 +0300

    Update tooltip.js

--- js/src/tooltip.js
+++ js/src/tooltip.js
@@ -69,9 +69,9 @@ const Default = {
   sanitizeFn: null,
   selector: false,
   template: '<div class="tooltip" role="tooltip">' +
-    '<div class="tooltip-arrow"></div>' +
-    '<div class="tooltip-inner"></div>' +
-    '</div>',
+            '<div class="tooltip-arrow"></div>' +
+            '<div class="tooltip-inner"></div>' +
+            '</div>',
   title: '',
   trigger: 'hover focus'
 }
