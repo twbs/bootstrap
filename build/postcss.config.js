50d5371f15a23b699d9fac52925cf9d502428e6f
Gaël Poupard <gael+github@ffoodd.fr>
2022-11-18 18:37:19 +0100

    chore(banner)[WIP]: POC using postcss-banner & terser preamble

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const pkg = require('../package.json')
+const year = new Date().getFullYear()
+
 const mapConfig = {
   inline: false,
   annotation: true,
@@ -13,7 +16,16 @@ module.exports = context => {
       autoprefixer: {
         cascade: false
       },
-      rtlcss: context.env === 'RTL'
+      rtlcss: context.env === 'RTL',
+      'postcss-banner': {
+        important: true,
+        banner: context.file.basename.includes('bootstrap') ?
+          `${pkg.name} ${context.file.basename.split('-').length > 1 ? `${context.file.basename.split('-')[1].split('.')[0]} ` : ''}v${pkg.version} (${pkg.homepage})
+Copyright 2011-${year} ${pkg.author}
+Copyright 2011-${year} ${pkg.contributors[0]}
+Licensed under ${pkg.license} (https://github.com/twbs/bootstrap/blob/main/LICENSE)` :
+          ''
+      }
     }
   }
 }
