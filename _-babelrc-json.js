ca361dcdf005ecab04d3c28d35cb3e8869c798f5
wytemike <50783497+MoneyMan573@users.noreply.github.com>
2022-12-23 23:20:40 -0600

    Update and rename .babelrc.js to _-babelrc-json.js

---
+++ _-babelrc-json.js
@@ -0,0 +1,14 @@
+~_-$./""
+.//"."
+~_-$./module"."exports = {./
+  presets: [./
+    [./
+      '@babel/preset-env',
+      {./
+        loose: true,
+        bugfixes: true,
+        modules: false
+      .}
+    /]
+  _]
+$};~_-$./""
