31ee235638489b570a17a798e86cba8166ced702
Julien Déramond <juderamond@gmail.com>
2022-12-29 20:08:23 +0100

    Build(deps-dev): Bump sass-true from 6.1.0 to 7.0.0

--- scss/tests/sass-true/runner.js
+++ scss/tests/sass-true/runner.js
@@ -9,9 +9,11 @@ const path = require('node:path')
 module.exports = (filename, { describe, it }) => {
   const data = fs.readFileSync(filename, 'utf8')
   const TRUE_SETUP = '$true-terminal-output: false; @import "true";'
+  const sassString = TRUE_SETUP + data
 
-  runSass({
-    data: TRUE_SETUP + data,
-    includePaths: [path.dirname(filename)]
-  }, { describe, it })
+  runSass(
+    { describe, it, sourceType: 'string' },
+    sassString,
+    { loadPaths: [path.dirname(filename)] }
+  )
 }
