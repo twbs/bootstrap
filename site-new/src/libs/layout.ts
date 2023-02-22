8a08140afcefd762a1c5c1abd12afc0bcf16e38d
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-02-22 17:58:12 +0100

    More thingies (#14)
    * fix: pass down astro props to base layout
    
    * feat: 404 page ui
    
    This commit introduces the concept of layout `overrides` to add custom
    attributes to the <body /> and <main /> tags.
    
    * fix: remove unused exports
    
    * docs: document base layout props
    
    This also refactor frontmatter props to use `MarkdownLayoutProps` as
    they should.
    
    * feat: 404 page title
    
    * chore: remove 404 description todo already handled
    
    * feat: docs layout body overrides
    
    * feat: add MastHead component

--- site-new/src/libs/layout.ts
+++ site-new/src/libs/layout.ts
@@ -1,3 +1,9 @@
+import type { HTMLAttributes, HTMLTag } from 'astro/types'
+
 // TODO(HiDeoo) Not sure yet if a union of literal strings is the best way to handle this, need to see more of the
 // current usage in the existing docs but it's at least strictly typed for now.
 export type Layout = 'docs' | 'examples' | undefined
+
+export type LayoutOverridesHTMLAttributes<TTag extends HTMLTag> = HTMLAttributes<TTag> & {
+  [key in `data-${string}`]: string
+}
--- site-new/src/pages/markdown.md
+++ site-new/src/pages/markdown.md
@@ -1,8 +1,9 @@
 ---
-layout: ../layouts/BaseLayout.astro
+layout: ../layouts/DocsLayout.astro
 title: Markdown page
 description: Amazing Markdown page
 thumbnail: 'test.png'
+toc: true
 ---
 
 This is a test page to test page titles, descriptions & thumbnails.
