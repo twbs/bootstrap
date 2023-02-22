511f6448e7c4015dfa602ff7e07333fe3dda6363
Julien Déramond <juderamond@gmail.com>
2023-02-22 18:30:07 +0100

    Use MDX instead of simple MD + first shortcode component: AddedIn.astro

--- site-new/astro.config.mjs
+++ site-new/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   site: 'https://getbootstrap.com/',
 })
