8789da66827a99a816008476af5b7b4ccd32eece
Mukesh Rana <mukeshr@lambdatest.com>
2023-03-23 16:37:04 +0530

    Adding Lambdatest support

---
+++ js/tests/browsers-old.js
@@ -0,0 +1,79 @@
+/* eslint-env node */
+/* eslint-disable camelcase */
+
+const browsers = {
+  safariMac: {
+    base: 'BrowserStack',
+    os: 'OS X',
+    os_version: 'Catalina',
+    browser: 'Safari',
+    browser_version: 'latest'
+  },
+  chromeMac: {
+    base: 'BrowserStack',
+    os: 'OS X',
+    os_version: 'Catalina',
+    browser: 'Chrome',
+    browser_version: 'latest'
+  },
+  firefoxMac: {
+    base: 'BrowserStack',
+    os: 'OS X',
+    os_version: 'Catalina',
+    browser: 'Firefox',
+    browser_version: 'latest'
+  },
+  chromeWin10: {
+    base: 'BrowserStack',
+    os: 'Windows',
+    os_version: '10',
+    browser: 'Chrome',
+    browser_version: '60'
+  },
+  firefoxWin10: {
+    base: 'BrowserStack',
+    os: 'Windows',
+    os_version: '10',
+    browser: 'Firefox',
+    browser_version: '60'
+  },
+  chromeWin10Latest: {
+    base: 'BrowserStack',
+    os: 'Windows',
+    os_version: '10',
+    browser: 'Chrome',
+    browser_version: 'latest'
+  },
+  firefoxWin10Latest: {
+    base: 'BrowserStack',
+    os: 'Windows',
+    os_version: '10',
+    browser: 'Firefox',
+    browser_version: 'latest'
+  },
+  iphone7: {
+    base: 'BrowserStack',
+    os: 'ios',
+    os_version: '12.0',
+    device: 'iPhone 7',
+    real_mobile: true
+  },
+  iphone12: {
+    base: 'BrowserStack',
+    os: 'ios',
+    os_version: '14.0',
+    device: 'iPhone 12',
+    real_mobile: true
+  },
+  pixel2: {
+    base: 'BrowserStack',
+    os: 'android',
+    os_version: '8.0',
+    device: 'Google Pixel 2',
+    real_mobile: true
+  }
+}
+
+module.exports = {
+  browsers
+}
--- js/tests/browsers.js
+++ js/tests/browsers.js
@@ -3,75 +3,58 @@
 
 const browsers = {
   safariMac: {
-    base: 'BrowserStack',
-    os: 'OS X',
-    os_version: 'Catalina',
-    browser: 'Safari',
-    browser_version: 'latest'
+    platform: 'macOS Catalina',
+    browserName: 'Safari',
+    browserVersion: 'latest'
   },
   chromeMac: {
-    base: 'BrowserStack',
-    os: 'OS X',
-    os_version: 'Catalina',
-    browser: 'Chrome',
-    browser_version: 'latest'
+    platform: 'macOS Catalina',
+    browserName: 'Chrome',
+    browserVersion: 'latest'
   },
   firefoxMac: {
-    base: 'BrowserStack',
-    os: 'OS X',
-    os_version: 'Catalina',
-    browser: 'Firefox',
-    browser_version: 'latest'
+    platform: 'macOS Catalina',
+    browserName: 'Firefox',
+    browserVersion: 'latest'
   },
   chromeWin10: {
-    base: 'BrowserStack',
-    os: 'Windows',
-    os_version: '10',
-    browser: 'Chrome',
-    browser_version: '60'
+    platform: 'Windows 10',
+    browserName: 'Chrome',
+    browserVersion: '60'
   },
   firefoxWin10: {
-    base: 'BrowserStack',
-    os: 'Windows',
-    os_version: '10',
-    browser: 'Firefox',
-    browser_version: '60'
+    platform: 'Windows 10',
+    browserName: 'Firefox',
+    browserVersion: '60'
   },
   chromeWin10Latest: {
-    base: 'BrowserStack',
-    os: 'Windows',
-    os_version: '10',
-    browser: 'Chrome',
-    browser_version: 'latest'
+    platform: 'Windows 10',
+    browserName: 'Chrome',
+    browserVersion: 'latest'
   },
   firefoxWin10Latest: {
-    base: 'BrowserStack',
-    os: 'Windows',
-    os_version: '10',
-    browser: 'Firefox',
-    browser_version: 'latest'
+    platform: 'Windows 10',
+    browserName: 'Firefox',
+    browserVersion: 'latest'
   },
-  iphone7: {
-    base: 'BrowserStack',
-    os: 'ios',
-    os_version: '12.0',
-    device: 'iPhone 7',
-    real_mobile: true
-  },
-  iphone12: {
-    base: 'BrowserStack',
-    os: 'ios',
-    os_version: '14.0',
-    device: 'iPhone 12',
-    real_mobile: true
-  },
-  pixel2: {
-    base: 'BrowserStack',
-    os: 'android',
-    os_version: '8.0',
-    device: 'Google Pixel 2',
-    real_mobile: true
-  }
+  // iphone7: {
+  //   platform: 'ios',
+  //   platformVersion: '12.0',
+  //   deviceName: 'iPhone 7',
+  //   real_mobile: true
+  // },
+  // iphone12: {
+  //   platform: 'ios',
+  //   platformVersion: '14.0',
+  //   deviceName: 'iPhone 12',
+  //   real_mobile: true
+  // },
+  // pixel2: {
+  //   platform: 'android',
+  //   platformVersion: '8.0',
+  //   deviceName: 'Google Pixel 2',
+  //   real_mobile: true
+  // }
 }
 
 module.exports = {
