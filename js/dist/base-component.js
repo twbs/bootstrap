c877cefcef18d6a60c5eaec8df469933e64e212a
XhmikosR <xhmikosr@gmail.com>
2023-03-24 16:30:16 +0200

    Release v5.3.0-alpha2 (#38244)
    * Bump version to 5.3.0-alpha2
    
    * Dist

--- js/dist/base-component.js
+++ js/dist/base-component.js
@@ -1,6 +1,6 @@
 /*!
-  * Bootstrap base-component.js v5.3.0-alpha1 (https://getbootstrap.com/)
-  * Copyright 2011-2022 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+  * Bootstrap base-component.js v5.3.0-alpha2 (https://getbootstrap.com/)
+  * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
 (function (global, factory) {
@@ -11,7 +11,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v5.3.0-alpha1): base-component.js
+   * Bootstrap base-component.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -20,7 +20,7 @@
    * Constants
    */
 
-  const VERSION = '5.3.0-alpha1';
+  const VERSION = '5.3.0-alpha2';
 
   /**
    * Class definition
--- js/dist/dom/event-handler.js
+++ js/dist/dom/event-handler.js
@@ -1,6 +1,6 @@
 /*!
-  * Bootstrap event-handler.js v5.3.0-alpha1 (https://getbootstrap.com/)
-  * Copyright 2011-2022 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+  * Bootstrap event-handler.js v5.3.0-alpha2 (https://getbootstrap.com/)
+  * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
 (function (global, factory) {
@@ -11,7 +11,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v5.3.0-alpha1): dom/event-handler.js
+   * Bootstrap dom/event-handler.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
