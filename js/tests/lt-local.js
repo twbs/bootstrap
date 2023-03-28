caa739f7d55fa615deb297651b43ed9bed2e45f3
XhmikosR <xhmikosr@gmail.com>
2023-03-28 13:22:30 +0300

    Fixes

--- js/tests/lt-local.js
+++ js/tests/lt-local.js
@@ -1,18 +1,26 @@
-const lambdaTunnel = require("@lambdatest/node-tunnel");
-const tunnelInstance = new lambdaTunnel();
-const tunnelArguments = {
-    user: process.env.LT_USERNAME,
-    key: process.env.LT_ACCESS_KEY,
-    tunnelName: process.env.LT_TUNNEL_NAME || 'jasmine',
-    logFile: "local.log"
-};
+/* eslint-env node */
+/* eslint-disable no-console */
 
-if (process.env.LAMBDATEST == 'true') {
-    (async () => {
-        try {
-            await tunnelInstance.start(tunnelArguments)
-        } catch (err) {
-            console.log(err.message)
-        }
-    })()
+'use strict'
+
+const process = require('node:process')
+const lambdaTunnel = require('@lambdatest/node-tunnel')
+
+const tunnelInstance = new lambdaTunnel() // eslint-disable-line new-cap
+const tunnelArguments = {
+  user: process.env.LT_USERNAME,
+  key: process.env.LT_ACCESS_KEY,
+  tunnelName: process.env.LT_TUNNEL_NAME || 'jasmine',
+  logFile: 'local.log'
+}
+
+if (process.env.LAMBDATEST === 'true') {
+  // eslint-disable-next-line unicorn/prefer-top-level-await
+  (async () => {
+    try {
+      await tunnelInstance.start(tunnelArguments)
+    } catch (error) {
+      console.log(error.message)
+    }
+  })()
 }
