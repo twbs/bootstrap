8880601ba62035e7d2aab3a22fef24d73044a24f
XhmikosR <xhmikosr@gmail.com>
2023-03-28 13:22:30 +0300

    Fixes

--- js/tests/lt-local.js
+++ js/tests/lt-local.js
@@ -1,18 +1,22 @@
-const lambdaTunnel = require("@lambdatest/node-tunnel");
-const tunnelInstance = new lambdaTunnel();
-const tunnelArguments = {
-    user: process.env.LT_USERNAME,
-    key: process.env.LT_ACCESS_KEY,
-    tunnelName: process.env.LT_TUNNEL_NAME || 'jasmine',
-    logFile: "local.log"
-};
+/* eslint-env node */
 
-if (process.env.LAMBDATEST == 'true') {
-    (async () => {
-        try {
-            await tunnelInstance.start(tunnelArguments)
-        } catch (err) {
-            console.log(err.message)
-        }
-    })()
+'use strict'
+
+const process = require('node:process')
+const lambdaTunnel = require('@lambdatest/node-tunnel')
+
+const tunnelInstance = new lambdaTunnel() // eslint-disable-line new-cap
+const tunnelArguments = {
+  user: process.env.LT_USERNAME,
+  key: process.env.LT_ACCESS_KEY,
+  tunnelName: process.env.LT_TUNNEL_NAME || 'jasmine',
+  logFile: 'local.log'
+}
+
+if (process.env.LAMBDATEST === 'true') {
+  try {
+    await tunnelInstance.start(tunnelArguments)
+  } catch (error) {
+    throw new Error(error)
+  }
 }
