4c3849f2317faddbd7615cea41d7234f948b9c6f
XhmikosR <xhmikosr@gmail.com>
2023-03-28 13:22:30 +0300

    Fixes

--- js/tests/browsers.js
+++ js/tests/browsers.js
@@ -7,50 +7,50 @@ const browsers = {
     safariMac: {
       browserName: 'Safari',
       browserVersion: 'latest',
-      "LT:Options": {
-        "platformName": "MacOS Monterey"
+      'LT:Options': {
+        platformName: 'MacOS Monterey'
       }
     },
     chromeMac: {
       browserName: 'Chrome',
       browserVersion: 'latest',
-      "LT:Options": {
-        "platformName": "MacOS Monterey"
+      'LT:Options': {
+        platformName: 'MacOS Monterey'
       }
     },
     firefoxMac: {
       browserName: 'Firefox',
       browserVersion: 'latest',
-      "LT:Options": {
-        "platformName": "MacOS Monterey"
+      'LT:Options': {
+        platformName: 'MacOS Monterey'
       }
     },
     chromeWin10: {
       browserName: 'Chrome',
       browserVersion: '60',
-      "LT:Options": {
-        "platformName": "Windows 10"
+      'LT:Options': {
+        platformName: 'Windows 10'
       }
     },
     firefoxWin10: {
       browserName: 'Firefox',
       browserVersion: '60',
-      "LT:Options": {
-        "platformName": "Windows 10"
+      'LT:Options': {
+        platformName: 'Windows 10'
       }
     },
     chromeWin10Latest: {
       browserName: 'Chrome',
       browserVersion: 'latest',
-      "LT:Options": {
-        "platformName": "Windows 10"
+      'LT:Options': {
+        platformName: 'Windows 10'
       }
     },
     firefoxWin10Latest: {
       browserName: 'Firefox',
       browserVersion: 'latest',
-      "LT:Options": {
-        "platformName": "Windows 10"
+      'LT:Options': {
+        platformName: 'Windows 10'
       }
     },
     iphone7: {
