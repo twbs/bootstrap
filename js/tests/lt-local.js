7f6b214a2a3b5b92ec317a8349ef381ce285f3e7
XhmikosR <xhmikosr@gmail.com>
2023-04-11 17:51:29 +0300

    Update lt-local.js

--- js/tests/lt-local.js
+++ js/tests/lt-local.js
@@ -6,24 +6,27 @@
 const process = require('node:process')
 const lambdaTunnel = require('@lambdatest/node-tunnel')
 
+if (process.env.LAMBDATEST !== 'true') {
+  console.log('Skipping lt-local script!')
+  return
+}
+
 const tunnelInstance = new lambdaTunnel() // eslint-disable-line new-cap
 const tunnelArguments = {
   user: process.env.LT_USERNAME,
   key: process.env.LT_ACCESS_KEY,
   tunnelName: process.env.LT_TUNNEL_NAME || 'jasmine',
   logFile: 'local.log'
-}
+};
 
-if (process.env.LAMBDATEST === 'true') {
-  // eslint-disable-next-line unicorn/prefer-top-level-await
-  (async () => {
-    try {
-      await tunnelInstance.start(tunnelArguments)
-      await new Promise(res => {
-        setTimeout(res, 5000)
-      })
-    } catch (error) {
-      console.log(error.message)
-    }
-  })()
-}
+// eslint-disable-next-line unicorn/prefer-top-level-await
+(async () => {
+  try {
+    await tunnelInstance.start(tunnelArguments)
+    await new Promise(res => {
+      setTimeout(res, 5000)
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+})()
