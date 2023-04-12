f27a9c02d8c40cbfd4a2eaf80f6093f08e0831ae
XhmikosR <xhmikosr@gmail.com>
2023-04-12 18:53:25 +0300

    Updates

--- js/tests/browsers.js
+++ js/tests/browsers.js
@@ -3,78 +3,6 @@
 'use strict'
 
 const browsers = {
-  lambdaTest: {
-    safariMac: {
-      browserName: 'Safari',
-      browserVersion: 'latest',
-      'LT:Options': {
-        platformName: 'MacOS Monterey'
-      }
-    },
-    chromeMac: {
-      browserName: 'Chrome',
-      browserVersion: 'latest',
-      'LT:Options': {
-        platformName: 'MacOS Monterey'
-      }
-    },
-    firefoxMac: {
-      browserName: 'Firefox',
-      browserVersion: 'latest',
-      'LT:Options': {
-        platformName: 'MacOS Monterey'
-      }
-    },
-    chromeWin10: {
-      browserName: 'Chrome',
-      browserVersion: '60',
-      'LT:Options': {
-        platformName: 'Windows 10'
-      }
-    },
-    firefoxWin10: {
-      browserName: 'Firefox',
-      browserVersion: '60',
-      'LT:Options': {
-        platformName: 'Windows 10'
-      }
-    },
-    chromeWin10Latest: {
-      browserName: 'Chrome',
-      browserVersion: 'latest',
-      'LT:Options': {
-        platformName: 'Windows 10'
-      }
-    },
-    firefoxWin10Latest: {
-      browserName: 'Firefox',
-      browserVersion: 'latest',
-      'LT:Options': {
-        platformName: 'Windows 10'
-      }
-    },
-    iphone11: {
-      platformName: 'ios',
-      platformVersion: '15',
-      browserName: 'Safari',
-      deviceName: 'iPhone 13',
-      isRealMobile: true
-    },
-    iphone14: {
-      platformName: 'ios',
-      platformVersion: '16',
-      browserName: 'Safari',
-      deviceName: 'iPhone 14',
-      isRealMobile: true
-    },
-    pixel6: {
-      platformName: 'android',
-      platformVersion: '12',
-      browserName: 'Chrome',
-      deviceName: 'Pixel 6',
-      isRealMobile: true
-    }
-  },
   browserStack: {
     safariMac: {
       base: 'BrowserStack',
@@ -145,6 +73,78 @@ const browsers = {
       os_version: '8.0',
       device: 'Google Pixel 2',
       real_mobile: true
+    }
+  },
+  lambdaTest: {
+    safariMac: {
+      browserName: 'Safari',
+      browserVersion: 'latest',
+      'LT:Options': {
+        platformName: 'MacOS Monterey'
+      }
+    },
+    chromeMac: {
+      browserName: 'Chrome',
+      browserVersion: 'latest',
+      'LT:Options': {
+        platformName: 'MacOS Monterey'
+      }
+    },
+    firefoxMac: {
+      browserName: 'Firefox',
+      browserVersion: 'latest',
+      'LT:Options': {
+        platformName: 'MacOS Monterey'
+      }
+    },
+    chromeWin10: {
+      browserName: 'Chrome',
+      browserVersion: '60',
+      'LT:Options': {
+        platformName: 'Windows 10'
+      }
+    },
+    firefoxWin10: {
+      browserName: 'Firefox',
+      browserVersion: '60',
+      'LT:Options': {
+        platformName: 'Windows 10'
+      }
+    },
+    chromeWin10Latest: {
+      browserName: 'Chrome',
+      browserVersion: 'latest',
+      'LT:Options': {
+        platformName: 'Windows 10'
+      }
+    },
+    firefoxWin10Latest: {
+      browserName: 'Firefox',
+      browserVersion: 'latest',
+      'LT:Options': {
+        platformName: 'Windows 10'
+      }
+    },
+    iphone11: {
+      platformName: 'ios',
+      platformVersion: '15',
+      browserName: 'Safari',
+      deviceName: 'iPhone 13',
+      isRealMobile: true
+    },
+    iphone14: {
+      platformName: 'ios',
+      platformVersion: '16',
+      browserName: 'Safari',
+      deviceName: 'iPhone 14',
+      isRealMobile: true
+    },
+    pixel6: {
+      platformName: 'android',
+      platformVersion: '12',
+      browserName: 'Chrome',
+      deviceName: 'Pixel 6',
+      isRealMobile: true
     }
   }
 }
