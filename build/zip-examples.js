8ff9364e43e5335489efaa95c535aba94cdd2cc6
Julien Déramond <juderamond@gmail.com>
2023-06-12 12:43:56 +0200

    Add `color-modes.js` to `bootstrap-x.y.z-examples.zip`

--- build/zip-examples.js
+++ build/zip-examples.js
@@ -34,6 +34,9 @@ const imgFiles = [
   'bootstrap-logo.svg',
   'bootstrap-logo-white.svg'
 ]
+const staticJsFiles = [
+  'color-modes.js'
+]
 
 sh.config.fatal = true
 
@@ -52,7 +55,8 @@ sh.mkdir('-p', [
   distFolder,
   `${distFolder}/assets/brand/`,
   `${distFolder}/assets/dist/css/`,
-  `${distFolder}/assets/dist/js/`
+  `${distFolder}/assets/dist/js/`,
+  `${distFolder}/assets/js/`
 ])
 
 sh.cp('-Rf', `${docsDir}/examples/*`, distFolder)
@@ -67,6 +71,10 @@ for (const file of jsFiles) {
 
 for (const file of imgFiles) {
   sh.cp('-f', `${docsDir}/assets/brand/${file}`, `${distFolder}/assets/brand/`)
+}
+
+for (const file of staticJsFiles) {
+  sh.cp('-f', `${docsDir}/assets/js/${file}`, `${distFolder}/assets/js/`)
 }
 
 sh.rm(`${distFolder}/index.html`)
