41e74a4ee756bdcdc892aea2a6d11514f4fd3a00
Muskan-Rao <20104096@apsit.edu.in>
2023-10-12 22:32:57 +0530

    ROSPL Project: Bootstrap Template

---
+++ bootstrap/docs/examples/bootstrap-template/bootstrap-template/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Business Frontpage v5.0.9 (https://startbootstrap.com/template/business-frontpage)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
