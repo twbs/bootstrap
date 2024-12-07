062009286e88eff4ad2ac3deceec258112055b76
alhernandez25 <hernalej@oregonstate.edu>
2024-12-07 03:27:14 -0500

    Linting fix

--- js/src/dropdown.js
+++ js/src/dropdown.js
@@ -10,7 +10,7 @@ import BaseComponent from './base-component.js'
 import EventHandler from './dom/event-handler.js'
 import Manipulator from './dom/manipulator.js'
 import SelectorEngine from './dom/selector-engine.js'
-import Modal from "./modal";
+import Modal from "./modal.js"
 
 import {
   defineJQueryPlugin,
