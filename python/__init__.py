be9632f54dcf83fdfa4247a9eba5daf495f1a3eb
MaximilianClemens <m.clemens@mailbox.org>
2025-03-12 23:40:29 +0100

    linting of __init__.py

--- python/__init__.py
+++ python/__init__.py
@@ -1,14 +1,17 @@
+"""
+Load the package.json to get the current version of the package for the installer
+"""
+
 import json
 from pathlib import Path
-from importlib import resources
 
 try:
     # Loading package.json
     package_json_path = Path(__file__).parent.parent / "package.json"
     with package_json_path.open(encoding="utf-8") as f:
         data = json.load(f)
-    
+
     # to get current version
     __version__ = data["version"]
-except Exception as e:
+except Exception as e: # pylint: disable=broad-exception-caught
     __version__ = "0.0.0"
