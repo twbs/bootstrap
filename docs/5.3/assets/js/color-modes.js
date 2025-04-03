0b56c3ef7e80d840fc5238cae6b0d18de89dcfb7
XhmikosR <xhmikosr@gmail.com>
2025-04-03 19:14:43 +0300

    Add v5.3.4 docs (#40657)
    * Add v5.3.4 docs
    
    * Rebuild
    
    * Update stackblitz
    
    ---------
    
    Co-authored-by: Mark Otto <markdotto@gmail.com>

--- docs/5.3/assets/js/color-modes.js
+++ docs/5.3/assets/js/color-modes.js
@@ -1,6 +1,6 @@
 /*!
  * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2024 The Bootstrap Authors
+ * Copyright 2011-2025 The Bootstrap Authors
  * Licensed under the Creative Commons Attribution 3.0 Unported License.
  */
 
