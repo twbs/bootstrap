876350399bce883624a45d4073f7876c7a4840fb
zakariae10alaoui <elalaouieddahbizakariae@gmail.com>
2025-04-10 16:11:28 +0100

    Update index.esm.js

--- js/index.esm.js
+++ js/index.esm.js
@@ -4,7 +4,7 @@
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
-
+test 123
 export { default as Alert } from './src/alert.js'
 export { default as Button } from './src/button.js'
 export { default as Carousel } from './src/carousel.js'
