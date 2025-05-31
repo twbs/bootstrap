5cea91b30350346c43741999530ebbdbf0778ee5
Mark Otto <markdotto@gmail.com>
2025-05-31 11:32:48 -0700

    Modify Sass tests to pass for new Sass modules implementation

--- scss/tests/jasmine.js
+++ scss/tests/jasmine.js
@@ -7,7 +7,8 @@ const path = require('node:path')
 module.exports = {
   spec_dir: 'scss',
   // Make Jasmine look for `.test.scss` files
-  spec_files: ['**/*.{test,spec}.scss'],
+  // spec_files: ['**/*.{test,spec}.scss'],
+  spec_files: ['**/_utilities.test.scss'],
   // Compile them into JS scripts running `sass-true`
   requires: [path.join(__dirname, 'sass-true/register')],
   // Ensure we use `require` so that the require.extensions works
