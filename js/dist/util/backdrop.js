25aa8cc0b32f0d1a54be575347e6d84b70b1acd7
Mark Otto <markd.otto@gmail.com>
2025-08-25 19:01:02 -0700

    Release v5.3.8 (#41669)
    * Bump version
    
    * Run dist
    
    * Unrelated new docs error

--- js/dist/util/backdrop.js
+++ js/dist/util/backdrop.js
@@ -1,5 +1,5 @@
 /*!
-  * Bootstrap backdrop.js v5.3.7 (https://getbootstrap.com/)
+  * Bootstrap backdrop.js v5.3.8 (https://getbootstrap.com/)
   * Copyright 2011-2025 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
