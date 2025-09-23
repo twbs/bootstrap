dc7aef3f891820aa39b13bf3c854d2f9f43bddab
Mark Otto <markdotto@gmail.com>
2025-09-22 21:58:49 -0700

    Use `@forward` instead of `@use` for proper customization

--- scss/tests/jasmine.js
+++ scss/tests/jasmine.js
@@ -8,7 +8,7 @@ module.exports = {
   spec_dir: 'scss',
   // Make Jasmine look for `.test.scss` files
   // spec_files: ['**/*.{test,spec}.scss'],
-  spec_files: ['**/_utilities.test.scss'],
+  spec_files: ['**/_utilities.test.scss', '**/modules/_configuration.test.scss'],
   // Compile them into JS scripts running `sass-true`
   requires: [path.join(__dirname, 'sass-true/register')],
   // Ensure we use `require` so that the require.extensions works
