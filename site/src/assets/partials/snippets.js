498de08235d9d983788a3ccc03fac929fa2925ce
Mark Otto <markd.otto@gmail.com>
2025-12-18 14:46:26 -0800

    Rename some components to be singular (#41952)
    * Singular spinner
    
    * Rename some component files to be singular

--- site/src/assets/partials/snippets.js
+++ site/src/assets/partials/snippets.js
@@ -80,7 +80,7 @@ export default () => {
   const appendAlert = (message, type) => {
     const wrapper = document.createElement('div')
     wrapper.innerHTML = [
-      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `<div class="alert theme-${type} alert-dismissible" role="alert">`,
       `   <div>${message}</div>`,
       '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
       '</div>'
