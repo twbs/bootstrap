30c1f702212cea0ff98e0708181eea1adc073ed1
Mark Otto <markdotto@gmail.com>
2025-12-29 14:55:50 -0800

    Missed file

--- js/index.esm.js
+++ js/index.esm.js
@@ -12,11 +12,8 @@ export { default as Collapse } from './src/collapse.js'
 export { default as Dialog } from './src/dialog.js'
 export { default as Dropdown } from './src/dropdown.js'
 export { default as Offcanvas } from './src/offcanvas.js'
-<<<<<<< HEAD
 export { default as Strength } from './src/strength.js'
-=======
 export { default as OtpInput } from './src/otp-input.js'
->>>>>>> 3e5b9bb18 (feat: add OTP input component)
 export { default as Popover } from './src/popover.js'
 export { default as ScrollSpy } from './src/scrollspy.js'
 export { default as Tab } from './src/tab.js'
