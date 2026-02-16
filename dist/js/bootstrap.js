857f80445accbc46cd2ff6af656fa9481dbcf168
Aarav02Garg10 <aaravgarg0210@gmail.com>
2026-02-16 20:14:42 +0530

    Added cursor utilities to scss

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1,6 +1,6 @@
 /*!
   * Bootstrap v5.3.8 (https://getbootstrap.com/)
-  * Copyright 2011-2025 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+  * Copyright 2011-2026 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
 (function (global, factory) {
