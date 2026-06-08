32e8a3ed89912511166c280352e2fc008d4871e7
Julien Déramond <juderamond@gmail.com>
2026-06-08 20:06:55 +0200

    Drop deprecated `.accordion-button` remaining usage

--- js/tests/unit/collapse.spec.js
+++ js/tests/unit/collapse.spec.js
@@ -316,7 +316,7 @@ describe('Collapse', () => {
           '<div class="accordion" id="accordionExample">',
           '  <div class="accordion-item">',
           '    <h2 class="accordion-header">',
-          '      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">',
+          '      <button type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">',
           '        Accordion Item #1',
           '      </button>',
           '    </h2>',
